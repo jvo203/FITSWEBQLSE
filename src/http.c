179e59a6ed85af8c63000944bb41e824ade4fa03
jvo203 <chris.zapart@nao.ac.jp>
2021-01-05 17:37:29 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -1,8 +1,83 @@
 #include <microhttpd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define HTTP_PORT 8080
+
+#define PAGE "<html><head><title>libmicrohttpd demo</title>" \
+             "</head><body>libmicrohttpd demo</body></html>"
 
 struct MHD_Daemon *http_server = NULL;
+
+static int on_client_connect(void *cls,
+                             const struct sockaddr *addr,
+                             socklen_t addrlen);
 
 static int on_http_connection(void *cls, struct MHD_Connection *connection,
                               const char *url, const char *method,
                               const char *version, const char *upload_data,
                               size_t *upload_data_size, void **con_cls);
+
+static enum MHD_Result
+ahc_echo(void *cls,
+         struct MHD_Connection *connection,
+         const char *url,
+         const char *method,
+         const char *version,
+         const char *upload_data,
+         size_t *upload_data_size,
+         void **ptr)
+{
+    static int dummy;
+    const char *page = cls;
+    struct MHD_Response *response;
+    int ret;
+
+    if (0 != strcmp(method, "GET"))
+        return MHD_NO; /* unexpected method */
+    if (&dummy != *ptr)
+    {
+        /* The first time only the headers are valid,
+         do not respond in the first round... */
+        *ptr = &dummy;
+        return MHD_YES;
+    }
+    if (0 != *upload_data_size)
+        return MHD_NO; /* upload data in a GET!? */
+    *ptr = NULL;       /* clear context pointer */
+    response = MHD_create_response_from_buffer(strlen(page),
+                                               (void *)page,
+                                               MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response(connection,
+                             MHD_HTTP_OK,
+                             response);
+    MHD_destroy_response(response);
+    return ret;
+}
+
+extern int start_http()
+{
+    /*http_server = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION | MHD_USE_INTERNAL_POLLING_THREAD | MHD_USE_ERROR_LOG | MHD_USE_ITC,
+                                   HTTP_PORT,
+                                   NULL, NULL, &on_http_connection, NULL,
+                                   MHD_OPTION_END);*/
+
+    http_server = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
+                                   HTTP_PORT,
+                                   NULL,
+                                   NULL,
+                                   &ahc_echo,
+                                   PAGE,
+                                   MHD_OPTION_END);
+
+    if (http_server == NULL)
+        printf("Could not start a libmicrohttpd web server.\n");
+    else
+    {
+        printf("µHTTP daemon listening on port %d... Press CTRL-C to stop it.\n", HTTP_PORT);
+    }
+
+    (void)getc(stdin);
+    MHD_stop_daemon(http_server);
+}
