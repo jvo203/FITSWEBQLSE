d581e3bf699f16c5503ae27f9c1aaf9206f7772a
jvo203 <chris.zapart@nao.ac.jp>
2021-01-05 16:08:41 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -1,1 +1,8 @@
 #include <microhttpd.h>
+
+struct MHD_Daemon *http_server = NULL;
+
+static int on_http_connection(void *cls, struct MHD_Connection *connection,
+                              const char *url, const char *method,
+                              const char *version, const char *upload_data,
+                              size_t *upload_data_size, void **con_cls);
