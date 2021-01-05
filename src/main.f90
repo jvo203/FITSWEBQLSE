839e8cc254f0d07d50cd7134b1337a5bd5ca167b
jvo203 <chris.zapart@nao.ac.jp>
2021-01-05 15:46:15 +0900

    modified:   src/main.f90

--- src/main.f90
+++ src/main.f90
@@ -2,6 +2,6 @@ program main
     use, intrinsic :: iso_c_binding
     implicit none
 
-    print *, 'FITSWEBQL SE (SUPERCOMPUTER EDITION)'
+    print *, 'FITSWEBQL SE CLUSTER EDITION'
     print *, 'POWERED BY FORTRAN 2018'
 end program main
