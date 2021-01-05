6d1d646759327fc3efe694df508d381f611ce562
jvo203 <chris.zapart@nao.ac.jp>
2021-01-05 17:47:47 +0900

    modified:   src/main.f90

--- src/main.f90
+++ src/main.f90
@@ -1,7 +1,11 @@
 program main
     use, intrinsic :: iso_c_binding
     implicit none
+    integer(c_int) :: status
 
     print *, 'FITSWEBQL SE CLUSTER EDITION'
     print *, 'POWERED BY FORTRAN 2018'
+
+    ! start an external libmicrohttpd server
+    call start_http(status)
 end program main
