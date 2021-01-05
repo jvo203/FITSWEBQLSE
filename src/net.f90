4138ce13ea5254195d1a48ccb923d79f986ae7e5
jvo203 <chris.zapart@nao.ac.jp>
2021-01-05 17:42:27 +0900

    modified:   src/net.f90

--- src/net.f90
+++ src/net.f90
@@ -1,29 +1,8 @@
 module net
     interface
-        subroutine cvtf32to16(src, dst, size) BIND(C, name='cvtf32to16')
+        integer(C_INT) function start_http() BIND(C, name='start_http')
             use, intrinsic :: ISO_C_BINDING
             implicit none
-            type(C_PTR), value :: src
-            type(C_PTR), value :: dst
-            integer(C_INT), value :: size
-        end subroutine cvtf32to16
-
-        subroutine cvtf32to16minmax(src, dst, dmin, dmax, size) BIND(C, name='cvtf32to16minmax')
-            use, intrinsic :: ISO_C_BINDING
-            implicit none
-            type(C_PTR), value :: src
-            type(C_PTR), value :: dst
-            real(C_FLOAT) :: dmin
-            real(C_FLOAT) :: dmax
-            integer(C_INT), value :: size
-        end subroutine cvtf32to16minmax
-
-        subroutine cvtf16to32(src, dst, size) BIND(C, name='cvtf16to32')
-            use, intrinsic :: ISO_C_BINDING
-            implicit none
-            type(C_PTR), value :: src
-            type(C_PTR), value :: dst
-            integer(C_INT), value :: size
-        end subroutine cvtf16to32
+        end function start_http
     end interface
 end module net
