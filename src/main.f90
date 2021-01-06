4b3fdf2fd84363a7159e187c8880f0766c5b4be2
jvo203 <chris.zapart@nao.ac.jp>
2021-01-06 14:00:51 +0900

    modified:   src/main.f90

--- src/main.f90
+++ src/main.f90
@@ -1,7 +1,18 @@
 program main
     use, intrinsic :: iso_c_binding
+    use mpi
     implicit none
+
     character(len=1) :: c
+
+    integer rank, size, ierror, tag, status(MPI_STATUS_SIZE)
+
+    ! call MPI_INIT(ierror)
+    call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
+    call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror)
+    if (this_image() == 1) then
+        print *, 'image', this_image(), 'node', rank, '/', size
+    end if
 
     print *, 'FITSWEBQL SE CLUSTER EDITION'
     print *, 'POWERED BY FORTRAN 2018'
@@ -11,6 +22,7 @@ program main
 
     read *, c
 
+    ! call MPI_FINALIZE(ierror)
 end program main
 
 subroutine quit_fortran
