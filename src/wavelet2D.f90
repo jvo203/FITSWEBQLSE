a1934246fbfa542e8176e8715c50d6a65f0f9c81
jvo203 <chris.zapart@nao.ac.jp>
2021-01-07 11:54:37 +0900

    modified:   src/wavelet2D.f90

--- src/wavelet2D.f90
+++ src/wavelet2D.f90
@@ -1,3 +1,5 @@
+subroutine daub4_transform(n, x, y)
+
 !*****************************************************************************80
 !
 !! DAUB4_TRANSFORM computes the DAUB4 transform of a vector.
@@ -23,61 +25,61 @@
 !
 !    Output, real ( kind = 4 ) Y(N), the transformed vector.
 !
-  implicit none
+    implicit none
 
-  integer(kind=4) n
-  integer(kind=4), parameter :: p = 3
+    integer(kind=4) n
+    integer(kind=4), parameter :: p = 3
 
-  real(kind=4), dimension(0:p) :: c = (/ &
-                                  0.4829629131445341D+00, &
-                                  0.8365163037378079D+00, &
-                                  0.2241438680420133D+00, &
-                                  -0.1294095225512603D+00/)
-  integer(kind=4) i
-  integer(kind=4) i4_wrap
-  integer(kind=4) j
-  integer(kind=4) j0
-  integer(kind=4) j1
-  integer(kind=4) j2
-  integer(kind=4) j3
-  integer(kind=4) m
-  real(kind=4) x(n)
-  real(kind=4) y(n)
-  real(kind=4) z(n)
+    real(kind=4), dimension(0:p) :: c = (/ &
+                                    0.4829629131445341E+00, &
+                                    0.8365163037378079E+00, &
+                                    0.2241438680420133E+00, &
+                                    -0.1294095225512603E+00/)
+    integer(kind=4) i
+    integer(kind=4) i4_wrap
+    integer(kind=4) j
+    integer(kind=4) j0
+    integer(kind=4) j1
+    integer(kind=4) j2
+    integer(kind=4) j3
+    integer(kind=4) m
+    real(kind=4) x(n)
+    real(kind=4) y(n)
+    real(kind=4) z(n)
 
-  y(1:n) = x(1:n)
-  z(1:n) = 0.0D+00
+    y(1:n) = x(1:n)
+    z(1:n) = 0.0D+00
 
-  m = n
+    m = n
 
-  do while (4 <= m)
+    do while (4 <= m)
 
-      i = 1
+        i = 1
 
-      do j = 1, m - 1, 2
+        do j = 1, m - 1, 2
 
-          j0 = i4_wrap(j, 1, m)
-          j1 = i4_wrap(j + 1, 1, m)
-          j2 = i4_wrap(j + 2, 1, m)
-          j3 = i4_wrap(j + 3, 1, m)
+            j0 = i4_wrap(j, 1, m)
+            j1 = i4_wrap(j + 1, 1, m)
+            j2 = i4_wrap(j + 2, 1, m)
+            j3 = i4_wrap(j + 3, 1, m)
 
-          z(i) = c(0)*y(j0) + c(1)*y(j1) &
-                 + c(2)*y(j2) + c(3)*y(j3)
+            z(i) = c(0)*y(j0) + c(1)*y(j1) &
+                   + c(2)*y(j2) + c(3)*y(j3)
 
-          z(i + m/2) = c(3)*y(j0) - c(2)*y(j1) &
-                       + c(1)*y(j2) - c(0)*y(j3)
+            z(i + m/2) = c(3)*y(j0) - c(2)*y(j1) &
+                         + c(1)*y(j2) - c(0)*y(j3)
 
-          i = i + 1
+            i = i + 1
 
-      end do
+        end do
 
-      y(1:m) = z(1:m)
+        y(1:m) = z(1:m)
 
-      m = m/2
+        m = m/2
 
-  end do
+    end do
 
-  return
+    return
 end
 subroutine daub4_transform_inverse(n, y, x)
 
@@ -112,10 +114,10 @@ subroutine daub4_transform_inverse(n, y, x)
     integer(kind=4), parameter :: p = 3
 
     real(kind=4), dimension(0:p) :: c = (/ &
-                                    0.4829629131445341D+00, &
-                                    0.8365163037378079D+00, &
-                                    0.2241438680420133D+00, &
-                                    -0.1294095225512603D+00/)
+                                    0.4829629131445341E+00, &
+                                    0.8365163037378079E+00, &
+                                    0.2241438680420133E+00, &
+                                    -0.1294095225512603E+00/)
     integer(kind=4) i
     integer(kind=4) i0
     integer(kind=4) i1
@@ -160,6 +162,185 @@ subroutine daub4_transform_inverse(n, y, x)
         m = m*2
 
     end do
+
+    return
+end
+function i4_modp(i, j)
+
+    !*****************************************************************************80
+    !
+    !! I4_MODP returns the nonnegative remainder of I4 division.
+    !
+    !  Discussion:
+    !
+    !    If
+    !      NREM = I4_MODP ( I, J )
+    !      NMULT = ( I - NREM ) / J
+    !    then
+    !      I = J * NMULT + NREM
+    !    where NREM is always nonnegative.
+    !
+    !    The MOD function computes a result with the same sign as the
+    !    quantity being divided.  Thus, suppose you had an angle A,
+    !    and you wanted to ensure that it was between 0 and 360.
+    !    Then mod(A,360) would do, if A was positive, but if A
+    !    was negative, your result would be between -360 and 0.
+    !
+    !    On the other hand, I4_MODP(A,360) is between 0 and 360, always.
+    !
+    !    An I4 is an integer ( kind = 4 ) value.
+    !
+    !  Example:
+    !
+    !        I     J     MOD I4_MODP    Factorization
+    !
+    !      107    50       7       7    107 =  2 *  50 + 7
+    !      107   -50       7       7    107 = -2 * -50 + 7
+    !     -107    50      -7      43   -107 = -3 *  50 + 43
+    !     -107   -50      -7      43   -107 =  3 * -50 + 43
+    !
+    !  Licensing:
+    !
+    !    This code is distributed under the GNU LGPL license.
+    !
+    !  Modified:
+    !
+    !    02 March 1999
+    !
+    !  Author:
+    !
+    !    John Burkardt
+    !
+    !  Parameters:
+    !
+    !    Input, integer ( kind = 4 ) I, the number to be divided.
+    !
+    !    Input, integer ( kind = 4 ) J, the number that divides I.
+    !
+    !    Output, integer ( kind = 4 ) I4_MODP, the nonnegative remainder when I is
+    !    divided by J.
+    !
+    implicit none
+
+    integer(kind=4) i
+    integer(kind=4) i4_modp
+    integer(kind=4) j
+    integer(kind=4) value
+
+    if (j == 0) then
+        write (*, '(a)') ' '
+        write (*, '(a)') 'I4_MODP - Fatal error!'
+        write (*, '(a,i8)') '  Illegal divisor J = ', j
+        stop
+    end if
+
+    value = mod(i, j)
+
+    if (value < 0) then
+        value = value + abs(j)
+    end if
+
+    i4_modp = value
+
+    return
+end
+function i4_wrap(ival, ilo, ihi)
+
+    !*****************************************************************************80
+    !
+    !! I4_WRAP forces an I4 to lie between given limits by wrapping.
+    !
+    !  Discussion:
+    !
+    !    An I4 is an integer ( kind = 4 ) value.
+    !
+    !    There appears to be a bug in the GFORTRAN compiler which can lead to
+    !    erroneous results when the first argument of I4_WRAP is an expression.
+    !    In particular:
+    !
+    !    do i = 1, 3
+    !      if ( test ) then
+    !        i4 = i4_wrap ( i + 1, 1, 3 )
+    !      end if
+    !    end do
+    !
+    !    was, when I = 3, returning I4 = 3.  So I had to replace this with
+    !
+    !    do i = 1, 3
+    !      if ( test ) then
+    !        i4 = i + 1
+    !        i4 = i4_wrap ( i4, 1, 3 )
+    !      end if
+    !    end do
+    !
+    !  Example:
+    !
+    !    ILO = 4, IHI = 8
+    !
+    !    I  Value
+    !
+    !    -2     8
+    !    -1     4
+    !     0     5
+    !     1     6
+    !     2     7
+    !     3     8
+    !     4     4
+    !     5     5
+    !     6     6
+    !     7     7
+    !     8     8
+    !     9     4
+    !    10     5
+    !    11     6
+    !    12     7
+    !    13     8
+    !    14     4
+    !
+    !  Licensing:
+    !
+    !    This code is distributed under the GNU LGPL license.
+    !
+    !  Modified:
+    !
+    !    07 September 2009
+    !
+    !  Author:
+    !
+    !    John Burkardt
+    !
+    !  Parameters:
+    !
+    !    Input, integer ( kind = 4 ) IVAL, a value.
+    !
+    !    Input, integer ( kind = 4 ) ILO, IHI, the desired bounds.
+    !
+    !    Output, integer ( kind = 4 ) I4_WRAP, a "wrapped" version of the value.
+    !
+    implicit none
+
+    integer(kind=4) i4_modp
+    integer(kind=4) i4_wrap
+    integer(kind=4) ihi
+    integer(kind=4) ilo
+    integer(kind=4) ival
+    integer(kind=4) jhi
+    integer(kind=4) jlo
+    integer(kind=4) value
+    integer(kind=4) wide
+
+    jlo = min(ilo, ihi)
+    jhi = max(ilo, ihi)
+
+    wide = jhi - jlo + 1
+
+    if (wide == 1) then
+        value = jlo
+    else
+        value = jlo + i4_modp(ival - jlo, wide)
+    end if
+
+    i4_wrap = value
 
     return
 end
