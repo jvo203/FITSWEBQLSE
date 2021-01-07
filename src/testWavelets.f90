9f736fe18d2cfe2887e0c9338daa9e6087614c41
jvo203 <chris.zapart@nao.ac.jp>
2021-01-07 15:51:26 +0900

    modified:   src/testWavelets.f90

--- src/testWavelets.f90
+++ src/testWavelets.f90
@@ -1,7 +1,7 @@
 program Wavelets
     implicit none
 
-    integer(kind=4), parameter :: N = 8
+    integer(kind=4), parameter :: N = 4
     integer i, j
 
     real(kind=4), dimension(N, N) :: x
@@ -13,6 +13,13 @@ program Wavelets
         end do
     end do
 
-    print *, 'BEFORE', x
+    print *, 'BEFORE'
+    print *, x
+
+    ! a forward 2D wavelet transform
+    call daub4_transform2D(N, x, y)
+
+    print *, 'AFTER'
+    print *, y
 
 end program Wavelets
