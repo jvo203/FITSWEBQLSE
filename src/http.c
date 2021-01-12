a25a17f1b3c8e2503057cd917bc11886d51bc981
jvo203 <chris.zapart@nao.ac.jp>
2021-01-12 16:53:28 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -36,6 +36,69 @@ static const lws_retry_bo_t retry = {
 	.secs_since_valid_hangup = 10,
 };
 
+static const struct lws_http_mount mount = {
+	/* .mount_next */		NULL,		/* linked-list "next" */
+	/* .mountpoint */		"/",		/* mountpoint URL */
+	/* .origin */			"./mount-origin",  /* serve from dir */
+	/* .def */			"index.html",	/* default filename */
+	/* .protocol */			NULL,
+	/* .cgienv */			NULL,
+	/* .extra_mimetypes */		NULL,
+	/* .interpret */		NULL,
+	/* .cgi_timeout */		0,
+	/* .cache_max_age */		0,
+	/* .auth_mask */		0,
+	/* .cache_reusable */		0,
+	/* .cache_revalidate */		0,
+	/* .cache_intermediaries */	0,
+	/* .origin_protocol */		LWSMPRO_FILE,	/* files in a dir */
+	/* .mountpoint_len */		1,		/* char count */
+	/* .basic_auth_login_file */	NULL,
+};
+
+void start_ws()
+{
+	struct lws_context_creation_info info;
+	struct lws_context *context;
+	const char *p;
+	int n = 0, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
+			/* for LLL_ verbosity above NOTICE to be built into lws,
+			 * lws must have been configured and built with
+			 * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
+			/* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
+			/* | LLL_EXT */ /* | LLL_CLIENT */ /* | LLL_LATENCY */
+			/* | LLL_DEBUG */;	
+
+	lws_set_log_level(logs, NULL);
+	lwsl_user("starting LWS ws server\n");
+
+	memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
+	info.port = WS_PORT;
+	info.mounts = &mount;
+	info.protocols = protocols;
+	info.vhost_name = "localhost";
+	info.options =
+		LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+
+    // assume default websocket timeouts
+	// info.retry_and_idle_policy = &retry;
+
+	context = lws_create_context(&info);
+	if (!context) {
+		lwsl_err("lws init failed\n");
+		return ;
+	}
+
+	while (n >= 0 && !interrupted)
+		n = lws_service(context, 0);
+
+	lws_context_destroy(context);
+
+    lwsl_user("LWS ws server stopped\n");
+
+	return ;
+}
+
 // HTML
 #define PAGE "<html><head><title>FITSWEBQL SE</title>" \
              "</head><body>FITSWEBQLSE (libmicrohttpd)</body></html>"
@@ -99,6 +162,8 @@ void SIGINTHandler(int sigint)
 {
     printf("\tCTRL-C detected. Exiting the C part.\n");    
 
+    interrupted = 1; // this will terminate websockets
+
     if (http_server != NULL)
     {
         printf("shutting down the HTTP server... ");
@@ -107,9 +172,7 @@ void SIGINTHandler(int sigint)
 
         //stop accepting new connections
         //MHD_quiesce_daemon(http_server);
-    };
-
-    interrupted = 1; // this will terminate websockets
+    };    
 
     printf("clean shutdown completed.\n");
 
