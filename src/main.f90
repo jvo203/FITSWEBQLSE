9765088441f9bad8aa83503507ee7535e53fbd45
jvo203 <chris.zapart@nao.ac.jp>
2021-01-28 16:20:25 +0900

    modified:   src/main.f90

--- src/main.f90
+++ src/main.f90
@@ -29,7 +29,7 @@ program main
     call MPI_GET_PROCESSOR_NAME(name, namelen, ierror)
 
     print *, 'co-array image:', this_image(), 'mpi rank:', rank, &
-    &'mpi world size:', size, 'running on', trim(name)
+    &'mpi world size:', size, 'running on ', trim(name)
     ! , 'with', max_threads, 'OpenMP thread(s).'
 
     print *, 'FITSWEBQLSE CLUSTER EDITION POWERED BY FORTRAN 2018'
