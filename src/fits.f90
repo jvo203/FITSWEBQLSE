3063df5f974683facab9ba4e0035394faf597f13
jvo203 <chris.zapart@nao.ac.jp>
2021-01-29 11:58:08 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -833,6 +833,40 @@ contains
         if (j + 1 < last) call quicksort(a, j + 1, last)
     end subroutine quicksort
 
+    ! quicksort.f -*-f90-*-
+    ! Author: t-nissie, some tweaks by 1AdAstra1
+    ! License: GPLv3
+    ! Gist: https://gist.github.com/t-nissie/479f0f16966925fa29ea
+    !!
+    recursive subroutine vec_quicksort(a)
+        implicit none
+        real :: a(:)
+        real x, t
+        integer :: first = 1, last
+        integer i, j
+
+        last = size(a, 1)
+        x = a((first + last)/2)
+        i = first
+        j = last
+
+        do
+            do while (a(i) < x)
+                i = i + 1
+            end do
+            do while (x < a(j))
+                j = j - 1
+            end do
+            if (i >= j) exit
+            t = a(i); a(i) = a(j); a(j) = t
+            i = i + 1
+            j = j - 1
+        end do
+
+        if (first < i - 1) call vec_quicksort(a(first:i - 1))
+        if (j + 1 < last) call vec_quicksort(a(j + 1:last))
+    end subroutine vec_quicksort
+
     ! --------------------------------------------------------------------
     ! REAL FUNCTION  Median() :
     !    This function receives an array X of N entries, sorts it
@@ -846,7 +880,8 @@ contains
         INTEGER, INTENT(IN)                :: N
         INTEGER                            :: i
 
-        CALL quicksort(X, 1, N)               ! sort the original data
+        ! CALL quicksort(X, 1, N)               ! sort the original data
+        CALL vec_quicksort(X)               ! sort the original data
 
         IF (MOD(N, 2) == 0) THEN           ! compute the median
             Median = (X(N/2) + X(N/2 + 1))/2.0
