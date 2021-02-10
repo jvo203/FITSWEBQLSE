b1a9f01e014ed6ba0590fead43134b7e74b479df
jvo203 <chris.zapart@nao.ac.jp>
2021-02-10 15:04:20 +0900

    modified:   src/lz4.f90

--- src/lz4.f90
+++ src/lz4.f90
@@ -7,4 +7,10 @@ module lz4
     end interface
 contains
 ! Fortran entry subroutines
+    subroutine compress_mask(n, mask)
+        implicit none
+
+        integer(kind=4) :: n
+        logical(kind=1), dimension(n, n), optional, intent(in) :: mask
+    end subroutine compress_mask
 end module lz4
