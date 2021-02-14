54533a872c172e3a616dbe93a979a8d97fca326d
jvo203 <chris.zapart@nao.ac.jp>
2021-02-14 10:51:39 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -1240,7 +1240,7 @@ static enum MHD_Result execute_alma(struct MHD_Connection *connection, char **va
     return ret;
 }
 
-extern void write_image_spectrum(int fd, const char *flux, int width, int height, float *pixels, char *mask)
+extern void write_image_spectrum(int fd, const char *flux, int width, int height, const float *pixels, const char *mask)
 {
     if (flux == NULL)
         return;
