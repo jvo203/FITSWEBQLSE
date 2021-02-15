963a1e60cb88622f859a2c87a1ad700e9c400165
jvo203 <chris.zapart@nao.ac.jp>
2021-02-15 12:27:44 +0900

    modified:   src/zfp.f90

--- src/zfp.f90
+++ src/zfp.f90
@@ -3,4 +3,28 @@ module zfp
     use, intrinsic :: iso_c_binding
     implicit none
 
+contains
+! Fortran convenience subroutines
+
+    subroutine compress_pixels(pixels, compressed)
+        use, intrinsic :: iso_c_binding
+        implicit none
+
+        ! inputs
+        real(kind=4), dimension(:, :), contiguous, target, intent(in) :: pixels
+
+        ! the output
+        character(kind=c_char), allocatable, intent(out) :: compressed(:)
+
+        ! internal variables
+        integer :: nx, ny
+        character(kind=c_char), allocatable, target :: buffer(:)
+
+        nx = ubound(pixels, 1)
+        ny = ubound(pixels, 2)
+
+        print *, 'nx = ', nx, ', ny = ', ny
+
+    end subroutine compress_pixels
+
 end module zfp
