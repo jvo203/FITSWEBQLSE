600db63f97d6dedf938a902a93ff5afac00ff108
jvo203 <chris.zapart@nao.ac.jp>
2021-02-24 16:29:49 +0900

    modified:   src/json_for.f90

--- src/json_for.f90
+++ src/json_for.f90
@@ -6,8 +6,22 @@ contains
     subroutine json_add_string(json, key, val)
         type(varying_string), intent(inout) :: json
         character(len=*), intent(in) :: key, val
+        integer :: i, str_len
+        character :: c
 
-        json = json//'"'//key//'":"'//val//'",'
+        json = json//'"'//key//'" : "'
+
+        str_len = len(val)
+
+        ! go through the string character by character
+        ! escaping special characters
+
+        do i = 1, str_len
+            c = val(i:i)
+            json = json//c
+        end do
+
+        json = json//'",'
 
     end subroutine json_add_string
 end module json_for
