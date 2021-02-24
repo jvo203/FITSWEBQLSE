1ed0d0fa8846df3054395b30f16b148dd1d092c0
jvo203 <chris.zapart@nao.ac.jp>
2021-02-24 16:39:33 +0900

    modified:   src/json_for.f90

--- src/json_for.f90
+++ src/json_for.f90
@@ -6,6 +6,7 @@ contains
     subroutine json_add_string(json, key, val)
         type(varying_string), intent(inout) :: json
         character(len=*), intent(in) :: key, val
+
         integer :: i, str_len
         character :: c
 
@@ -13,11 +14,56 @@ contains
 
         str_len = len(val)
 
-        ! go through the string character by character
-        ! escaping special characters
-
+        ! go through the string escaping special characters
         do i = 1, str_len
             c = val(i:i)
+
+            ! test for escape characters
+
+            ! backspace
+            if (c .eq. char(8)) then
+                json = json//'\b'
+                cycle
+            end if
+
+            ! form feed
+            if (c .eq. char(12)) then
+                json = json//'\f'
+                cycle
+            end if
+
+            ! new line (line feed)
+            if (c .eq. char(10)) then
+                json = json//'\n'
+                cycle
+            end if
+
+            ! carriage return
+            if (c .eq. char(13)) then
+                json = json//'\r'
+                cycle
+            end if
+
+            ! horizontal tab
+            if (c .eq. char(9)) then
+                json = json//'\t'
+                cycle
+            end if
+
+            ! double quote
+            if (c .eq. '"') then
+                json = json//'\"'
+                cycle
+            end if
+
+            ! backslash
+            if (c .eq. '\') then
+                json = json//'\\'
+                cycle
+            end if
+
+            ! OK, we got through the escape characters
+            ! append the actual value
             json = json//c
         end do
 
