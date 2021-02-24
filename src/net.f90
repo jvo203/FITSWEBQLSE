268aa1d3b0ef9814b2534e3a054f107e01b2503a
jvo203 <chris.zapart@nao.ac.jp>
2021-02-24 11:59:13 +0900

    modified:   src/net.f90

--- src/net.f90
+++ src/net.f90
@@ -148,6 +148,8 @@ contains
 
         ! compare the datasetId with item%frameid
         ! if (.not. compare_frameid(trim(item%frameid), datasetId, int(n))) then
+
+        ! item%frameid replaced by item%datasetid
         if (.not. compare_frameid(char(item%datasetid), datasetId, int(n))) then
             print *, 'dataset ids do not match: (', datasetId, ') .ne. (', char(item%datasetid), ')'
             return
