83f2c2a93394482ff6236f251bc38ef8f9f6126c
jvo203 <chris.zapart@nao.ac.jp>
2021-02-26 17:48:34 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -11,7 +11,7 @@
  * but using an lws_ring ringbuffer to hold up to 8 messages at a time.
  */
 
-#if !defined (LWS_PLUGIN_STATIC)
+#if !defined(LWS_PLUGIN_STATIC)
 #define LWS_DLL
 #define LWS_INTERNAL
 #include <libwebsockets.h>
@@ -21,14 +21,16 @@
 
 /* one of these created for each message */
 
-struct msg {
+struct msg
+{
 	void *payload; /* is malloc'd */
 	size_t len;
 };
 
 /* one of these is created for each client connecting to us */
 
-struct per_session_data__minimal {
+struct per_session_data__minimal
+{
 	struct per_session_data__minimal *pss_list;
 	struct lws *wsi;
 	int last; /* the last message number we sent */
@@ -36,7 +38,8 @@ struct per_session_data__minimal {
 
 /* one of these is created for each vhost our protocol is used with */
 
-struct per_vhost_data__minimal {
+struct per_vhost_data__minimal
+{
 	struct lws_context *context;
 	struct lws_vhost *vhost;
 	const struct lws_protocols *protocol;
@@ -44,7 +47,7 @@ struct per_vhost_data__minimal {
 	struct per_session_data__minimal *pss_list; /* linked-list of live pss*/
 
 	struct msg amsg; /* the one pending message... */
-	int current; /* the current message number we are caching */
+	int current;	 /* the current message number we are caching */
 };
 
 /* destroys the message when everyone has had a copy of it */
@@ -61,21 +64,22 @@ __minimal_destroy_message(void *_msg)
 
 static int
 callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
-			void *user, void *in, size_t len)
+				 void *user, void *in, size_t len)
 {
 	struct per_session_data__minimal *pss =
-			(struct per_session_data__minimal *)user;
+		(struct per_session_data__minimal *)user;
 	struct per_vhost_data__minimal *vhd =
-			(struct per_vhost_data__minimal *)
+		(struct per_vhost_data__minimal *)
 			lws_protocol_vh_priv_get(lws_get_vhost(wsi),
-					lws_get_protocol(wsi));
+									 lws_get_protocol(wsi));
 	int m;
 
-	switch (reason) {
+	switch (reason)
+	{
 	case LWS_CALLBACK_PROTOCOL_INIT:
 		vhd = lws_protocol_vh_priv_zalloc(lws_get_vhost(wsi),
-				lws_get_protocol(wsi),
-				sizeof(struct per_vhost_data__minimal));
+										  lws_get_protocol(wsi),
+										  sizeof(struct per_vhost_data__minimal));
 		vhd->context = lws_get_context(wsi);
 		vhd->protocol = lws_get_protocol(wsi);
 		vhd->vhost = lws_get_vhost(wsi);
@@ -91,7 +95,7 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 	case LWS_CALLBACK_CLOSED:
 		/* remove our closing pss from the list of live pss */
 		lws_ll_fwd_remove(struct per_session_data__minimal, pss_list,
-				  pss, vhd->pss_list);
+						  pss, vhd->pss_list);
 		break;
 
 	case LWS_CALLBACK_SERVER_WRITEABLE:
@@ -102,9 +106,9 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 			break;
 
 		/* notice we allowed for LWS_PRE in the payload already */
-		m = lws_write(wsi, ((unsigned char *)vhd->amsg.payload) +
-			      LWS_PRE, vhd->amsg.len, LWS_WRITE_TEXT);
-		if (m < (int)vhd->amsg.len) {
+		m = lws_write(wsi, ((unsigned char *)vhd->amsg.payload) + LWS_PRE, vhd->amsg.len, LWS_WRITE_TEXT);
+		if (m < (int)vhd->amsg.len)
+		{
 			lwsl_err("ERROR %d writing to ws\n", m);
 			return -1;
 		}
@@ -119,7 +123,8 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 		vhd->amsg.len = len;
 		/* notice we over-allocate by LWS_PRE */
 		vhd->amsg.payload = malloc(LWS_PRE + len);
-		if (!vhd->amsg.payload) {
+		if (!vhd->amsg.payload)
+		{
 			lwsl_user("OOM: dropping\n");
 			break;
 		}
@@ -127,14 +132,24 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 		memcpy((char *)vhd->amsg.payload + LWS_PRE, in, len);
 		vhd->current++;
 
+		char *msg = malloc(vhd->amsg.len);
+		if (msg != NULL)
+		{
+			lws_strncpy(msg, vhd->amsg.payload, vhd->amsg.len);
+			printf("[C::ws] %s\n", msg);
+			free(msg);
+		}
+
 		/*
 		 * let everybody know we want to write something on them
 		 * as soon as they are ready
 		 */
 		lws_start_foreach_llp(struct per_session_data__minimal **,
-				      ppss, vhd->pss_list) {
+							  ppss, vhd->pss_list)
+		{
 			lws_callback_on_writable((*ppss)->wsi);
-		} lws_end_foreach_llp(ppss, pss_list);
+		}
+		lws_end_foreach_llp(ppss, pss_list);
 		break;
 
 	default:
@@ -144,11 +159,11 @@ callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
 	return 0;
 }
 
-#define LWS_PLUGIN_PROTOCOL_MINIMAL \
-	{ \
-		"lws-minimal", \
-		callback_minimal, \
-		sizeof(struct per_session_data__minimal), \
-		128, \
-		0, NULL, 0 \
+#define LWS_PLUGIN_PROTOCOL_MINIMAL                   \
+	{                                                 \
+		"lws-minimal",                                \
+			callback_minimal,                         \
+			sizeof(struct per_session_data__minimal), \
+			128,                                      \
+			0, NULL, 0                                \
 	}
