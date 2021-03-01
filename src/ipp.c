ba4079993135e81526dc9b5b6d23430472e62b0c
jvo203 <chris.zapart@nao.ac.jp>
2021-03-01 14:17:35 +0900

    modified:   src/ipp.c

--- src/ipp.c
+++ src/ipp.c
@@ -3,6 +3,78 @@
 
 #include <ipp.h>
 
+IppStatus resizeNearest8u_C1R(Ipp8u *pSrc, IppiSize srcSize, Ipp32s srcStep,
+                              Ipp8u *pDst, IppiSize dstSize, Ipp32s dstStep)
+{
+    IppiResizeSpec_32f *pSpec = 0;
+    int specSize = 0, initSize = 0, bufSize = 0;
+    Ipp8u *pBuffer = 0;
+    Ipp32u numChannels = 1;
+    IppiPoint dstOffset = {0, 0};
+    IppiPoint srcOffset = {0, 0};
+    IppStatus status = ippStsNoErr;
+    IppStatus pStatus;
+
+    /* Spec and init buffer sizes */
+    status = ippiResizeGetSize_8u(srcSize, dstSize, ippNearest, 0, &specSize, &initSize);
+
+    if (status != ippStsNoErr)
+        return status;
+
+    /* Memory allocation */
+    pSpec = (IppiResizeSpec_32f *)ippsMalloc_8u(specSize);
+
+    if (pSpec == NULL)
+    {
+        ippsFree(pSpec);
+        return ippStsNoMemErr;
+    }
+
+    /* Filter initialization */
+    status = ippiResizeNearestInit_8u(srcSize, dstSize, pSpec);
+
+    if (status != ippStsNoErr)
+    {
+        ippsFree(pSpec);
+        return status;
+    }
+
+    /* General transform function */
+    ippiResizeGetBufferSize_8u(pSpec, dstSize, ippC1, &bufSize);
+
+    pBuffer = ippsMalloc_8u(bufSize);
+
+    if (pBuffer)
+    {
+        Ipp8u *pOneBuf;
+
+        pStatus = ippiResizeGetSrcRoi_8u(pSpec, dstOffset, dstSize,
+                                         &srcOffset, &srcSize);
+
+        if (pStatus == ippStsNoErr)
+        {
+            pOneBuf = pBuffer;
+
+            pStatus = ippiResizeNearest_8u_C1R(
+                pSrc, srcStep * sizeof(Ipp32f), pDst, dstStep * sizeof(Ipp32f),
+                dstOffset, dstSize, pSpec, pOneBuf);
+        }
+    }
+
+    ippsFree(pSpec);
+
+    if (pBuffer == NULL)
+        return ippStsNoMemErr;
+
+    ippsFree(pBuffer);
+
+    /* Return bad status */
+    if (pStatus != ippStsNoErr)
+        return pStatus;
+
+    return status;
+}
+
 IppStatus resizeLanczos32f_C1R(Ipp32f *pSrc, IppiSize srcSize, Ipp32s srcStep,
                                Ipp32f *pDst, IppiSize dstSize, Ipp32s dstStep,
                                int numLobes)
