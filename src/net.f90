da3440d9fd5ed446bd769fd1d88ab9f622d62e93
jvo203 <chris.zapart@nao.ac.jp>
2021-03-02 13:28:15 +0900

    modified:   src/net.f90

--- src/net.f90
+++ src/net.f90
@@ -285,9 +285,10 @@ contains
 
             ! FITS header
             if (allocated(item%hdr)) then
-                print *, 'header length:', len(item%hdr)
+                print *, 'header size:', size(item%hdr)
                 ! print *, item%hdr
-                call write_header(fd, item%hdr//c_null_char)
+                ! call write_header(fd, item%hdr//c_null_char)
+                call write_header(fd, 'NULL'//c_null_char)
             else
                 call write_header(fd, 'NULL'//c_null_char)
             end if
