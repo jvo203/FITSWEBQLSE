fdf3762631ceb79cdde92a0ab563026996df71fb
jvo203 <chris.zapart@nao.ac.jp>
2021-03-03 16:59:04 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -1470,7 +1470,8 @@ contains
         call cpu_time(t1)
 
         ! CALL quicksort(X, 1, N)               ! sort the original data
-        CALL vec_quicksort(X)               ! sort the original data
+        ! CALL vec_quicksort(X)               ! sort the original data
+        call psrs_sort(c_loc(X), N)
 
         call cpu_time(t2)
 
