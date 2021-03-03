d3c01506636ef0d18306e8889cac22fe258d9f19
jvo203 <chris.zapart@nao.ac.jp>
2021-03-03 17:00:42 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -1470,8 +1470,8 @@ contains
         call cpu_time(t1)
 
         ! CALL quicksort(X, 1, N)               ! sort the original data
-        ! CALL vec_quicksort(X)               ! sort the original data
-        call psrs_sort(c_loc(X), N)
+        CALL vec_quicksort(X)               ! sort the original data
+        ! call psrs_sort(c_loc(X), N)         ! single-threaded vec_quicksort is faster than parallel psrc_sort !!!
 
         call cpu_time(t2)
 
