2aaf8404c5c4415f861ad0345b51d9cdf4763eb1
jvo203 <chris.zapart@nao.ac.jp>
2021-03-08 10:19:25 +0900

    modified:   src/global.c

--- src/global.c
+++ src/global.c
@@ -1,1 +1,11 @@
 #include "global.h"
+
+void init_hash_table()
+{
+    datasets = g_hash_table_new(g_str_hash, g_str_equal);
+}
+
+void delete_hash_table()
+{
+    g_hash_table_destroy(datasets);
+}
