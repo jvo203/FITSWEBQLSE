375615d14ab22f756fdc60f22627c734b9f48fe8
jvo203 <chris.zapart@nao.ac.jp>
2021-03-08 10:56:21 +0900

    modified:   src/global.h

--- src/global.h
+++ src/global.h
@@ -3,3 +3,9 @@
 
 static GMutex *datasets_mtx;
 static GHashTable *datasets;
+
+void init_hash_table();
+void delete_hash_table();
+void insert_item(const char *datasetid, void *item);
+void insert_item_with_replace(const char *datasetid, void *item);
+bool item_exists(const char *datasetid);
