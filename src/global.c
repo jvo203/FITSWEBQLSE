7127467d751c5c44cb4f52565c89be1f064c014c
jvo203 <chris.zapart@nao.ac.jp>
2021-03-08 10:28:33 +0900

    modified:   src/global.c

--- src/global.c
+++ src/global.c
@@ -2,15 +2,26 @@
 
 void init_hash_table()
 {
+    g_mutex_init(datasets_mtx);
     datasets = g_hash_table_new(g_str_hash, g_str_equal);
 }
 
 void delete_hash_table()
 {
     g_hash_table_destroy(datasets);
+    g_mutex_clear(datasets_mtx);
 }
 
-void replace_item(const char *datasetid, void *item)
+void insert_item(const char *datasetid, void *item)
 {
+    g_mutex_lock(datasets_mtx);
+    g_hash_table_insert(datasets, datasetid, item);
+    g_mutex_unlock(datasets_mtx);
+}
+
+void insert_item_with_replace(const char *datasetid, void *item)
+{
+    g_mutex_lock(datasets_mtx);
     g_hash_table_replace(datasets, datasetid, item);
+    g_mutex_unlock(datasets_mtx);
 }
