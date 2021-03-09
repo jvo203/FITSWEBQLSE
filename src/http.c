b48bc972e37685fc29890d1d20c1caceb75d2f40
jvo203 <chris.zapart@nao.ac.jp>
2021-03-09 14:29:56 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -713,9 +713,13 @@ static enum MHD_Result on_http_connection(void *cls,
         {
             datasetId++;
 
-            //void *item =
-            float progress = get_progress();
-            float elapsed = get_elapsed();
+            void *item = get_dataset(datasetId);
+
+            if (item == NULL)
+                return http_not_found(connection);
+
+            float progress = get_progress(item);
+            float elapsed = get_elapsed(item);
 
             // printf("[C] [progress] datasetId(%s): %f%% in %f [s]\n", datasetId, progress, elapsed);
 
