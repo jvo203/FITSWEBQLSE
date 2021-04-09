73c96aa03ab3aabb5a5cac905e09563acdd0f0ec
jvo203 <chris.zapart@nao.ac.jp>
2021-04-09 15:12:36 +0900

    modified:   src/json_write.c

--- src/json_write.c
+++ src/json_write.c
@@ -18,8 +18,35 @@ void delete_json(GString *json)
 
 void end_json(GString *json)
 {
-    // remove the 'last ,'
-
     if (json != NULL)
+    {
+        // remove the final ","
+        g_string_truncate(json, json->len - 1);
+
+        // end JSON
         g_string_printf(json, "}");
+    }
+}
+
+void add_json_string(GString *json, char *key, char *val)
+{
+    if (json == NULL)
+        return;
+
+    char *encoded = json_encode_string(val);
+
+    if (encoded != NULL)
+    {
+        g_string_append_printf(json, "\"%s\" : %s,", key, encoded);
+
+        free(encoded);
+    }
+}
+
+void add_json_integer(GString *json, char *key, int val)
+{
+    if (json == NULL)
+        return;
+
+    g_string_append_printf(json, "\"%s\" : %d,", key, val);
 }
