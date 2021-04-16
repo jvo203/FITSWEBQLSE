6a8c5872479618def68c1913ddb53f461adb3cd6
jvo203 <chris.zapart@nao.ac.jp>
2021-04-16 09:25:33 +0900

    modified:   src/scratchpad.f90

--- src/scratchpad.f90
+++ src/scratchpad.f90
@@ -608,6 +608,33 @@ function extract_datasetid_old(filename) result(datasetid)
 
 end function extract_datasetid_old
 
+!======================================================================
+!    IDX$HASH
+! Author:
+! Alan Miller, Retired Scientist (Statistician)
+! CSIRO Mathematical & Information Sciences
+! Alan.Miller -at- vic.cmis.csiro.au
+! http://www.ozemail.com.au/~milleraj
+!======================================================================
+FUNCTION hash(text) RESULT(hashed)
+    IMPLICIT NONE
+    CHARACTER(LEN=*), INTENT(IN) :: text
+    INTEGER                       :: hashed     ! 32-bit integers assumed
+
+    INTEGER, PARAMETER :: magic_numb = Z'5D7A9F43'
+    INTEGER            :: i, j
+
+    hashed = 0                              ! Note: B was not initialized before
+    DO i = 1, LEN_TRIM(text)
+        j = MOD(i - 1, 4)*8
+        hashed = IEOR(hashed, ISHFT(ICHAR(text(i:i)), j))
+    END DO
+
+    hashed = ABS(IEOR(hashed, magic_numb))    ! Why take the absolute value?
+
+    RETURN
+END FUNCTION hash
+
 use :: zmq
 
 ! ØMQ
