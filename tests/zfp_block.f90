84537247c91e937b7633af0fa1e27b718ab05d1d
jvo203 <chris.zapart@nao.ac.jp>
2021-05-03 15:38:17 +0900

    modified:   tests/zfp_block.f90

--- tests/zfp_block.f90
+++ tests/zfp_block.f90
@@ -2,6 +2,8 @@ program main
     use, intrinsic :: iso_c_binding
 
     real(kind=4), dimension(4, 4) :: x
+    integer, dimension(4, 4) :: e
+    integer :: max_exp
 
     ! loop counters
     integer i, j
@@ -13,4 +15,11 @@ program main
     end do
 
     print *, x
+
+    e = exponent(x)
+    max_exp = maxval(e)
+
+    print *, 'e:', e
+    print *, 'max_exp:', max_exp
+
 end program main
