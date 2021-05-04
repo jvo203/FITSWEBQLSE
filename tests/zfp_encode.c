ac0cc986be5d16de03833f9c30cf64494619c336
jvo203 <chris.zapart@nao.ac.jp>
2021-05-04 15:30:56 +0900

    modified:   tests/zfp_encode.c

--- tests/zfp_encode.c
+++ tests/zfp_encode.c
@@ -5,19 +5,24 @@
 #define ZFP_MAX_PREC 64
 #define ZFP_MIN_EXP -1074
 
+#define EBITS 8                        /* single-precision floating-point */
+#define EBIAS ((1 << (EBITS - 1)) - 1) /* exponent bias */
+
+#define BLOCK_SIZE 16
+
 int main()
 {
     int i, j;
     int offset;
 
-    float fblock[16];
+    float fblock[BLOCK_SIZE];
 
     offset = 0;
     for (i = 0; i < 4; i++)
         for (j = 0; j < 4; j++)
             fblock[offset++] = (i + 1) * (j + 1);
 
-    for (i = 0; i < 16; i++)
+    for (i = 0; i < BLOCK_SIZE; i++)
         printf("%f\t", fblock[i]);
     printf("\n");
 }
