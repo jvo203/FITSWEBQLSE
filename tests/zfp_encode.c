51ad7c6eb77dda48d004d3f5c260221fad34a5cf
jvo203 <chris.zapart@nao.ac.jp>
2021-05-04 15:45:17 +0900

    modified:   tests/zfp_encode.c

--- tests/zfp_encode.c
+++ tests/zfp_encode.c
@@ -5,10 +5,41 @@
 #define ZFP_MAX_PREC 64
 #define ZFP_MIN_EXP -1074
 
+#define DIMS 2
 #define EBITS 8                        /* single-precision floating-point */
 #define EBIAS ((1 << (EBITS - 1)) - 1) /* exponent bias */
 
 #define BLOCK_SIZE 16
+
+#define FREXP(x, e) frexpf(x, e)
+#define FABS(x) fabsf(x)
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+int exponent(float x)
+{
+    if (x > 0)
+    {
+        int e;
+        FREXP(x, &e);
+        /* clamp exponent in case x is denormal */
+        return MAX(e, 1 - EBIAS);
+    }
+    return -EBIAS;
+}
+
+int exponent_block(const float *p, unsigned int n)
+{
+    float max = 0;
+    do
+    {
+        float f = FABS(*p++);
+        if (max < f)
+            max = f;
+    } while (--n);
+    return exponent(max);
+}
 
 int main()
 {
@@ -25,4 +56,15 @@ int main()
     for (i = 0; i < BLOCK_SIZE; i++)
         printf("%f\t", fblock[i]);
     printf("\n");
+
+    int maxbits = 8;
+    int rate = maxbits / BLOCK_SIZE;
+    int minbits = maxbits;
+    int minexp = ZFP_MIN_EXP;
+
+    unsigned int bits = 1;
+    /* compute maximum exponent */
+    int emax = exponent_block(fblock, BLOCK_SIZE);
+    int maxprec = precision(emax, ZFP_MAX_PREC, minexp, DIMS);
+    unsigned int e = maxprec ? emax + EBIAS : 0;
 }
