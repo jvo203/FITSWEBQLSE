8fcc4fa473b89af32fa1069991c5f20fcbd2912b
jvo203 <chris.zapart@nao.ac.jp>
2021-05-07 17:19:55 +0900

    modified:   tests/zfp_encode.c

--- tests/zfp_encode.c
+++ tests/zfp_encode.c
@@ -70,6 +70,39 @@ static const uchar perm_2[16] = {
 
 #undef index
 
+#define index(j, i) ((i) + 4 * (j))
+
+/* order coefficients (i, j) by i + j, then i^2 + j^2 */
+static const uchar perm_f[16] = {
+    index(0, 0), /*  0 : 0 */
+
+    index(1, 0), /*  1 : 1 */
+    index(0, 1), /*  2 : 1 */
+
+    index(1, 1), /*  3 : 2 */
+
+    index(2, 0), /*  4 : 2 */
+    index(0, 2), /*  5 : 2 */
+
+    index(2, 1), /*  6 : 3 */
+    index(1, 2), /*  7 : 3 */
+
+    index(3, 0), /*  8 : 3 */
+    index(0, 3), /*  9 : 3 */
+
+    index(2, 2), /* 10 : 4 */
+
+    index(3, 1), /* 11 : 4 */
+    index(1, 3), /* 12 : 4 */
+
+    index(3, 2), /* 13 : 5 */
+    index(2, 3), /* 14 : 5 */
+
+    index(3, 3), /* 15 : 6 */
+};
+
+#undef index
+
 uint precision(int maxexp, uint maxprec, int minexp, int dims)
 {
     return MIN(maxprec, (uint)MAX(0, maxexp - minexp + 2 * (dims + 1)));
@@ -660,7 +693,13 @@ int main()
         printf("%f\t", fblock[i]);
     printf("\n");
 
+    printf("perm_2:\n");
     for (i = 0; i < 16; i++)
         printf("%u ", perm_2[i]);
+    printf("\n");
+
+    printf("perm_f:\n");
+    for (i = 0; i < 16; i++)
+        printf("%u ", perm_f[i]);
     printf("\n");
 }
