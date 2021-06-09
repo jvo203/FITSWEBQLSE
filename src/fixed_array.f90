9ab35ea0185e361acb858b9afdd84789011fb803
jvo203 <chris.zapart@nao.ac.jp>
2021-06-09 11:57:44 +0900

    modified:   src/fixed_array.f90

--- src/fixed_array.f90
+++ src/fixed_array.f90
@@ -92,7 +92,7 @@ contains
                 input(1:x2 - x1 + 1, 1:y2 - y1 + 1) = x(x1:x2, y1:y2)
 
                 ! pre-condition the input array
-                ! input = log(0.5 + (input - pmin)/(pmax - pmin))
+                input = log(0.5 + (input - pmin)/(pmax - pmin))
 
                 call to_fixed_block(input, compressed(i, j), ignrval, datamin, datamax)
             end block
@@ -241,7 +241,7 @@ contains
         end do
 
         ! recover the original range
-        ! x = pmin + (exp(x) - 0.5)*(pmax - pmin)
+        x = pmin + (exp(x) - 0.5)*(pmax - pmin)
 
     end subroutine from_fixed_block
 
