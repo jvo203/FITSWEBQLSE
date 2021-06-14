c1f9a9ddef2ab1a8ce18dfd1261d10ee336f1392
jvo203 <chris.zapart@nao.ac.jp>
2021-06-14 11:59:14 +0900

    modified:   Julia/fitswebqlse.jl

--- Julia/fitswebqlse.jl
+++ Julia/fitswebqlse.jl
@@ -255,6 +255,9 @@ function serveFITS(request::HTTP.Request)
     write(resp, "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\" integrity=\"sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd\" crossorigin=\"anonymous\"></script>")
 
     # GLSL vertex shader
+    write(resp, "<script id=\"vertex-shader\" type=\"x-shader/x-vertex\">\n")
+    write(resp, read(HT_DOCS * "/fitswebql/vertex-shader.vert"))
+    write(resp, "</script>\n")
 
     try
         return HTTP.Response(200, take!(resp))
