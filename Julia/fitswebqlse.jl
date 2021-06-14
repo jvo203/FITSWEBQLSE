ed7c890a62343f67b53b2a27279d16adce07bbdc
jvo203 <chris.zapart@nao.ac.jp>
2021-06-14 11:50:06 +0900

    modified:   Julia/fitswebqlse.jl

--- Julia/fitswebqlse.jl
+++ Julia/fitswebqlse.jl
@@ -244,8 +244,17 @@ function serveFITS(request::HTTP.Request)
     write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/marchingsquares-isocontours.min.js\"></script>\n")
     write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/marchingsquares-isobands.min.js\"></script>\n")
 
+    # WebAssembly
     write(resp, "<script src=\"client.", WASM_VERSION, ".js\"></script>\n")
     write(resp, "<script>\n", "Module.ready\n", "\t.then(status => console.log(status))\n", "\t.catch(e => console.error(e));\n", "</script>\n")
+
+    # Bootstrap v3.4.1
+    write(resp, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1\">\n")
+    write(resp, "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\" integrity=\"sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu\" crossorigin=\"anonymous\">")
+    write(resp, "<script src=\"https://code.jquery.com/jquery-1.12.4.min.js\" integrity=\"sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ\" crossorigin=\"anonymous\"></script>")
+    write(resp, "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\" integrity=\"sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd\" crossorigin=\"anonymous\"></script>")
+
+    # GLSL vertex shader
 
     try
         return HTTP.Response(200, take!(resp))
