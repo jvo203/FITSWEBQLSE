78c48bd5ff93a56658ed24d75607b49b5a3566ef
jvo203 <chris.zapart@nao.ac.jp>
2021-06-15 11:30:16 +0900

    modified:   Julia/fitswebqlse.jl

--- Julia/fitswebqlse.jl
+++ Julia/fitswebqlse.jl
@@ -10,7 +10,13 @@ const PRODUCTION = false
 const VERSION_MAJOR = 5
 const VERSION_MINOR = 0
 const VERSION_SUB = 0
-const SERVER_STRING = "FITSWEBQLSE v" * string(VERSION_MAJOR) * "." * string(VERSION_MINOR) * "." * string(VERSION_SUB)
+const SERVER_STRING =
+    "FITSWEBQLSE v" *
+    string(VERSION_MAJOR) *
+    "." *
+    string(VERSION_MINOR) *
+    "." *
+    string(VERSION_SUB)
 
 const WASM_VERSION = "21.04.XX.X"
 const VERSION_STRING = "SV2021-06-XX.X-ALPHA"
@@ -28,7 +34,7 @@ function serveFile(path::String)
     pos = findlast("?", path)
 
     if !isnothing(pos)
-        path = SubString(path, 1:(pos[1] - 1))
+        path = SubString(path, 1:(pos[1]-1))
     end
 
     # cache a response
@@ -88,16 +94,16 @@ function serveFile(path::String)
     end
 
     try
-        return isfile(path) ? HTTP.Response(200, headers; body=read(path)) :
+        return isfile(path) ? HTTP.Response(200, headers; body = read(path)) :
                HTTP.Response(404, "$path Not Found.")
     catch e
         return HTTP.Response(404, "Error: $e")
     end
-    end
+end
 
 function serveDirectory(request::HTTP.Request)
     headers = ["Content-Type" => "application/json"]
-    
+
     params = HTTP.queryparams(HTTP.URI(request.target))
 
     dir = ""
@@ -111,8 +117,8 @@ function serveDirectory(request::HTTP.Request)
 
     println("Scanning $dir ...")
 
-    resp = chop(JSON.json(Dict("location" => dir)), tail=1) * ", \"contents\":["
-        
+    resp = chop(JSON.json(Dict("location" => dir)), tail = 1) * ", \"contents\":["
+
     elements = false
 
     foreach(readdir(dir)) do f
@@ -122,7 +128,7 @@ function serveDirectory(request::HTTP.Request)
 
             path = dir * "/" * f
 
-                info = stat(path)
+            info = stat(path)
 
             if isdir(path)
                 dict = Dict(
@@ -154,15 +160,15 @@ function serveDirectory(request::HTTP.Request)
 
         end
     end
-    
+
     if elements
-        resp = chop(resp, tail=1) * "]}"
+        resp = chop(resp, tail = 1) * "]}"
     else
         resp *= "]}"
     end
 
     try
-        return HTTP.Response(200, headers; body=resp)
+        return HTTP.Response(200, headers; body = resp)
     catch e
         return HTTP.Response(404, "Error: $e")
     end
@@ -183,7 +189,11 @@ function serveROOT(request::HTTP.Request)
     path = HT_DOCS * HTTP.unescapeuri(request.target)
 
     if request.target == "/"
-        path *= "local_j.html"
+        if LOCAL_VERSION
+            path *= "local_j.html"
+        else
+            path *= "test.html"
+        end
     end
 
     return serveFile(path)
@@ -192,7 +202,7 @@ end
 # a recursive function (very elegant)
 function get_dataset(prefix::String, params, datasets, idx::Integer)
     try
-        push!(datasets, params[prefix * string(idx)])
+        push!(datasets, params[prefix*string(idx)])
         get_dataset(prefix, params, datasets, idx + 1)
     catch e
         # no more datasets, stop recursion
@@ -202,7 +212,7 @@ end
 
 function serveFITS(request::HTTP.Request)
     root_path = HTTP.URIs.splitpath(request.target)[1]
-    
+
     params = HTTP.queryparams(HTTP.URI(request.target))
 
     println("root path: \"$root_path\"")
@@ -210,21 +220,21 @@ function serveFITS(request::HTTP.Request)
 
     has_fits = false
     is_composite = false
-        
+
     dir = ""
     datasets = []
     ext = ""
-    
+
     try
         ext = params["ext"]
     catch e
     end
-    
+
     try
         dir = params["dir"]
     catch e
     end
-            
+
     try
         if params["view"] == "composite"
             is_composite = true
@@ -245,32 +255,84 @@ function serveFITS(request::HTTP.Request)
             @async loadFITS(filepath)
         end
     end
-    
-    resp = IOBuffer();
-    
+
+    resp = IOBuffer()
+
     write(resp, "<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"utf-8\">\n")
-    write(resp, "<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n")
-    write(resp, "<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n")
+    write(
+        resp,
+        "<link href=\"https://fonts.googleapis.com/css?family=Inconsolata\" rel=\"stylesheet\"/>\n",
+    )
+    write(
+        resp,
+        "<link href=\"https://fonts.googleapis.com/css?family=Material+Icons\" rel=\"stylesheet\"/>\n",
+    )
     write(resp, "<script src=\"https://d3js.org/d3.v5.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/reconnecting-websocket.min.js\"></script>\n")
-    write(resp, "<script src=\"//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/ra_dec_conversion.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/sylvester.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/shortcut.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/colourmaps.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/lz4.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/marchingsquares-isocontours.min.js\"></script>\n")
-    write(resp, "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/marchingsquares-isobands.min.js\"></script>\n")
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/reconnecting-websocket.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/ra_dec_conversion.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/sylvester.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/shortcut.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/colourmaps.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/lz4.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/marchingsquares-isocontours.min.js\"></script>\n",
+    )
+    write(
+        resp,
+        "<script src=\"https://cdn.jsdelivr.net/gh/jvo203/fits_web_ql/htdocs/fitswebql/marchingsquares-isobands.min.js\"></script>\n",
+    )
 
     # WebAssembly
     write(resp, "<script src=\"client.", WASM_VERSION, ".js\"></script>\n")
-    write(resp, "<script>\n", "Module.ready\n", "\t.then(status => console.log(status))\n", "\t.catch(e => console.error(e));\n", "</script>\n")
+    write(
+        resp,
+        "<script>\n",
+        "Module.ready\n",
+        "\t.then(status => console.log(status))\n",
+        "\t.catch(e => console.error(e));\n",
+        "</script>\n",
+    )
 
     # Bootstrap v3.4.1
-    write(resp, "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1\">\n")
-    write(resp, "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\" integrity=\"sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu\" crossorigin=\"anonymous\">")
-    write(resp, "<script src=\"https://code.jquery.com/jquery-1.12.4.min.js\" integrity=\"sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ\" crossorigin=\"anonymous\"></script>")
-    write(resp, "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\" integrity=\"sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd\" crossorigin=\"anonymous\"></script>")
+    write(
+        resp,
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1\">\n",
+    )
+    write(
+        resp,
+        "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\" integrity=\"sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu\" crossorigin=\"anonymous\">",
+    )
+    write(
+        resp,
+        "<script src=\"https://code.jquery.com/jquery-1.12.4.min.js\" integrity=\"sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ\" crossorigin=\"anonymous\"></script>",
+    )
+    write(
+        resp,
+        "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\" integrity=\"sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd\" crossorigin=\"anonymous\"></script>",
+    )
 
     # GLSL vertex shader
     write(resp, "<script id=\"vertex-shader\" type=\"x-shader/x-vertex\">\n")
@@ -378,18 +440,23 @@ function serveFITS(request::HTTP.Request)
 
     # FITSWebQL main JavaScript + CSS
     write(resp, "<script src=\"fitswebqlse.js?", VERSION_STRING, "\"></script>\n")
-    write(resp, "<link rel=\"stylesheet\" href=\"fitswebqlse.css?", VERSION_STRING, "\"/>\n")
+    write(
+        resp,
+        "<link rel=\"stylesheet\" href=\"fitswebqlse.css?",
+        VERSION_STRING,
+        "\"/>\n",
+    )
 
     # HTML content
     va_count = length(datasets)
     write(resp, "<title>FITSWEBQLSE</title></head><body>\n")
     write(resp, "<div id='votable' style='width: 0; height: 0;' data-va_count='$va_count' ")
-        
+
     if va_count == 1
         datasetid = datasets[1]
         write(resp, "data-datasetId='$datasetid' ")
     else
-        for i in 1:va_count
+        for i = 1:va_count
             datasetid = datasets[i]
             write(resp, "data-datasetId$i='$datasetid' ")
         end
@@ -405,7 +472,13 @@ function serveFITS(request::HTTP.Request)
         write(resp, "data-root-path='/' ")
     end
 
-    write(resp, " data-server-version='", VERSION_STRING, "' data-server-string='", SERVER_STRING)
+    write(
+        resp,
+        " data-server-version='",
+        VERSION_STRING,
+        "' data-server-string='",
+        SERVER_STRING,
+    )
 
     if LOCAL_VERSION
         write(resp, "' data-server-mode='LOCAL")
@@ -425,33 +498,35 @@ function serveFITS(request::HTTP.Request)
     write(resp, "<script>var WS_PORT = $WS_PORT;</script>\n")
 
     # the page entry point
-    write(resp,
-    "<script>",
-              "const golden_ratio = 1.6180339887;",
-              "var ALMAWS = null ;",
-              "var wsVideo = null ;",
-              "var wsConn = null;",
-              "var firstTime = true;",
-              "var has_image = false;",
-              "var PROGRESS_VARIABLE = 0.0;",
-              "var PROGRESS_INFO = '' ;",
-              "var RESTFRQ = 0.0;",
-              "var USER_SELFRQ = 0.0;",
-              "var USER_DELTAV = 0.0;",
-              "var ROOT_PATH = '/fitswebql/';",
-              "var idleResize = -1;",
-              "window.onresize = resizeMe;",
-              "window.onbeforeunload = function() {",
-              "    if (wsConn != null)",
-              "    {",
-              "        for (let i = 0; i < va_count; i++)",
-              "            wsConn[i].close();",
-              "    }",
-              "",
-              "          if (wsVideo != null)",
-              "             wsVideo.close();",
-              "    };",
-              "mainRenderer(); </script>\n")
+    write(
+        resp,
+        "<script>",
+        "const golden_ratio = 1.6180339887;",
+        "var ALMAWS = null ;",
+        "var wsVideo = null ;",
+        "var wsConn = null;",
+        "var firstTime = true;",
+        "var has_image = false;",
+        "var PROGRESS_VARIABLE = 0.0;",
+        "var PROGRESS_INFO = '' ;",
+        "var RESTFRQ = 0.0;",
+        "var USER_SELFRQ = 0.0;",
+        "var USER_DELTAV = 0.0;",
+        "var ROOT_PATH = '/fitswebql/';",
+        "var idleResize = -1;",
+        "window.onresize = resizeMe;",
+        "window.onbeforeunload = function() {",
+        "    if (wsConn != null)",
+        "    {",
+        "        for (let i = 0; i < va_count; i++)",
+        "            wsConn[i].close();",
+        "    }",
+        "",
+        "          if (wsVideo != null)",
+        "             wsVideo.close();",
+        "    };",
+        "mainRenderer(); </script>\n",
+    )
 
     write(resp, "</body></html>")
 
@@ -475,8 +550,8 @@ println("Press CTRL+C to exit.")
 # Sockets.localhost or Sockets.IPv4(0)
 host = Sockets.IPv4(0)
 
-function ws_coroutine(ws, datasetid)
-        @info "Started websocket coroutine for $datasetid" ws
+function ws_coroutine(ws, datasetid::String)
+    @info "Started websocket coroutine for $datasetid" ws
 
     while isopen(ws)
         data, = readguarded(ws)
@@ -488,7 +563,7 @@ function ws_coroutine(ws, datasetid)
         end
 
         @info "Received: $s"
-        
+
         # ping back messages
         writeguarded(ws, s)
     end
@@ -499,16 +574,16 @@ end
 
 function ws_gatekeeper(req, ws)
     orig = WebSockets.origin(req)
-    
+
     @info "\nOrigin: $orig   Target: $(req.target)   subprotocol: $(subprotocol(req))"
 
     # check if there is a <datasetid> present in <req.target>
     pos = findlast("/", req.target)
-        
+
     if !isnothing(pos)
         datasetid = SubString(req.target, pos[1] + 1)
 
-    @info "\n[ws] datasetid $datasetid"
+        @info "\n[ws] datasetid $datasetid"
 
         ws_coroutine(ws, datasetid)
     else
