9d9fcb2f2cb45bb554a9923b401043509a6b2614
jvo203 <chris.zapart@nao.ac.jp>
2021-06-15 09:21:07 +0900

    modified:   Julia/fits.jl

--- Julia/fits.jl
+++ Julia/fits.jl
@@ -2,4 +2,18 @@ using FITSIO;
 
 function loadFITS(filepath::String)
     println("loading $filepath")
+
+    local f
+
+    try
+        f = FITS(filepath)
+        println(f)
+    catch e
+        println(e)
+        return
+    end
+
+    for hdu in f
+        println(typeof(hdu))
+    end
 end
