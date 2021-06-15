1ac5dd82b72ee17f2b310d4af55482e54079ee9c
jvo203 <chris.zapart@nao.ac.jp>
2021-06-15 11:27:33 +0900

    modified:   Julia/fits.jl

--- Julia/fits.jl
+++ Julia/fits.jl
@@ -3,7 +3,7 @@ using FITSIO;
 function loadFITS(filepath::String)
     println("loading $filepath")
 
-    local f
+    local f , width , height , depth
 
     try
         f = FITS(filepath)
@@ -12,6 +12,10 @@ function loadFITS(filepath::String)
         println(e)
         return
     end
+
+    width = 0
+    height = 0
+    depth = 1
 
     for hdu in f
         println(typeof(hdu))
@@ -22,6 +26,24 @@ function loadFITS(filepath::String)
             # continue searching for the "right" HDU
             continue
         end
+
+        try
+            width = size(hdu, 1)
+            height = size(hdu, 2)
+            depth = size(hdu, 3)
+        catch e
+        end
+
+        println(
+            "naxes: ",
+            naxes,
+            "\twidth: ",
+            width,
+            "\theight: ",
+            height,
+            "\tdepth: ",
+            depth,
+        )
 
         # we have at least two dimensions
         # read the header & data; break the <for> loop
