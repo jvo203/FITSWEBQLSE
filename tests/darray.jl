79649813e8d42fd04e6e97496147007f331260fe
jvo203 <chris.zapart@nao.ac.jp>
2021-06-21 14:26:13 +0900

    modified:   darray.jl

--- tests/darray.jl
+++ tests/darray.jl
@@ -5,17 +5,23 @@ println(workers())
 n = length(workers())
 println("#workers: $n")
 
-# pixels = dzeros((3, 3), workers(), [1,n])
-pixels = dzeros(Int32, (3, 3, n), workers())
-mask = dfill(false, (3, 3, n), workers())
+# pixels = dzeros((3, 4), workers(), [1,n])
+# pixels = dzeros(Int32, (3, 4, n))
+
+chunks = (1, 1, n)
+pixels = DArray(I -> zeros(Int32, map(length, I)), (3, 4, n), workers(), chunks)
+
+# mask = dfill(false, (3, 4, n), workers())
+mask = DArray(I -> fill(false, map(length, I)), (3, 4, n), workers(), chunks)
 
 @everywhere function fill_array(d::DArray)
     pid = myid()
 
     la = localpart(d)
     la .= pid
+    la[1,1] = pid - 1
 
-    # println(localpart(d))
+    println("size(la) = $(size(la))")
 end
 
 @sync for w in workers()
