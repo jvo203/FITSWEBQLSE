b7fa04ebf99899733aa7a8aecb6443668c86830a
jvo203 <chris.zapart@nao.ac.jp>
2021-06-29 11:30:29 +0900

    new file:   tests/downsize.jl

---
+++ tests/downsize.jl
@@ -0,0 +1,9 @@
+using Images, ImageTransformations, Interpolations;
+
+pixels = randn(Float32, 10, 10)
+
+println(pixels, "dims: ", size(pixels))
+
+res = imresize(pixels, (3, 3))
+
+println(res, "dims: ", size(res))
