9d3f299915354621468a0eeffa94eb5b8370d8dd
jvo203 <chris.zapart@nao.ac.jp>
2021-06-29 11:30:52 +0900

    modified:   tests/downsize.jl

--- tests/downsize.jl
+++ tests/downsize.jl
@@ -2,8 +2,8 @@ using Images, ImageTransformations, Interpolations;
 
 pixels = randn(Float32, 10, 10)
 
-println(pixels, "dims: ", size(pixels))
+println(pixels, "; dims: ", size(pixels))
 
 res = imresize(pixels, (3, 3))
 
-println(res, "dims: ", size(res))
+println(res, "; dims: ", size(res))
