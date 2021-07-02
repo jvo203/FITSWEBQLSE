a652383611ea5e1102d2b08705855f0e90481041
jvo203 <chris.zapart@nao.ac.jp>
2021-07-02 15:20:02 +0900

    modified:   Julia/fitswebqlse.jl

--- Julia/fitswebqlse.jl
+++ Julia/fitswebqlse.jl
@@ -329,9 +329,11 @@ function serveImageSpectrum(request::HTTP.Request)
             return HTTP.Response(501, "Not Implemented")
         end
 
+        # by default chop cuts the last character only; replace it with ','
         json = chop(fetch(json_task)) * ","
         histogram, pixels, mask = fetch(image_task)
 
+        # chop the first '{' character only
         json = json * chop(JSON.json("histogram" => histogram), head = 1, tail = 0)
         println(json)
 
