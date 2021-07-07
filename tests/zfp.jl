15d295136577f5a94aa297cd6e9c7427f65e5dce
jvo203 <chris.zapart@nao.ac.jp>
2021-07-07 22:51:06 +0900

    modified:   tests/zfp.jl

--- tests/zfp.jl
+++ tests/zfp.jl
@@ -2,5 +2,10 @@ using ZfpCompression;
 
 width = 100
 height = 67
+precision = 8
 
 pixels = 100.0 * randn(Float32, width, height)
+
+compressed_pixels = zfp_compress(pixels,precision=precision)
+
+println("compressed size:", length(compressed_pixels))
