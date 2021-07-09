e03785bc07b7f3c657f171243beb571d6ea2d9eb
jvo203 <chris.zapart@nao.ac.jp>
2021-07-09 12:00:01 +0900

    modified:   fpzip.jl

--- tests/fpzip.jl
+++ tests/fpzip.jl
@@ -1,7 +1,19 @@
-using fpzip_jll;
+using fpzip_jll
 
 const FPZIP_MEDIUM_PRECISION = 16
 const FPZIP_HIGH_PRECISION = 24
 
 depth = 768
 spectrum = Float32(100.0) * randn(Float32, depth)
+
+function fpzip_compress(A::Array{Float32,1})
+
+    bufbytes = 1024 + length(A) * sizeof(eltype(A))
+    compressed = Vector{UInt8}(undef, bufbytes)
+
+    fpz = ccall((:fpzip_write_to_buffer, libfpzip), Ptr{Cvoid}, (Ptr{Cvoid}, Csize_t), compressed, bufbytes)
+    println("fpz: $fpz")
+
+end
+
+fpzip_compress(spectrum)
