23d5165c3bd6ebac4a4e53972ed7635139cc39a1
jvo203 <chris.zapart@nao.ac.jp>
2021-07-09 17:25:21 +0900

    modified:   tests/zfp.jl

--- tests/zfp.jl
+++ tests/zfp.jl
@@ -1,4 +1,4 @@
-import Base.Iterators: flatten
+import Base.Iterators:flatten
 
 using CodecLz4;
 using ZfpCompression;
@@ -7,10 +7,10 @@ width = 14
 height = 6
 precision = 8
 
-pixels = 100.0 * randn(Float32, width, height)
+pixels = Float32(100.0) * randn(Float32, width, height)
 mask = map(x -> x > -90, pixels)
 
-compressed_pixels = zfp_compress(pixels, precision = precision)
+compressed_pixels = zfp_compress(pixels, precision=precision)
 compressed_mask = transcode(LZ4HCCompressor, collect(flatten(UInt8.(mask))))
 
 println("compressed pixels size:", length(compressed_pixels))
@@ -22,4 +22,17 @@ println("compressed mask size:", length(compressed_mask))
 decomp = zfp_decompress(compressed_pixels)
 
 display(pixels)
+display(decomp)
+
+# 1D spectrum compression
+depth = 768
+spectrum = Float32(100.0) * randn(Float32, depth)
+precision = 24
+
+compressed_spectrum = zfp_compress(spectrum, precision=precision)
+println("compressed spectrum size: ", length(compressed_spectrum))
+
+decomp = zfp_decompress(compressed_spectrum)
+
+display(spectrum)
 display(decomp)
