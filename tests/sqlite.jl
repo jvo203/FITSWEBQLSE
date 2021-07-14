cd238f5b8ec1028792895cbb025cd3f8f6cdf640
jvo203 <chris.zapart@nao.ac.jp>
2021-07-14 16:42:50 +0900

    modified:   tests/sqlite.jl

--- tests/sqlite.jl
+++ tests/sqlite.jl
@@ -1,4 +1,5 @@
-using Base:splat
+# using Base:splat
+using JSON
 using SQLite
 
 splat_db = SQLite.DB("../splatalogue_v3.db")
@@ -10,8 +11,25 @@ freq_end = 689.3824263880999
 strSQL = "SELECT * FROM lines WHERE frequency>=$freq_start AND frequency<=$freq_end;"
 println(strSQL)
 
-res = SQLite.DBInterface.execute(splat_db, strSQL)
+has_molecules = false
+resp = IOBuffer()
+write(resp, "{\"molecules\" : [")
 
-for row in res
-    println(row)
+for row in SQLite.DBInterface.execute(splat_db, strSQL)
+    global has_molecules = true
+    json = JSON.json(row)
+
+    write(resp, json)
+    write(resp, ",")
 end
+
+json = String(take!(resp))
+
+if !has_molecules
+    json = "{\"molecules\" : []}"
+else
+    # remove the last character (comma) from json
+    json = chop(json, tail=1) * "]}"
+end
+
+println(json)
