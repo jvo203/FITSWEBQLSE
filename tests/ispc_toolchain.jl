849803d1900237314844a744c036261c4eb5057c
jvo203 <chris.zapart@nao.ac.jp>
2021-07-15 15:09:16 +0900

    modified:   tests/ispc_toolchain.jl

--- tests/ispc_toolchain.jl
+++ tests/ispc_toolchain.jl
@@ -2,7 +2,7 @@
 """
 Runs `ispc` on `code` and prints the resulting assembly to stdout.
 """
-function ispc_native(code, options=``)
+function ispc_native(code, options = ``)
     ispc_cmd = `ispc -o - --emit-asm $options -`
     open(ispc_cmd, "w", STDOUT) do stdin
         write(stdin, code)
@@ -12,7 +12,7 @@ end
 """
 Runs `ispc` on `code` and prints the resulting LLVM IR to stdout.
 """
-function ispc_llvm(code, options=``, llvm_dis="/usr/local/opt/llvm/bin/llvm-dis")
+function ispc_llvm(code, options = ``, llvm_dis = "/usr/local/opt/llvm/bin/llvm-dis")
     ispc_cmd = pipeline(`ispc -o - --emit-llvm $options -`, `$llvm_dis -`)
     open(ispc_cmd, "w", STDOUT) do stdin
         write(stdin, code)
@@ -20,21 +20,21 @@ function ispc_llvm(code, options=``, llvm_dis="/usr/local/opt/llvm/bin/llvm-dis"
 end
 
 # Detect how to link a shared library:
-@osx_only begin
+@static if Sys.isapple()
     libtool = "/usr/bin/libtool"
     println("Linker: $libtool")
     link(objfile, libfile) = run(`$libtool -dynamic -o "$libfile" "$objfile"`)
 end
 
-@linux_only begin
-    gpp = strip(readall(`which g++`))
+@static if Sys.islinux()
+    gpp = strip(read(`which g++`, String))
     gpp == "" && error("libtool or g++ is required")
     println("Linker: $gpp")
-    link(objfile, libfile) = run(
-        `$gpp -shared -Wl,-export-dynamic "$objfile" -o "$libfile"`)
+    link(objfile, libfile) =
+        run(`$gpp -shared -Wl,-export-dynamic "$objfile" -o "$libfile"`)
 end
 
-@windows_only begin
+@static if Sys.iswindows()
     error("Not implemented: don't know how to create a shared lib on Windows")
 end
 
@@ -42,7 +42,7 @@ end
 _keep_files = false
 _debug_generated = false
 
-function keep_files(flag, debug=false)
+function keep_files(flag, debug = false)
     global _keep_files
     global _debug_generated
     _keep_files = flag
@@ -53,16 +53,16 @@ end
 Runs `ispc` on `code`, creates a shared library and returns a library
 handle `lib` for use with `Libdl.dlsym(lib, symbol)`.
 """
-function load_ispc(code, options=``)
+function load_ispc(code, options = ``)
 
     function load(tmpdir)
         objfile = "$tmpdir/program.o"
         libfile = "$tmpdir/program.so"
-        
+
         # Pipe the input program to ispc:
         ispc_cmd = `ispc -o "$objfile" --pic $options -`
         println(ispc_cmd)
-        open(ispc_cmd, "w", STDERR) do stdin
+        open(ispc_cmd, "w", stderr) do stdin
             write(stdin, code)
         end
 
@@ -76,7 +76,7 @@ function load_ispc(code, options=``)
             run(`ispc -o $asmfile --emit-asm $options $sourcefile`)
             run(`ispc -o $bcfile --emit-llvm $options $sourcefile`)
         end
-        
+
         # Create a shared library:
         link(objfile, libfile)
         return Libdl.dlopen(libfile)
@@ -86,7 +86,7 @@ function load_ispc(code, options=``)
         load(mktempdir())
     else
         load(mktempdir())
-#        mktempdir(load)
+        #        mktempdir(load)
         # Note: the temp dir and library files will be deleted now.
         # That's OK because the library is already loaded.
     end
