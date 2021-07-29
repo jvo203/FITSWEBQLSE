6247d88270110e81391c8adde1eca224c29003f0
jvo203 <chris.zapart@nao.ac.jp>
2021-07-29 11:23:37 +0900

    modified:   Julia/ISPC.jl

--- Julia/ISPC.jl
+++ Julia/ISPC.jl
@@ -3,5 +3,5 @@ include("ispc_toolchain.jl")
 code = open(f -> read(f, String), "webql.ispc")
 
 # Compile the code and get function pointers
-lib = load_ispc(code, `--pic --opt=fast-math --addressing=32`)
+lib = load_ispc(code, `--opt=fast-math --addressing=32`)
 # fptr = Libc.Libdl.dlsym(lib, "simple")
