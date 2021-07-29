88134eb96bfbbf71c06e18c7b879ab25a4b1541d
jvo203 <chris.zapart@nao.ac.jp>
2021-07-29 11:18:57 +0900

    modified:   Julia/ISPC.jl

--- Julia/ISPC.jl
+++ Julia/ISPC.jl
@@ -1,29 +1,9 @@
 include("ispc_toolchain.jl")
 
-# A basic ISPC kernel:
-code = """
-export void simple(uniform float vin[], uniform float vout[],
-                   uniform int count) {
-    foreach (index = 0 ... count) {
-        float v = vin[index];
-        if (v < 0.5)
-            v = v * v;
-        else
-            v = sqrt(v);
-        vout[index] = v;
-    }
-}
-"""
+code = open(f -> read(f, String), "webql.ispc")
 
 println(code)
 
 # Compile the code and get a function pointer to our kernel:
-lib = load_ispc(code, `--target=avx1-i32x8`)
-fptr = Libc.Libdl.dlsym(lib, "simple")
-
-# Call the kernel:
-vin = rand(Float32, 1000);
-vout = zeros(Float32, 1000);
-ccall(fptr, Cvoid, (Ref{Float32}, Ref{Float32}, UInt64), vin, vout, length(vout))
-
-println(vout[1:10])
+lib = load_ispc(code, `--pic --opt=fast-math --addressing=32`)
+# fptr = Libc.Libdl.dlsym(lib, "simple")
