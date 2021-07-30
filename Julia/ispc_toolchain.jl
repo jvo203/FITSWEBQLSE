bf8ae70326de546c067fe56b937bcd9379ae2015
ザパート・クリストファー <chris.zapart@nao.ac.jp>
2021-07-30 11:52:28 +0900

    modified:   Julia/ispc_toolchain.jl

--- Julia/ispc_toolchain.jl
+++ Julia/ispc_toolchain.jl
@@ -2,7 +2,7 @@
 """
 Runs `ispc` on `code` and prints the resulting assembly to stdout.
 """
-function ispc_native(code, options = ``)
+function ispc_native(code, options=``)
     ispc_cmd = `ispc -o - --emit-asm $options -`
     open(ispc_cmd, "w", STDOUT) do stdin
         write(stdin, code)
@@ -12,7 +12,7 @@ end
 """
 Runs `ispc` on `code` and prints the resulting LLVM IR to stdout.
 """
-function ispc_llvm(code, options = ``, llvm_dis = "/usr/local/opt/llvm/bin/llvm-dis")
+function ispc_llvm(code, options=``, llvm_dis="/usr/local/opt/llvm/bin/llvm-dis")
     ispc_cmd = pipeline(`ispc -o - --emit-llvm $options -`, `$llvm_dis -`)
     open(ispc_cmd, "w", STDOUT) do stdin
         write(stdin, code)
@@ -22,14 +22,14 @@ end
 # Detect how to link a shared library:
 @static if Sys.isapple()
     libtool = "/usr/bin/libtool"
-    println("Linker: $libtool")
+    # println("Linker: $libtool")
     link(objfile, libfile) = run(`$libtool -dynamic -o "$libfile" "$objfile"`)
 end
 
 @static if Sys.islinux()
     gpp = strip(read(`which g++`, String))
     gpp == "" && error("libtool or g++ is required")
-    println("Linker: $gpp")
+    # println("Linker: $gpp")
     link(objfile, libfile) =
         run(`$gpp -shared -Wl,-export-dynamic "$objfile" -o "$libfile"`)
 end
@@ -42,7 +42,7 @@ end
 _keep_files = false
 _debug_generated = false
 
-function keep_files(flag, debug = false)
+function keep_files(flag, debug=false)
     global _keep_files
     global _debug_generated
     _keep_files = flag
@@ -53,7 +53,7 @@ end
 Runs `ispc` on `code`, creates a shared library and returns a library
 handle `lib` for use with `Libdl.dlsym(lib, symbol)`.
 """
-function load_ispc(code, options = ``)
+function load_ispc(code, options=``)
 
     function load(tmpdir)
         objfile = "$tmpdir/program.o"
