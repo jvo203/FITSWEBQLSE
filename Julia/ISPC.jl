7e9abf428825b5a334c5ea0fa6c0cfef22046e4d
jvo203 <chris.zapart@nao.ac.jp>
2021-08-06 11:35:08 +0900

    modified:   Julia/ISPC.jl

--- Julia/ISPC.jl
+++ Julia/ISPC.jl
@@ -7,5 +7,8 @@ code = open(f -> read(f, String), "webql.ispc")
 lib = load_ispc(code, `--opt=fast-math --addressing=32`)
 
 # get function pointers
-const radial_view_fptr = Libc.Libdl.dlsym(lib, "calculate_radial_spectrumF16")
-const square_view_fptr = Libc.Libdl.dlsym(lib, "calculate_square_spectrumF16")
+const radial_spec_fptr = Libc.Libdl.dlsym(lib, "calculate_radial_spectrumF16")
+const square_spec_fptr = Libc.Libdl.dlsym(lib, "calculate_square_spectrumF16")
+
+const radial_view_fptr = Libc.Libdl.dlsym(lib, "calculate_radial_viewport_spectrumF16")
+const square_view_fptr = Libc.Libdl.dlsym(lib, "calculate_square_viewport_spectrumF16")
