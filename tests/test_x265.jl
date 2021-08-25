3ad833250b1a8e33b1fa63e1bcf11b46a84d2506
jvo203 <chris.zapart@nao.ac.jp>
2021-08-25 15:54:22 +0900

    modified:   tests/test_x265.jl

--- tests/test_x265.jl
+++ tests/test_x265.jl
@@ -9,7 +9,18 @@ if param == C_NULL
     error("NULL x265_param")
 end
 
+# default parameters
 ccall((:x265_param_default_preset, libx265), Cvoid, (Ptr{Cvoid}, Cstring, Cstring), param, "superfast", "zerolatency")
 
+# set extra parameters manually
+fps = Integer(5)
+stat = ccall((:x265_param_parse, libx265), Cint, (Ptr{Cvoid}, Cstring, Cstring), param, "fps", string(fps))
+println("x265_param_parse::$stat")
+
+# bRepeatHeaders = 1
+stat = ccall((:x265_param_parse, libx265), Cint, (Ptr{Cvoid}, Cstring, Ptr{Cvoid}), param, "repeat-headers", C_NULL)
+println("x265_param_parse::$stat")
+
+# release memory
 ccall((:x265_param_free, libx265), Cvoid, (Ptr{Cvoid},), param)
 param = Nothing
