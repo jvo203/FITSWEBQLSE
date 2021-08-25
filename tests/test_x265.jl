00123a229a103daafc41ea9e566366520194c8e2
jvo203 <chris.zapart@nao.ac.jp>
2021-08-25 14:08:28 +0900

    modified:   tests/test_x265.jl

--- tests/test_x265.jl
+++ tests/test_x265.jl
@@ -1,3 +1,14 @@
 using x265_jll;
 
 param = Nothing
+
+param = ccall((:x265_param_alloc, libx265), Ptr{Cvoid}, ())
+println("typeof(param): ", typeof(param), "; value: $param")
+
+if param == C_NULL
+    error("NULL x265_param")
+end
+
+ccall((:x265_param_default_preset, libx265), Cvoid, (Ptr{Cvoid}, Cstring, Cstring), param, "superfast", "zerolatency")
+
+ccall((:x265_param_free, libx265), Cvoid, (Ptr{Cvoid},), param)
