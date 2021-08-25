fc4c9e71e3235dc23602352d880034e98691ce61
jvo203 <chris.zapart@nao.ac.jp>
2021-08-25 16:58:46 +0900

    modified:   tests/test_x265.jl

--- tests/test_x265.jl
+++ tests/test_x265.jl
@@ -19,7 +19,17 @@ println("x265_param_parse::$stat")
 
 # bRepeatHeaders = 1
 stat = ccall((:x265_param_parse, libx265), Cint, (Ptr{Cvoid}, Cstring, Ptr{Cvoid}), param, "repeat-headers", C_NULL)
+#stat = ccall((:x265_param_parse, libx265), Cint, (Ptr{Cvoid}, Cstring, Cstring), param, "repeat-headers", "1")
 println("x265_param_parse::$stat")
+
+# internalCsp = X265_CSP_I444
+stat = ccall((:x265_param_parse, libx265), Cint, (Ptr{Cvoid}, Cstring, Cstring), param, "input-csp", "i444")
+println("x265_param_parse::$stat")
+
+# internalBitDepth = 8
+# "input-depth", "output-depth" are CLI-ONLY
+#stat = ccall((:x265_param_parse, libx265), Cint, (Ptr{Cvoid}, Cstring, Cstring), param, "output-depth", "8")
+#println("x265_param_parse::$stat")
 
 # release memory
 ccall((:x265_param_free, libx265), Cvoid, (Ptr{Cvoid},), param)
