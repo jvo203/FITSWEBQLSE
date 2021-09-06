dbfd577b56162fd9694ad9f0eb55c40ff610c75b
jvo203 <chris.zapart@nao.ac.jp>
2021-09-06 12:17:11 +0900

    modified:   Julia/fits.jl

--- Julia/fits.jl
+++ Julia/fits.jl
@@ -2678,8 +2678,11 @@ end
 
     if bDownsize
         try
+            mask_task = Threads.@spawn mask = Bool.(imresize(mask, (image_width, image_height), method = Constant()),) # use Nearest-Neighbours for the mask    
+
             pixels = Float16.(imresize(pixels, (image_width, image_height)),)
-            mask = Bool.(imresize(mask, (image_width, image_height), method = Constant()),) # use Nearest-Neighbours for the mask
+
+            wait(mask_task)
         catch e
             # println(e)
             println(
