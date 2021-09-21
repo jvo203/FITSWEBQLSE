9b06ba2b2dd348e127bd8b89e0bddbfc6670d8f5
jvo203 <chris.zapart@nao.ac.jp>
2021-09-21 15:00:20 +0900

    modified:   tests/ipp_toolchain.jl

--- tests/ipp_toolchain.jl
+++ tests/ipp_toolchain.jl
@@ -17,3 +17,27 @@ end
     error("Not implemented: don't know how to create a shared lib on Windows")
 end
 
+"""
+Runs `gcc` on `code`, links with `ipp`, creates a shared library and returns a library
+handle `lib` for use with `Libdl.dlsym(lib, symbol)`.
+"""
+function load_ipp(code, options = ``)
+
+    function load(tmpdir)
+        objfile = "$tmpdir/program.o"
+        libfile = "$tmpdir/program.so"
+
+        # Pipe the input program to gcc:
+        gcc_cmd = `gcc -o "$objfile" --fPIC $options -`
+        println(gcc_cmd)
+        open(gcc_cmd, "w", stderr) do stdin
+            write(stdin, code)
+        end
+
+        # Create a shared library:
+        link(objfile, libfile)
+        return Libc.Libdl.dlopen(libfile)
+    end
+
+    load(mktempdir())
+end
