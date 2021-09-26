7f1b6309cbb251f0812d7a0ec15e4ffc06b54378
jvo203 <chris.zapart@nao.ac.jp>
2021-09-26 15:45:15 +0900

    modified:   Julia/ISPC.jl

--- Julia/ISPC.jl
+++ Julia/ISPC.jl
@@ -12,3 +12,9 @@ const square_spec_fptr = Libc.Libdl.dlsym(lib, "calculate_square_spectrumF16")
 
 const radial_view_fptr = Libc.Libdl.dlsym(lib, "calculate_radial_viewport_spectrumF16")
 const square_view_fptr = Libc.Libdl.dlsym(lib, "calculate_square_viewport_spectrumF16")
+
+const make_linear_video_frame_fptr = Libc.Libdl.dlsym(lib, "make_video_frameF16_linear")
+const make_logistic_video_frame_fptr = Libc.Libdl.dlsym(lib, "make_video_frameF16_logistic")
+const make_ratio_video_frame_fptr = Libc.Libdl.dlsym(lib, "make_video_frameF16_ratio")
+const make_square_video_frame_fptr = Libc.Libdl.dlsym(lib, "make_video_frameF16_square")
+const make_kegacy_video_frame_fptr = Libc.Libdl.dlsym(lib, "make_video_frameF16_legacy")
