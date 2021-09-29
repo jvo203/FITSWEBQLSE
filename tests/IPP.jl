429995afc3d9958cadd98defe8c3c568929032db
jvo203 <chris.zapart@nao.ac.jp>
2021-09-29 17:55:24 +0900

    modified:   tests/IPP.jl

--- tests/IPP.jl
+++ tests/IPP.jl
@@ -12,8 +12,8 @@ resizeCubic32F = Libc.Libdl.dlsym(ipp, "resizeCubic")
 dim = 10
 view = 3
 
-pixels = 100.0f0 * randn(Float32, dim, dim)
-mask = round.(UInt8, pixels)
+pixels = 10.0f0 * randn(Float32, dim, dim)
+mask = round.(UInt8, abs.(pixels))
 #display(pixels)
 
 #=
@@ -227,5 +227,132 @@ function resizeCubic32fC1R(src::Matrix{Float32}, width::Integer, height::Integer
     return dst
 end
 
+function resizeNearest8uC1R(src::Matrix{UInt8}, width::Integer, height::Integer)
+    # a destination buffer
+    dst = Matrix{UInt8}(undef, width, height)
+
+    dims = size(src)
+    srcSize = IppiSize(dims[1], dims[2])
+    dstSize = IppiSize(width, height)
+
+    specSize = Ref{Cint}(0)
+    initSize = Ref{Cint}(0)
+
+    # Spec and init buffer sizes
+    status = ccall(
+        (:ippiResizeGetSize_8u, ipplib * "/libippi.so"),
+        Cint,
+        (IppiSize, IppiSize, Cint, Cint, Ref{Cint}, Ref{Cint}),
+        srcSize,
+        dstSize,
+        ippNearest,
+        0,
+        specSize,
+        initSize,
+    )
+
+    println("status: $status; specSize: $(specSize[]), initSize: $(initSize[])")
+
+    if status != 0
+        error("ippiResizeGetSize_8u::$status")
+    end
+
+    # Memory allocation
+    pSpec = ccall((:ippsMalloc_8u, ipplib * "/libipps.so"), Ptr{Cvoid}, (Cint,), specSize[])
+    println("pSpec:", pSpec)
+
+    if pSpec == C_NULL
+        error("NULL pSpec")
+    end
+
+    # Filter initialization
+    status = ccall(
+        (:ippiResizeNearestInit_8u, ipplib * "/libippi.so"),
+        Cint,
+        (IppiSize, IppiSize, Ptr{Cvoid}),
+        srcSize,
+        dstSize,
+        pSpec,
+    )
+    println("ippiResizeNearestInit_8u::$status")
+
+    if status != 0
+        ccall((:ippsFree, ipplib * "/libipps.so"), Cvoid, (Ptr{Cvoid},), pSpec)
+        error("ippiResizeNearestInit_8u::$status")
+    end
+
+    bufSize = Ref{Cint}(0)
+
+    # General transform function
+    status = ccall(
+        (:ippiResizeGetBufferSize_8u, ipplib * "/libippi.so"),
+        Cint,
+        (Ptr{Cvoid}, IppiSize, Cint, Ref{Cint}),
+        pSpec,
+        dstSize,
+        ippC1,
+        bufSize,
+    )
+    println("ippiResizeGetBufferSize_8u::$status, bufSize:", bufSize[])
+
+    if status != 0
+        ccall((:ippsFree, ipplib * "/libipps.so"), Cvoid, (Ptr{Cvoid},), pSpec)
+        error("ippiResizeGetBufferSize_8u::$status")
+    end
+
+    pBuffer =
+        ccall((:ippsMalloc_8u, ipplib * "/libipps.so"), Ptr{Cvoid}, (Cint,), bufSize[])
+    println("pBuffer:", pBuffer)
+
+    if pBuffer != C_NULL
+        srcOffset = IppiPoint(0, 0)
+        dstOffset = IppiPoint(0, 0)
+
+        status = ccall(
+            (:ippiResizeGetSrcRoi_8u, ipplib * "/libippi.so"),
+            Cint,
+            (Ptr{Cvoid}, IppiPoint, IppiSize, Ref{IppiPoint}, Ref{IppiSize}),
+            pSpec,
+            dstOffset,
+            dstSize,
+            srcOffset,
+            srcSize,
+        )
+        println("ippiResizeGetSrcRoi_8u::$status")
+
+        if status == 0
+            # finally resize the image            
+            srcStep = strides(src)[2] * sizeof(Float32)
+            dstStep = strides(dst)[2] * sizeof(Float32)
+
+            status = ccall(
+                (:ippiResizeNearest_8u_C1R, ipplib * "/libippi.so"),
+                Cint,
+                (Ptr{UInt8}, Cint, Ptr{UInt8}, Cint, IppiPoint, IppiSize, Cint, Ptr{Cvoid}),
+                src,
+                srcStep,
+                dst,
+                dstStep,
+                dstOffset,
+                dstSize,
+                pSpec,
+                pBuffer,
+            )
+            println("ippiResizeNearest_8u_C1R::$status")
+
+        end
+
+        ccall((:ippsFree, ipplib * "/libipps.so"), Cvoid, (Ptr{Cvoid},), pBuffer)
+    end
+
+    # Release memory
+    ccall((:ippsFree, ipplib * "/libipps.so"), Cvoid, (Ptr{Cvoid},), pSpec)
+
+    return dst
+end
+
 res = resizeCubic32fC1R(pixels, view, view)
 display(res)
+
+res2 = resizeNearest8uC1R(mask, view, view)
+display(res2)
