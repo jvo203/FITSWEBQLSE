ba9740ced47b96fa6f35caa42889e9dec583bf57
jvo203 <chris.zapart@nao.ac.jp>
2021-11-09 17:21:49 +0900

    modified:   htdocs/fitswebql/fitswebqlse.js

--- htdocs/fitswebql/fitswebqlse.js
+++ htdocs/fitswebql/fitswebqlse.js
@@ -1,5 +1,5 @@
 function get_js_version() {
-	return "JS2021-11-09.1";
+	return "JS2021-11-09.2";
 }
 
 const wasm_supported = (() => {
@@ -89,36 +89,6 @@ function round(value, precision, mode) {
 
 	return (isHalf ? value : Math.round(value)) / m
 }
-
-// ----------------------------------------------------------
-// If you're not in IE (or IE version is less than 5) then:
-// ie === undefined
-// If you're in IE (>=5) then you can determine which version:
-// ie === 7; // IE7
-// Thus, to detect IE:
-// if (ie) {}
-// And to detect the version:
-// ie === 6 // IE6
-// ie > 7 // IE8, IE9, IE10 ...
-// ie < 9 // Anything less than IE9
-// ----------------------------------------------------------
-var ie = (function () {
-	var undef, rv = -1; // Return value assumes failure.
-	var ua = window.navigator.userAgent;
-	var msie = ua.indexOf('MSIE ');
-	var trident = ua.indexOf('Trident/');
-
-	if (msie > 0) {
-		// IE 10 or older => return version number
-		rv = parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-	} else if (trident > 0) {
-		// IE 11 (or newer) => return version number
-		var rvNum = ua.indexOf('rv:');
-		rv = parseInt(ua.substring(rvNum + 3, ua.indexOf('.', rvNum)), 10);
-	}
-
-	return ((rv > -1) ? rv : undef);
-}());
 
 function getUint64(dataview, byteOffset, littleEndian) {
 	// 64ビット数を2つの32ビット (4バイト) の部分に分割する
