08040169507c89148c9ac3681b0c5ddefa7eb704
jvo203 <chris.zapart@nao.ac.jp>
2021-12-04 11:36:49 +0900

    modified:   tests/test_distributed2.jl

--- tests/test_distributed2.jl
+++ tests/test_distributed2.jl
@@ -11,3 +11,18 @@ println(workers())
 
 addprocs(["grid61"])
 println(workers())
+
+addprocs(["grid61"])
+println(workers())
+
+addprocs(["grid62"])
+println(workers())
+
+addprocs(["grid62"])
+println(workers())
+
+addprocs(["grid63"])
+println(workers())
+
+addprocs(["grid63"])
+println(workers())
