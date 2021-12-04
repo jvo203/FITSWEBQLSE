c456d31626e8fe4645fe7d9c006095b4660eca76
jvo203 <chris.zapart@nao.ac.jp>
2021-12-04 11:38:50 +0900

    modified:   tests/test_distributed2.jl

--- tests/test_distributed2.jl
+++ tests/test_distributed2.jl
@@ -26,3 +26,24 @@ println(workers())
 
 addprocs(["grid63"])
 println(workers())
+
+println("distributed workers initialised, starting tests")
+
+@everywhere function test()
+    id = myid()
+    println(id)
+
+    try
+        open("/data/cache/$(id).txt", "w") do io
+            println(io, "myid() = $id")
+        end
+    catch err
+        println(err)
+    end
+end
+
+ras = [@spawnat w test() for w in workers()]
+
+println("ras: ", ras)
+
+@time wait.(ras)
