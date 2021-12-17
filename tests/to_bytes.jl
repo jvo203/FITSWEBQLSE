1679d3321515c3a2f05379c642690404eb5fd47e
jvo203 <chris.zapart@nao.ac.jp>
2021-12-17 18:39:45 +0900

    modified:   tests/to_bytes.jl

--- tests/to_bytes.jl
+++ tests/to_bytes.jl
@@ -1,3 +1,5 @@
+import Base.Iterators: flatten
+
 a = ones(5)
 b = [1 3 5]
 
@@ -10,4 +12,8 @@ z2 = map(x -> reverse(reinterpret(UInt8, [x])), a)
 println(z1)
 println(z2)
 
-println(typeof(z2))
+y1 = collect(flatten(z1))
+y2 = collect(flatten(z2))
+
+println(y1)
+println(y2)
