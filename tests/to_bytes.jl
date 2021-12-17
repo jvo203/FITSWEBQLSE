845f9f6b5609aa97dcd965c1e8885101fb91fa76
jvo203 <chris.zapart@nao.ac.jp>
2021-12-17 18:18:10 +0900

    modified:   Julia.txt

--- tests/to_bytes.jl
+++ tests/to_bytes.jl
@@ -1,7 +1,11 @@
-using BitConverter;
-
 a = ones(5)
 b = [1 3 5]
 
 c = reinterpret(UInt8, a)
 println(c)
+
+z1 = map(x -> reinterpret(UInt8, [x]), a)
+z2 = map(x -> reverse(reinterpret(UInt8, [x])), a)
+
+println(z1)
+println(z2)
