d03d3f832262415b84e6010c1a261ef4aa461f2e
jvo203 <chris.zapart@nao.ac.jp>
2022-01-13 16:22:15 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -4,10 +4,10 @@
 
 void start_http()
 {
-    printf("A libmicrohttpd server has started.\n");
+    printf("libmicrohttpd server has started.\n");
 };
 
 void stop_http()
 {
-    printf("A libmicrohttpd server has stopped.\n");
+    printf("libmicrohttpd server has stopped.\n");
 };
