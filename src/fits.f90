8352b36b1c9609dc1441e762d8f9ec2f81ec6dce
jvo203 <chris.zapart@nao.ac.jp>
2022-01-28 16:38:32 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -33,4 +33,97 @@ module fits
         enumerator medium
         enumerator high
     end enum
+
+    type fp16
+        integer(kind=2), dimension(:, :), pointer :: ptr
+    end type fp16
+
+    type dataset
+        character(kind=c_char), dimension(:), allocatable :: datasetid
+        character(len=:), allocatable :: uri
+        ! the id will be made by hashing the dataset uri
+        integer :: unit = -1! a FITS file handle
+
+        ! FITS header values
+        character(kind=c_char), dimension(:), allocatable :: hdr
+        integer :: naxis = 0
+        integer :: bitpix = 0
+        integer naxes(4)
+        character frameid*70, object*70, line*70, filter*70, date_obs*70
+        character btype*70, bunit*70, specsys*70, timesys*70
+        character cunit1*70, ctype1*70
+        character cunit2*70, ctype2*70
+        character cunit3*70, ctype3*70
+        real :: ignrval, restfrq, bmaj, bmin, bpa
+        real crval1, cdelt1, crpix1
+        real crval2, cdelt2, crpix2
+        real crval3, cdelt3, crpix3
+        real obsra, obsdec, datamin, datamax
+        real cd1_1, cd1_2, cd2_1, cd2_2
+
+        ! extras
+        real :: frame_multiplier = 1.0
+        logical :: has_velocity = .false.
+        logical :: has_frequency = .false.
+
+        ! derived values
+        character(len=16) :: flux
+        real(kind=c_float) dmin, dmax
+        real(kind=4), allocatable :: frame_min(:), frame_max(:)
+        real(kind=c_float), allocatable :: pixels(:, :)
+        logical(kind=c_bool), allocatable :: mask(:, :)
+
+        ! an array holding pointers to half-float 2D planes
+        type(fp16), dimension(:), allocatable :: half_compressed
+
+        logical :: is_optical = .true.
+        logical :: is_xray = .false.
+
+        logical :: error = .false.
+        logical :: ok = .false.
+        logical :: header = .false.
+
+        ! mutexes
+        type(gmutex) :: header_mtx, ok_mtx, error_mtx, progress_mtx
+
+        ! 2D image statistics
+        real(kind=c_float) pmin, pmax, pmedian
+        real(kind=c_float) mad, madN, madP
+        real(kind=c_float) black, white, sensitivity, ratio_sensitivity
+
+        ! image histogram
+        integer, allocatable :: hist(:)
+
+        ! progress
+        integer(8) :: start_time, crate, cmax
+        integer :: progress = 0
+        integer :: total = 0
+        real :: elapsed = 0
+
+        ! spectra
+        real(kind=c_float), allocatable :: mean_spectrum(:)
+        real(kind=c_float), allocatable :: integrated_spectrum(:)
+
+    contains
+        final :: close_fits_file
+    end type dataset
+
+    interface
+
+    end interface
+
+contains
+    subroutine close_fits_file(item)
+        type(dataset) :: item
+        integer status
+
+        ! nothing to do if the FITS file has never been opened
+        if (item%unit .eq. -1) return
+
+        print *, item%datasetid, ':closing the FITS file unit'
+
+        call ftclos(item%unit, status)
+        call ftfiou(item%unit, status)
+
+    end subroutine close_fits_file
 end module fits
