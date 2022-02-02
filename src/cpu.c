10e97b0752c652b139dd4be598f3871400e5d90b
jvo203 <chris.zapart@nao.ac.jp>
2022-02-02 16:05:30 +0900

    new file:   src/cpu.c

---
+++ src/cpu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <libcpuid.h>
+
+int get_physical_cores()
+{
+    struct cpu_raw_data_t raw;
+    struct cpu_id_t data;
+
+    cpuid_get_raw_data(&raw);
+    cpu_identify(&raw, &data);
+
+    printf("[C] No. of Physical Core(s) : %d\n", data.num_cores);
+
+    return data.num_cores;
+}
