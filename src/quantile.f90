977816c2a1caf5b01e3bd2e6f828b2c960ab8575
jvo203 <chris.zapart@nao.ac.jp>
2022-02-14 14:09:24 +0900

    new file:   src/quantile.f90

---
+++ src/quantile.f90
@@ -0,0 +1,26 @@
+module quantile_mod    ! quantile finder.    median = quantile( size(a)/2, a)
+contains
+    recursive function quantile(k, a) result(value)
+        integer, intent(in)  :: k          ! position in array
+        real, dimension(:), intent(in)  :: a
+
+        real                              :: value      ! output value of quantile
+        integer                           :: j
+        real                              :: ak
+
+        ak = a(k)
+        j = count(a < ak)                              ! how many a(:) < ak
+
+        if (j .ge. k) then
+            value = quantile(k, pack(a, a < ak))
+        else
+            j = count(a > ak) + k - size(a)
+
+            if (j .gt. 0) then
+                value = quantile(j, pack(a, a > ak))
+            else
+                value = ak
+            end if
+        end if
+    end function quantile
+end module quantile_mod
