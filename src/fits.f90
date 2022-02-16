80a04825a4a58b14f7f22c8894f7ac9b47447e34
jvo203 <chris.zapart@nao.ac.jp>
2022-02-16 16:23:51 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -19,7 +19,7 @@ module fits
     integer(c_int), parameter :: ZFP_MIN_EXP = -1074
 
     ! FITS channels are allocated to cluster nodes in blocks
-    integer, parameter :: CHANNEL_BLOCK = 16 ! 16 ! 64 ! 128
+    integer, parameter :: CHANNEL_BLOCK = 1 ! 16 ! 64 ! 128
 
     type(c_pthread_mutex_t), save :: logger_mtx
 
