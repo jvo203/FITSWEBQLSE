2ae7f3c24cca2765c0117b88f8752dd11fd7a03f
jvo203 <chris.zapart@nao.ac.jp>
2022-02-16 22:43:06 +0900

    new file:   rss.sh

---
+++ rss.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+grep -ris "banana" /usr/bin &
+pid=$!
+
+logfile=$(mktemp -t memory.log.XXXX)
+start=$(date +%s)
+
+# get the process' memory usage and run until `ps` fails which it will do when
+# the pid cannot be found any longer
+
+while mem=$(ps -o rss= -p "$pid"); do
+    time=$(date +%s)
+
+    # print the time since starting the program followed by its memory usage
+    printf "%d %s\n" $((time-start)) "$mem" >> "$logfile"
+
+    # sleep for a tenth of a second
+    sleep .1
+done
+
+printf "Find the log at %s\n" "$logfile"
