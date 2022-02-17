5b9df94ac7fd152da0bc65f66c199c9df933cd4c
jvo203 <chris.zapart@nao.ac.jp>
2022-02-17 11:15:44 +0900

    modified:   htdocs/fitswebql/fitswebqlse.js

--- htdocs/fitswebql/fitswebqlse.js
+++ htdocs/fitswebql/fitswebqlse.js
@@ -1,5 +1,5 @@
 function get_js_version() {
-	return "JS2022-02-04.0";
+	return "JS2022-02-17.0";
 }
 
 const wasm_supported = (() => {
@@ -12568,7 +12568,7 @@ function show_welcome() {
 
 	headerDiv.append("h2")
 		.attr("align", "center")
-		.html('WELCOME TO FITSWEBQLSE ALPHA RELEASE');
+		.html('WELCOME TO FITSWEBQLSE (ALPHA)');
 
 	var bodyDiv = contentDiv.append("div")
 		.attr("id", "modal-body")
