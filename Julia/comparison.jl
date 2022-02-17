ba5cec5215c2f044ead604aab807dbf98d893059
jvo203 <chris.zapart@nao.ac.jp>
2022-02-17 10:58:01 +0900

    modified:   Julia/comparison.jl

--- Julia/comparison.jl
+++ Julia/comparison.jl
@@ -13,5 +13,5 @@ allocated_f = data_f[:, 2] ./ (1024^3)
 
 common = min(size(timestamp_r)[1], size(timestamp_f)[1])
 
-plot(timestamp_f[1:common], [allocated_r[1:common], allocated_f[1:common]], label = ["Rust fits_web_ql v4" "C / FORTRAN FITSWEBQLSE v5"], xlabel = "elapsed time [s]", ylabel = "jemalloc stats.allocated memory [GB]")
+plot(timestamp_f[1:common], [allocated_r[1:common], allocated_f[1:common]], label = ["Rust fits_web_ql v4" "C / FORTRAN FITSWEBQLSE v5"], xlabel = "elapsed time [s]", ylabel = "allocated memory [GB]", legend = :bottomright, title = "memory consumption comparison")
 savefig(homedir() * "/mem_comparison.pdf")
