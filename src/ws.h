99fca9113e5a4766a31759846cce32bfdd0c4cde
jvo203 <chris.zapart@nao.ac.jp>
2022-02-24 11:19:21 +0900

    modified:   src/ws.h

--- src/ws.h
+++ src/ws.h
@@ -3,3 +3,4 @@
 #include "mongoose.h"
 
 void start_ws();
+extern void submit_channel_range(void *ptr, int idx, int progress, float *frame_min, float *frame_max, float *frame_median, float *mean_spectrum, float *integrated_spectrum);
