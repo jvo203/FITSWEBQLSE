403337bee8f83a79ec6ba2ecac5570b06eebaff6
jvo203 <chris.zapart@nao.ac.jp>
2022-03-06 15:39:43 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -51,6 +51,7 @@ typedef struct
     char *url;
     int port;
     int progress;
+    int *counter;
     bool done;
 } progress_t;
 
@@ -2074,7 +2075,7 @@ int submit_progress(char *root, char *datasetid, int len, int progress)
         // use mongoose HTTP client as libcURL leaks memory in Intel Clear Linux ...
         // apparently it's OK, it is not a real memory leak, just DBus caching ...
         // https://github.com/clearlinux/distribution/issues/2574#issuecomment-1058618721
-        progress_t req = {root, options.ws_port, progress, false};
+        progress_t req = {root, options.ws_port, progress, &counter, false};
 
         struct mg_mgr mgr; // Event manager
 
