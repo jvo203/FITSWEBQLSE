0c938a94d98bc72dd1aea2b5f572c46b570b0c84
jvo203 <chris.zapart@nao.ac.jp>
2022-03-10 15:03:17 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -308,7 +308,7 @@ void start_ws()
 
     struct mg_mgr mgr; // Event manager
     mg_mgr_init(&mgr); // Initialise event manager
-    mg_log_set("3");
+    // mg_log_set("3");
     printf("Starting WS listener on %s\n", url);
 
     mg_http_listen(&mgr, url, mg_http_ws_callback, NULL); // Create HTTP listener
