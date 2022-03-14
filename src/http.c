3a923e4d80aefc47aa63081e5f6e4ecf3c2849cf
jvo203 <chris.zapart@nao.ac.jp>
2022-03-14 17:48:00 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -2952,7 +2952,7 @@ void *fetch_image(void *ptr)
 
     if (handle_count == 0)
     {
-        printf("[C] no cluster nodes found. aborting 'fetch_image'\n");
+        printf("[C] aborting fetch_image (no cluster nodes found)\n");
         g_mutex_unlock(&cluster_mtx);
         pthread_exit(NULL);
     };
