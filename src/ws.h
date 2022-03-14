4ac7aaa36c8ad995243b5b17631b2ca649156947
jvo203 <chris.zapart@nao.ac.jp>
2022-03-14 15:07:01 +0900

    modified:   src/ws.h

--- src/ws.h
+++ src/ws.h
@@ -2,5 +2,50 @@
 
 #include "mongoose.h"
 
+enum zoom_shape
+{
+    circle,
+    square
+};
+
+enum intensity_mode
+{
+    mean,
+    integrated
+};
+
+enum image_quality
+{
+    low,
+    medium,
+    high
+};
+
+struct image_spectrum_request
+{
+    // input
+    int dx;
+    bool image;
+    enum image_quality quality;
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    int width;
+    int height;
+    enum zoom_shape beam;
+    enum intensity_mode intensity;
+    double frame_start;
+    double frame_end;
+    double ref_freq;
+    int seq_id;
+    float timestamp;
+    char *datasetid;
+
+    // output
+    int fd;
+};
+
 void start_ws();
 extern void submit_channel_range(void *ptr, int idx, int progress, float *frame_min, float *frame_max, float *frame_median, float *mean_spectrum, float *integrated_spectrum);
+extern void realtime_image_spectrum_request(void *ptr, void *req);
