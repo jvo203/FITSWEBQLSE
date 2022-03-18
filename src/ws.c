320f47e1427eff46d19b96f3a0fb6c74687b5db2
jvo203 <chris.zapart@nao.ac.jp>
2022-03-18 10:53:54 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -465,6 +465,15 @@ static void mg_http_ws_callback(struct mg_connection *c, int ev, void *ev_data, 
             }
 
             printf("[C] dx: %d, image: %d, quality: %d, x1: %d, y1: %d, x2: %d, y2: %d, beam: %d, intensity: %d, frame_start: %f, frame_end: %f, ref_freq: %f, seq_id: %d, timestamp: %f\n", req.dx, req.image, req.quality, req.x1, req.y1, req.x2, req.y2, req.beam, req.intensity, req.frame_start, req.frame_end, req.ref_freq, req.seq_id, req.timestamp);
+
+            // pass the request to FORTRAN
+            char *datasetId = (char *)fn_data;
+            void *item = get_dataset(datasetId);
+
+            if (item != NULL)
+                realtime_image_spectrum_request(item, &req);
+            else
+                printf("[C] cannot find a dataset '%s' in the hash table\n", datasetId);
         }
 
         break;
