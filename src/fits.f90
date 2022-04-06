5e3551d47bf4288c94c8a48a191e1cfb43378b85
jvo203 <chris.zapart@nao.ac.jp>
2022-04-06 14:35:20 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -302,9 +302,9 @@ module fits
       end function viewport_spectrum_circle
 
       ! export uniform float viewport_image_spectrum_rect(uniform struct fixed_block_t compressed[],&
-      ! uniform int width, uniform int height, uniform float pixels[], uniform unsigned int8 mask[],  uniform int npixels, uniform int tid, uniform int stride,&
+      ! uniform int width, uniform int height, uniform float pixels[], uniform unsigned int8 mask[], uniform int stride,&
       ! uniform int x1, uniform int x2, uniform int y1, uniform int y2, uniform bool average, uniform float cdelt3)
-      real(c_float) function viewport_image_spectrum_rect(compressed, width, height, pixels, mask, npixels, tid, &
+      real(c_float) function viewport_image_spectrum_rect(compressed, width, height, pixels, mask, &
       & stride, x1, x2, y1, y2, average, cdelt3) BIND(C, name="viewport_image_spectrum_rect")
          use, intrinsic :: ISO_C_BINDING
          implicit none
@@ -312,7 +312,7 @@ module fits
          type(C_PTR), value, intent(in) :: compressed
          integer(c_int), value, intent(in) :: width, height
          type(C_PTR), value, intent(in) :: pixels, mask
-         integer(c_int), value, intent(in) :: npixels, tid, stride
+         integer(c_int), value, intent(in) :: stride
          integer(c_int), value, intent(in) :: x1, x2, y1, y2, average
          real(c_float), value, intent(in) :: cdelt3
 
@@ -4533,8 +4533,7 @@ contains
 
       integer :: first, last, length, threshold
 
-      integer :: max_threads, frame
-      integer(c_int) :: tid, npixels
+      integer :: max_threads, frame, tid, npixels
       integer(c_int) :: x1, x2, y1, y2, width, height, average
       real(c_float) :: cx, cy, r, r2
       integer :: start_x, start_y, end_x, end_y
@@ -4671,7 +4670,7 @@ contains
          else
             if (req%beam .eq. square) then
                spectrum(frame) = viewport_image_spectrum_rect(c_loc(item%compressed(frame)%ptr),&
-               &width, height, c_loc(thread_pixels(:, tid)), c_loc(thread_mask(:, tid)), npixels, tid, dimx, &
+               &width, height, c_loc(thread_pixels(:, tid)), c_loc(thread_mask(:, tid)), dimx, &
                &x1 - 1, x2 - 1, y1 - 1, y2 - 1, average, cdelt3)
             end if
          end if
