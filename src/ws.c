7a96f07a61ec6cf0ce49d1084dc2f374ad398141
Christopher ZAPART <chris.zapart@nao.ac.jp>
2022-04-22 15:38:53 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -942,7 +942,7 @@ static void mg_http_ws_callback(struct mg_connection *c, int ev, void *ev_data, 
             get_spectrum_range_C(item, frame, frame, ref_freq, &frame_idx, &frame_idx);
             req->frame = frame_idx;
 
-            printf("[C]::video fps: %d, bitrate: %d, seq_id: %d, keyframe: %d, frame: {%f->%d}, ref_freq: %f, timestamp: %f\n", req->fps, req->bitrate, req->seq_id, req->keyframe, frame, req->frame, ref_freq, req->timestamp);
+            printf("[C]::video fps: %d, bitrate: %d, seq_id: %d, keyframe: %d, frame: {%f --> %d}, ref_freq: %f, timestamp: %f\n", req->fps, req->bitrate, req->seq_id, req->keyframe, frame, req->frame, ref_freq, req->timestamp);
 
             // skip repeated frames
             if (frame_idx == session->last_frame_idx)
