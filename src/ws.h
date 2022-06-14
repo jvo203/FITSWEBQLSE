a9c5fe8fa5e69a3e8297eef5b5ad974a0895fd8a
Christopher ZAPART <chris.zapart@nao.ac.jp>
2022-06-14 13:35:46 +0900

    modified:   src/ws.h

--- src/ws.h
+++ src/ws.h
@@ -59,3 +59,4 @@ void *ws_image_spectrum_response(void *ptr);
 void *video_response(void *ptr);
 extern void get_inner_dimensions(void *ptr, int width, int height, int *fits_width, int *fits_height, int *inner_width, int *inner_height, float *scale);
 extern void get_spectrum_range_C(void *ptr, double frame_start, double frame_end, double ref_freq, int *first, int *last);
+extern void fill_global_statistics(void *ptr, float *dmin, float *dmax, float *dmedian, float *dmadN, float *dmadP);
