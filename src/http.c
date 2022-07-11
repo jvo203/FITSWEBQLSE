fa077f36df87c07b838db64ed12be01df95142c2
jvo203 <chris.zapart@nao.ac.jp>
2022-07-11 15:30:29 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -4640,7 +4640,8 @@ char *get_jvo_path(PGconn *jvo_db, char *db, char *table, char *data_id)
             strncat(path, &ch, 1);*/
 
             char slash[2] = "/";
-            strncat(path, slash, 1);
+            // strncat(path, slash, 1);
+            strcat(path, "/");
 
             strcat(path, (const char *)PQgetvalue(res, 0, 0));
         }
