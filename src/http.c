1fa0a652441606f9d2d9544eae601d4fcc5420bb
jvo203 <chris.zapart@nao.ac.jp>
2022-07-11 15:36:58 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -4636,7 +4636,12 @@ char *get_jvo_path(PGconn *jvo_db, char *db, char *table, char *data_id)
                 strncat(path, &ch, 1);
             }
 
-            strcat(path, "/");
+            /*ch = '/';
+            strncat(path, &ch, 1);*/
+
+            char slash[2] = "/";
+            strncat(path, slash, 1);
+
             strcat(path, (const char *)PQgetvalue(res, 0, 0));
         }
     }
