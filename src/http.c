196e798726e996d3e04567f56e161ed0757023d6
jvo203 <chris.zapart@nao.ac.jp>
2022-07-15 16:29:35 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -154,6 +154,7 @@ void write_json(int fd, GString *json);
 void write_header(int fd, const char *header_str, int str_len);
 void write_elapsed(int fd, const float *elapsed);
 void write_csv_comments(int fd, const char *ra, const char *dec, double lng, double lat, int beam, double beam_width, double beam_height, float cx, float cy, int dimx, int dimy, double deltaV, double ref_freq, const char *specsys);
+void write_csv_row(int fd, int channel, double f, double v, bool header);
 void write_partial_statistics(int fd, const float *sumP, const int64_t *countP, const float *sumN, const int64_t *countN);
 void write_statistics(int fd, float *dmin, float *dmax, float *dmedian, float *dmadN, float *dmadP);
 void write_spectrum(int fd, const float *spectrum, int n, int precision);
@@ -3720,6 +3721,10 @@ void split_wcs(const char *coord, char *key, char *value, const char *null_key)
         *pos = *(pos + 1); // this overwrites '\' with the next character (most likely a double quote)
         *(++pos) = '\0';   // hide the next (last) character
     }
+}
+
+void write_csv_row(int fd, int channel, double f, double v, bool header)
+{
 }
 
 void write_csv_comments(int fd, const char *ra, const char *dec, double lng, double lat, int beam, double beam_width, double beam_height, float cx, float cy, int dimx, int dimy, double deltaV, double ref_freq, const char *specsys)
