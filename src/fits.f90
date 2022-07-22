0628896c42f7e8722bc954facf7cd04bf034905e
Christopher ZAPART <chris.zapart@nao.ac.jp>
2022-07-22 16:47:59 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -5970,8 +5970,8 @@ contains
       y2 = min(item%naxes(2), req%y2)
 
       ! calculate the centre and squared radius
-      cx = abs(x1 + x2)/2
-      cy = abs(y1 + y2)/2
+      cx = 0.5*abs(x1 + x2)
+      cy = 0.5*abs(y1 + y2)
       ! r = 0.5*min(abs(x2 - x1), abs(y2 - y1))
       r = min(abs(cx - x1), abs(cy - y1)) + 1
       r2 = r*r
