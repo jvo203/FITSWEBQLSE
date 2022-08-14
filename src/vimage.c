50f1d441e55aea66f71dc235872fc9e38cefafea
jvo203 <chris.zapart@nao.ac.jp>
2022-08-14 15:36:29 +0900

    modified:   src/vimage.c

--- src/vimage.c
+++ src/vimage.c
@@ -5,14 +5,25 @@
 
 // entry functions from Fortran
 
-extern void resizeLanczos(const float *pSrc, int srcWidth, int srcHeight, float *pDest, int dstWidth, int dstHeight, int numLobes)
+extern void resizeLanczos(float *pSrc, int srcWidth, int srcHeight, float *pDest, int dstWidth, int dstHeight, int numLobes)
+{
+    struct vImage_Buffer srcBuffer, dstBuffer;
+
+    srcBuffer.data = pSrc;
+    srcBuffer.height = srcHeight;
+    srcBuffer.width = srcWidth;
+    srcBuffer.rowBytes = srcWidth * sizeof(float);
+
+    dstBuffer.data = pDest;
+    dstBuffer.height = dstHeight;
+    dstBuffer.width = dstWidth;
+    dstBuffer.rowBytes = dstWidth * sizeof(float);
+}
+
+extern void resizeSuper(float *pSrc, int srcWidth, int srcHeight, float *pDest, int dstWidth, int dstHeight)
 {
 }
 
-extern void resizeSuper(const float *pSrc, int srcWidth, int srcHeight, float *pDest, int dstWidth, int dstHeight)
-{
-}
-
-extern void resizeNearest(const unsigned char *pSrc, int srcWidth, int srcHeight, unsigned char *pDest, int dstWidth, int dstHeight)
+extern void resizeNearest(unsigned char *pSrc, int srcWidth, int srcHeight, unsigned char *pDest, int dstWidth, int dstHeight)
 {
 }
