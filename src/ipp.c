43319266036084c7075aaf76cc4cebbb3ca15270
jvo203 <chris.zapart@nao.ac.jp>
2022-08-23 16:28:51 +0900

    modified:   src/ipp.c

--- src/ipp.c
+++ src/ipp.c
@@ -7,6 +7,8 @@
 #include <ipps.h>
 #include <ippcore_tl.h>
 
+extern int get_physical_cores();
+
 IppStatus resizeNearest8u_C1R(Ipp8u *pSrc, IppiSize srcSize, Ipp32s srcStep,
                               Ipp8u *pDst, IppiSize dstSize, Ipp32s dstStep)
 {
@@ -168,6 +170,94 @@ IppStatus resizeLanczos32f_C1R(Ipp32f *pSrc, IppiSize srcSize, Ipp32s srcStep,
                                Ipp32f *pDst, IppiSize dstSize, Ipp32s dstStep,
                                int numLobes)
 {
+    IppiResizeSpec_32f *pSpec = 0;
+    int specSize = 0, initSize = 0, bufSize = 0;
+    Ipp8u *pBuffer = 0;
+    Ipp8u *pInitBuf = 0;
+    IppiPoint dstOffset = {0, 0};
+    IppiPoint srcOffset = {0, 0};
+    IppStatus status = ippStsNoErr;
+    IppiBorderSize borderSize = {0, 0, 0, 0};
+    IppiBorderType border = ippBorderRepl;
+    IppStatus pStatus;
+
+    /* Spec and init buffer sizes */
+    status = ippiResizeGetSize_32f(srcSize, dstSize, ippLanczos, 0, &specSize, &initSize);
+
+    if (status != ippStsNoErr)
+        return status;
+
+    /* Memory allocation */
+    pInitBuf = ippsMalloc_8u(initSize);
+    pSpec = (IppiResizeSpec_32f *)ippsMalloc_8u(specSize);
+
+    if (pInitBuf == NULL || pSpec == NULL)
+    {
+        ippsFree(pInitBuf);
+        ippsFree(pSpec);
+        return ippStsNoMemErr;
+    }
+
+    /* Filter initialization */
+    status = ippiResizeLanczosInit_32f(srcSize, dstSize, numLobes, pSpec, pInitBuf);
+    ippsFree(pInitBuf);
+
+    if (status != ippStsNoErr)
+    {
+        ippsFree(pSpec);
+        return status;
+    }
+
+    status = ippiResizeGetBorderSize_32f(pSpec, &borderSize);
+    if (status != ippStsNoErr)
+    {
+        ippsFree(pSpec);
+        return status;
+    }
+
+    /* General transform function */
+    ippiResizeGetBufferSize_32f(pSpec, dstSize, ippC1, &bufSize);
+
+    pBuffer = ippsMalloc_8u(bufSize);
+
+    if (pBuffer)
+    {
+        Ipp8u *pOneBuf;
+
+        pStatus = ippiResizeGetSrcRoi_32f(pSpec, dstOffset, dstSize,
+                                          &srcOffset, &srcSize);
+
+        if (pStatus == ippStsNoErr)
+        {
+            pOneBuf = pBuffer;
+
+            pStatus = ippiResizeLanczos_32f_C1R(
+                pSrc, srcStep * sizeof(Ipp32f), pDst, dstStep * sizeof(Ipp32f),
+                dstOffset, dstSize, border, 0, pSpec, pOneBuf);
+        }
+    }
+
+    ippsFree(pSpec);
+
+    if (pBuffer == NULL)
+        return ippStsNoMemErr;
+
+    ippsFree(pBuffer);
+
+    /* Return bad status */
+    if (pStatus != ippStsNoErr)
+        return pStatus;
+
+    return status;
+}
+
+IppStatus resizeLanczos32f_C1R_LT(Ipp32f *pSrc, IppiSize srcSize, Ipp32s srcStep,
+                                  Ipp32f *pDst, IppiSize dstSize, Ipp32s dstStep,
+                                  int numLobes)
+{
+    int threads = get_physical_cores();
+    omp_set_num_threads(threads);
+
     IppiResizeSpec_32f *pSpec = 0;
     int specSize = 0, initSize = 0, bufSize = 0;
     Ipp8u *pBuffer = 0;
