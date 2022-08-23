13aebae5c7b1c4d2002aae8a228b4b59b30502c9
jvo203 <chris.zapart@nao.ac.jp>
2022-08-23 17:39:49 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -2547,9 +2547,9 @@ contains
 
             ! launch a pthread, passing the FORTRAN <item> dataset via a C pointer
             rc = c_pthread_create(thread=pid, &
-               attr=c_null_ptr, &
-               start_routine=c_funloc(global_statistics), &
-               arg=c_loc(item))
+                                  attr=c_null_ptr, &
+                                  start_routine=c_funloc(global_statistics), &
+                                  arg=c_loc(item))
 
             ! detach a thread
             if (rc .eq. 0) rc = c_pthread_detach(pid)
@@ -4600,17 +4600,17 @@ contains
             tone_mapping = histogram_classifier(c_loc(Slot))
 
             select case (tone_mapping)
-             case (0)
+            case (0)
                tone%flux = 'legacy'
-             case (1)
+            case (1)
                tone%flux = 'linear'
-             case (2)
+            case (2)
                tone%flux = 'logistic'
-             case (3)
+            case (3)
                tone%flux = 'ratio'
-             case (4)
+            case (4)
                tone%flux = 'square'
-             case default
+            case default
                tone%flux = 'legacy'
             end select
          end block
@@ -5155,7 +5155,7 @@ contains
 
       ! calculate the FITS file size
       filesize = nint(real(size(item%hdr)) + real(item%naxes(1))*real(item%naxes(2))&
-      &*real(item%naxes(3))*real(item%naxes(4))*real(abs(item%bitpix)/8), kind=8)
+                     &*real(item%naxes(3))*real(item%naxes(4))*real(abs(item%bitpix)/8), kind=8)
 
       json = begin_json()
 
@@ -5249,9 +5249,9 @@ contains
 
       ! launch a pthread
       rc = c_pthread_create(thread=pid, &
-         attr=c_null_ptr, &
-         start_routine=c_funloc(fetch_inner_dimensions), &
-         arg=c_loc(inner_dims))
+                            attr=c_null_ptr, &
+                            start_routine=c_funloc(fetch_inner_dimensions), &
+                            arg=c_loc(inner_dims))
 
       ! get the inner image bounding box (excluding NaNs)
       call inherent_image_dimensions(item, inner_width, inner_height)
@@ -5323,9 +5323,9 @@ contains
 
          ! launch a pthread
          rc = c_pthread_create(thread=pid, &
-            attr=c_null_ptr, &
-            start_routine=c_funloc(fetch_inner_dimensions), &
-            arg=c_loc(inner_dims))
+                               attr=c_null_ptr, &
+                               start_routine=c_funloc(fetch_inner_dimensions), &
+                               arg=c_loc(inner_dims))
       end if
 
       ! get the inner image bounding box (excluding NaNs)
@@ -5356,6 +5356,7 @@ contains
          t1 = omp_get_wtime()
 
          !$omp parallel
+         !$omp single
          !$omp task
          if (scale .gt. 0.2) then
             call resizeLanczos(c_loc(item%pixels), item%naxes(1), item%naxes(2), c_loc(pixels), img_width, img_height, 3)
@@ -5363,12 +5364,15 @@ contains
             call resizeSuper(c_loc(item%pixels), item%naxes(1), item%naxes(2), c_loc(pixels), img_width, img_height)
          end if
          !$omp end task
+         !$omp end single
 
+         !$omp single
          !$omp task
          ! Boolean mask: the naive Nearest-Neighbour method
          call resizeNearest(c_loc(item%mask), item%naxes(1), item%naxes(2), c_loc(mask), img_width, img_height)
          ! call resizeMask(item%mask, item%naxes(1), item%naxes(2), mask, img_width, img_height)
          !$omp end task
+         !$omp end single
          !$omp end parallel
 
          t2 = omp_get_wtime()
@@ -5399,9 +5403,9 @@ contains
 
          ! launch a pthread
          rc = c_pthread_create(thread=pid, &
-            attr=c_null_ptr, &
-            start_routine=c_funloc(fetch_image), &
-            arg=c_loc(image_req))
+                               attr=c_null_ptr, &
+                               start_routine=c_funloc(fetch_image), &
+                               arg=c_loc(image_req))
 
          ! join a thread (wait for the results)
          rc = c_pthread_join(pid, c_null_ptr)
@@ -5643,7 +5647,7 @@ contains
       real(c_float) :: cx, cy, rx, ry, r, r2
       real :: cdelt3
 
-      real(kind=c_double) :: lng, lat;
+      real(kind=c_double) :: lng, lat; 
       real(kind=c_double) :: ra1, dec1, ra2, dec2
       real(kind=c_double) :: beam_width, beam_height
       real(kind=c_double) :: frequency, velocity
@@ -5747,9 +5751,9 @@ contains
 
       ! launch a thread
       rc = c_pthread_create(thread=pid, &
-         attr=c_null_ptr, &
-         start_routine=c_funloc(fetch_realtime_image_spectrum), &
-         arg=c_loc(cluster_req))
+                            attr=c_null_ptr, &
+                            start_routine=c_funloc(fetch_realtime_image_spectrum), &
+                            arg=c_loc(cluster_req))
 
       ! end of cluster
 
@@ -6010,9 +6014,9 @@ contains
 
       ! launch a thread
       rc = c_pthread_create(thread=pid, &
-         attr=c_null_ptr, &
-         start_routine=c_funloc(fetch_realtime_image_spectrum), &
-         arg=c_loc(cluster_req))
+                            attr=c_null_ptr, &
+                            start_routine=c_funloc(fetch_realtime_image_spectrum), &
+                            arg=c_loc(cluster_req))
 
       ! end of cluster
 
@@ -6119,11 +6123,11 @@ contains
          ! the image (viewport) part
          if (req%image) then
             select case (req%quality)
-             case (low)
+            case (low)
                precision = ZFP_LOW_PRECISION
-             case (high)
+            case (high)
                precision = ZFP_HIGH_PRECISION
-             case default
+            case default
                precision = ZFP_MEDIUM_PRECISION
             end select
 
@@ -6218,11 +6222,11 @@ contains
       mask(x1:x2, y1:y2) = item%mask(x1:x2, y1:y2)
 
       select case (req%quality)
-       case (low)
+      case (low)
          precision = ZFP_LOW_PRECISION
-       case (high)
+      case (high)
          precision = ZFP_HIGH_PRECISION
-       case default
+      case default
          precision = ZFP_MEDIUM_PRECISION
       end select
 
@@ -6607,9 +6611,9 @@ contains
 
       ! launch a pthread
       rc = c_pthread_create(thread=pid, &
-         attr=c_null_ptr, &
-         start_routine=c_funloc(fetch_global_statistics), &
-         arg=c_loc(req))
+                            attr=c_null_ptr, &
+                            start_routine=c_funloc(fetch_global_statistics), &
+                            arg=c_loc(req))
 
       ! calculate global statistics locally
       call calculate_global_statistics(item, item%dmedian, sumP, countP, sumN, countN, req%first, req%last)
@@ -6798,9 +6802,9 @@ contains
 
          ! launch a pthread
          rc = c_pthread_create(thread=pid, &
-            attr=c_null_ptr, &
-            start_routine=c_funloc(fetch_video_frame), &
-            arg=c_loc(fetch_req))
+                               attr=c_null_ptr, &
+                               start_routine=c_funloc(fetch_video_frame), &
+                               arg=c_loc(fetch_req))
 
          ! join a thread
          rc = c_pthread_join(pid, c_null_ptr)
@@ -7239,9 +7243,9 @@ contains
 
       ! launch a thread
       rc = c_pthread_create(thread=pid, &
-         attr=c_null_ptr, &
-         start_routine=c_funloc(fetch_realtime_image_spectrum), &
-         arg=c_loc(cluster_req))
+                            attr=c_null_ptr, &
+                            start_routine=c_funloc(fetch_realtime_image_spectrum), &
+                            arg=c_loc(cluster_req))
 
       ! end of cluster
 
@@ -7341,11 +7345,11 @@ contains
       if (req%fd .ne. -1) then
 
          select case (req%quality)
-          case (low)
+         case (low)
             precision = ZFP_LOW_PRECISION
-          case (high)
+         case (high)
             precision = ZFP_HIGH_PRECISION
-          case default
+         case default
             precision = ZFP_MEDIUM_PRECISION
          end select
 
