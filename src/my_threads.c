719233955554c7f6d568e309023e1b557fef2775
jvo203 <chris.zapart@nao.ac.jp>
2022-08-24 16:13:20 +0900

    modified:   src/my_threads.c

--- src/my_threads.c
+++ src/my_threads.c
@@ -4,20 +4,14 @@
 
 extern int my_pthread_create(pthread_t *thread, void *(*start_routine)(void *), void *arg)
 {
-    int rc;
-
-    rc = pthread_create(thread, NULL, start_routine, arg);
+    int rc = pthread_create(thread, NULL, start_routine, arg);
     printf("pthread_create rc = %d\n", rc);
-
     return rc;
 }
 
 extern int my_pthread_join(pthread_t thread)
 {
-    int rc;
-
-    rc = pthread_join(thread, NULL);
+    int rc = pthread_join(thread, NULL);
     printf("pthread_join rc = %d\n", rc);
-
     return rc;
 }
