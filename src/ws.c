71a7016ce60d9513d0f765bf2f5d51a0333791e6
jvo203 <chris.zapart@nao.ac.jp>
2022-09-08 14:10:14 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -524,6 +524,28 @@ static void mg_http_ws_callback(struct mg_connection *c, int ev, void *ev_data, 
         // [WS] {"type":"pv","x1":108,"y1":127,"x2":131,"y2":99,"width":1129,"height":801,"frame_start":146830393957.08142,"frame_end":147767129569,"ref_freq":147300000000,"deltaV":0,"rest":false,"timestamp":10713.300000000745}
         if (strcmp(type, "pv") == 0)
         {
+            struct pv_request *req = (struct pv_request *)malloc(sizeof(struct pv_request));
+
+            if (req == NULL)
+                break;
+
+            // default values just in case ...
+            req->x1 = -1;
+            req->y1 = -1;
+            req->x2 = -1;
+            req->y2 = -1;
+            req->width = 0;
+            req->height = 0;
+            req->frame_start = 0.0;
+            req->frame_end = 0.0;
+            req->ref_freq = 0.0;
+            req->deltaV = 0.0;
+            req->rest = false;
+            req->timestamp = 0.0;
+            req->fd = -1;
+            req->ptr = NULL;
+
+            free(req);
             break;
         }
 
