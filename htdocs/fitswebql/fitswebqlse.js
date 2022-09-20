66d7fee6f1ee7e40403221bf811938d6b6149d4d
jvo203 <chris.zapart@nao.ac.jp>
2022-09-20 09:20:09 +0900

    modified:   htdocs/fitswebql/fitswebqlse.js

--- htdocs/fitswebql/fitswebqlse.js
+++ htdocs/fitswebql/fitswebqlse.js
@@ -1,5 +1,5 @@
 function get_js_version() {
-	return "JS2022-09-16.0";
+	return "JS2022-09-20.0";
 }
 
 function uuidv4() {
@@ -3258,8 +3258,11 @@ function open_websocket_connection(_datasetId, index) {
 
 						if (id == "ZFP") {
 							// decompress ZFP							
-							var res = Module.decompressZFPimage(img_width, img_height, frame_pv);
-							const pv = Module.HEAPF32.slice(res[0] / 4, res[0] / 4 + res[1]);
+							/*var res = Module.decompressZFPimage(img_width, img_height, frame_pv);
+							const pv = Module.HEAPF32.slice(res[0] / 4, res[0] / 4 + res[1]);*/
+
+							var res = Module.decompressPVdiagram(img_width, img_height, frame_pv);
+							pv = new Uint8ClampedArray(Module.HEAPU8.subarray(res[0], res[0] + res[1])); // it's OK to use .subarray() instead of .slice() as a copy is made in "new Uint8ClampedArray()"
 							console.log(pv);
 						}
 
