9f27194b080ce6571a9e0f3d9886ad65cfc0f0d5
jvo203 <chris.zapart@nao.ac.jp>
2022-10-24 16:45:27 +0900

    modified:   src/ring_buffer.c

--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -1,1 +1,14 @@
 #include "ring_buffer.h"
+
+/*void put(int item)
+{
+    buf[end++] = item;
+    end %= BUFLEN;
+}*/
+
+/*int get()
+{
+    int item = buf[start++];
+    start %= BUFLEN;
+    return item;
+}*/
