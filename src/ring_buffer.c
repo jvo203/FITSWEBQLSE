9c2b258104081e0b1dcf7e26a4f51ddcd112fa8b
jvo203 <chris.zapart@nao.ac.jp>
2022-10-24 16:58:24 +0900

    modified:   src/ring_buffer.c

--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -1,4 +1,28 @@
+#include <stdlib.h>
+
 #include "ring_buffer.h"
+
+void init_ring_buffer(struct ring_buffer *rb)
+{
+    rb->head = 0;
+    rb->tail = 0;
+
+    // init data to NULL
+    for (int i = 0; i < RING_BUFFER_SIZE; i++)
+        rb->data[i] = NULL;
+
+    pthread_mutex_init(&rb->ring_mtx, NULL);
+}
+
+void delete_ring_buffer(struct ring_buffer *rb)
+{
+    pthread_mutex_destroy(&rb->ring_mtx);
+
+    // free all data
+    for (int i = 0; i < RING_BUFFER_SIZE; i++)
+        if (rb->data[i] != NULL)
+            free(rb->data[i]);
+}
 
 /*void put(int item)
 {
