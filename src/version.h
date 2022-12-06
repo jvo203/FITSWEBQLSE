1b3119c209ba0073fddbec510297cd065fe82e80
jvo203 <chris.zapart@nao.ac.jp>
2022-12-06 17:24:09 +0900

    modified:   src/version.h

--- src/version.h
+++ src/version.h
@@ -7,9 +7,9 @@
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
-#define SERVER_STRING                                                  \
-    "FITSWEBQLSE v" STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR( \
-        VERSION_SUB)
+#define SERVER_STRING                                                \
+  "FITSWEBQLSE v" STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR( \
+      VERSION_SUB)
 
-#define WASM_VERSION "22.11.16.0"
+#define WASM_VERSION "22.12.06.0"
 #define VERSION_STRING "F/SV2022-12-01.0-BETA"
