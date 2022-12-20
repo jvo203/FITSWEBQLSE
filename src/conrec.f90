687892d02da7c6879234e73fb16d1f4ba43410df
jvo203 <chris.zapart@nao.ac.jp>
2022-12-20 10:42:25 +0900

    modified:   src/conrec.f90

--- src/conrec.f90
+++ src/conrec.f90
@@ -1,9 +1,9 @@
 module contour
-   ! implicit none
-   use list
+    ! implicit none
+    use list
 
-   private
-   public :: conrec
+    private
+    public :: conrec
 
 contains
 !======================================================================
@@ -27,65 +27,65 @@ contains
 !     real*4 z(1:nc)             ! contour levels in increasing order
 !     real*4 contours(1:nc,1:4)  ! output list of contour coordinates
 
-   subroutine conrec(d,ilb,iub,jlb,jub,x,y,nc,z,contours)
-      real(kind=4) d(ilb:iub,jlb:jub)
-      integer ilb,iub,jlb,jub
-      real(kind=4) x(ilb:iub)
-      real(kind=4) y(jlb:jub)
-      integer nc
-      real(kind=4) z(1:nc)
-      type(list_t), pointer :: contours
+    subroutine conrec(d, ilb, iub, jlb, jub, x, y, nc, z, contours)
+        real(kind=4) d(ilb:iub, jlb:jub)
+        integer ilb, iub, jlb, jub
+        real(kind=4) x(ilb:iub)
+        real(kind=4) y(jlb:jub)
+        integer nc
+        real(kind=4) z(1:nc)
+        type(list_t), pointer :: contours
 !
 !     Local declarations
 !
-      real(kind=4) h(0:4)
-      integer sh(0:4)
-      real(kind=4) xh(0:4),yh(0:4)
-      integer im(1:4),jm(1:4)
-      integer case
-      integer castab(-1:1,-1:1,-1:1)
-      integer p1,p2
+        real(kind=4) h(0:4)
+        integer sh(0:4)
+        real(kind=4) xh(0:4), yh(0:4)
+        integer im(1:4), jm(1:4)
+        integer case
+        integer castab(-1:1, -1:1, -1:1)
+        integer p1, p2
 !
 !     Data
 !
-      data im/0,1,1,0/
-      data jm/0,0,1,1/
-      data castab/0,0,9,  0,1,5,  7,4,8,&
-      &0,3,6,  2,3,2,  6,3,0,&
-      &8,4,7,  5,1,0,  9,0,0/
+        data im/0, 1, 1, 0/
+        data jm/0, 0, 1, 1/
+        data castab/0, 0, 9, 0, 1, 5, 7, 4, 8,&
+        &0, 3, 6, 2, 3, 2, 6, 3, 0,&
+        &8, 4, 7, 5, 1, 0, 9, 0, 0/
 !
 !     Use statement functions for the line intersections
 !
-      xsect(p1,p2) = (h(p2)*xh(p1)-h(p1)*xh(p2))/(h(p2)-h(p1))
-      ysect(p1,p2) = (h(p2)*yh(p1)-h(p1)*yh(p2))/(h(p2)-h(p1))
+        xsect(p1, p2) = (h(p2)*xh(p1) - h(p1)*xh(p2))/(h(p2) - h(p1))
+        ysect(p1, p2) = (h(p2)*yh(p1) - h(p1)*yh(p2))/(h(p2) - h(p1))
 !
 !     Scan the arrays, top down, left to right within rows
 !
-20    do 100 j=jub-1,jlb,-1
-         do 90 i=ilb,iub-1
-            dmin = min(d(i,j),d(i,j+1),d(i+1,j),d(i+1,j+1))
-            dmax = max(d(i,j),d(i,j+1),d(i+1,j),d(i+1,j+1))
-            if (dmax.ge.z(1) .and. dmin.le.z(nc)) then
-               do 80 k=1,nc
-                  if (z(k).ge.dmin .and. z(k).le.dmax) then
-                     do 22 m=4,0,-1
-                        if (m.gt.0) then
-                           h(m)=d(i+im(m),j+jm(m))-z(k)
-                           xh(m)=x(i+im(m))
-                           yh(m)=y(j+jm(m))
-                        else
-                           h(0)=0.25*(h(1)+h(2)+h(3)+h(4))
-                           xh(0)=0.5*(x(i)+x(i+1))
-                           yh(0)=0.5*(y(j)+y(j+1))
-                        endif
-                        if (h(m).gt.0.0) then
-                           sh(m)=+1
-                        else if (h(m).lt.0.0) then
-                           sh(m)=-1
-                        else
-                           sh(m)=0
-                        endif
-22                   continue
+20      do 100 j = jub - 1, jlb, -1
+            do 90 i = ilb, iub - 1
+                dmin = min(d(i, j), d(i, j + 1), d(i + 1, j), d(i + 1, j + 1))
+                dmax = max(d(i, j), d(i, j + 1), d(i + 1, j), d(i + 1, j + 1))
+                if (dmax .ge. z(1) .and. dmin .le. z(nc)) then
+                    do 80 k = 1, nc
+                        if (z(k) .ge. dmin .and. z(k) .le. dmax) then
+                            do 22 m = 4, 0, -1
+                                if (m .gt. 0) then
+                                    h(m) = d(i + im(m), j + jm(m)) - z(k)
+                                    xh(m) = x(i + im(m))
+                                    yh(m) = y(j + jm(m))
+                                else
+                                    h(0) = 0.25*(h(1) + h(2) + h(3) + h(4))
+                                    xh(0) = 0.5*(x(i) + x(i + 1))
+                                    yh(0) = 0.5*(y(j) + y(j + 1))
+                                end if
+                                if (h(m) .gt. 0.0) then
+                                    sh(m) = +1
+                                else if (h(m) .lt. 0.0) then
+                                    sh(m) = -1
+                                else
+                                    sh(m) = 0
+                                end if
+22                              continue
 !
 !     Note: at this stage the relative heights of the corners and the
 !     centre are in the h array, and the corresponding coordinates are
@@ -114,103 +114,103 @@ contains
 !
 !                    Scan each triangle in the box
 !
-                     do 60 m=1,4
-                        m1=m
-                        m2=0
-                        if (m.ne.4) then
-                           m3=m+1
-                        else
-                           m3=1
-                        endif
-                        case = castab(sh(m1),sh(m2),sh(m3))
-                        if (case.ne.0) then
-                           goto (31,32,33,34,35,36,37,38,39),case
+                                do 60 m = 1, 4
+                                    m1 = m
+                                    m2 = 0
+                                    if (m .ne. 4) then
+                                        m3 = m + 1
+                                    else
+                                        m3 = 1
+                                    end if
+                                    case = castab(sh(m1), sh(m2), sh(m3))
+                                    if (case .ne. 0) then
+                                        goto(31, 32, 33, 34, 35, 36, 37, 38, 39), case
 !
 !     Case 1 - Line between vertices 1 and 2
 !
-31                         x1=xh(m1)
-                           y1=yh(m1)
-                           x2=xh(m2)
-                           y2=yh(m2)
-                           goto 40
+31                                      x1 = xh(m1)
+                                        y1 = yh(m1)
+                                        x2 = xh(m2)
+                                        y2 = yh(m2)
+                                        goto 40
 !
 !     Case 2 - Line between vertices 2 and 3
 !
-32                         x1=xh(m2)
-                           y1=yh(m2)
-                           x2=xh(m3)
-                           y2=yh(m3)
-                           goto 40
+32                                      x1 = xh(m2)
+                                        y1 = yh(m2)
+                                        x2 = xh(m3)
+                                        y2 = yh(m3)
+                                        goto 40
 !
 !     Case 3 - Line between vertices 3 and 1
 !
-33                         x1=xh(m3)
-                           y1=yh(m3)
-                           x2=xh(m1)
-                           y2=yh(m1)
-                           goto 40
+33                                      x1 = xh(m3)
+                                        y1 = yh(m3)
+                                        x2 = xh(m1)
+                                        y2 = yh(m1)
+                                        goto 40
 !
 !     Case 4 - Line between vertex 1 and side 2-3
 !
-34                         x1=xh(m1)
-                           y1=yh(m1)
-                           x2=xsect(m2,m3)
-                           y2=ysect(m2,m3)
-                           goto 40
+34                                      x1 = xh(m1)
+                                        y1 = yh(m1)
+                                        x2 = xsect(m2, m3)
+                                        y2 = ysect(m2, m3)
+                                        goto 40
 !
 !     Case 5 - Line between vertex 2 and side 3-1
 !
-35                         x1=xh(m2)
-                           y1=yh(m2)
-                           x2=xsect(m3,m1)
-                           y2=ysect(m3,m1)
-                           goto 40
+35                                      x1 = xh(m2)
+                                        y1 = yh(m2)
+                                        x2 = xsect(m3, m1)
+                                        y2 = ysect(m3, m1)
+                                        goto 40
 !
 !     Case 6 - Line between vertex 3 and side 1-2
 !
-36                         x1=xh(m3)
-                           y1=yh(m3)
-                           x2=xsect(m1,m2)
-                           y2=ysect(m1,m2)
-                           goto 40
+36                                      x1 = xh(m3)
+                                        y1 = yh(m3)
+                                        x2 = xsect(m1, m2)
+                                        y2 = ysect(m1, m2)
+                                        goto 40
 !
 !     Case 7 - Line between sides 1-2 and 2-3
 !
-37                         x1=xsect(m1,m2)
-                           y1=ysect(m1,m2)
-                           x2=xsect(m2,m3)
-                           y2=ysect(m2,m3)
-                           goto 40
+37                                      x1 = xsect(m1, m2)
+                                        y1 = ysect(m1, m2)
+                                        x2 = xsect(m2, m3)
+                                        y2 = ysect(m2, m3)
+                                        goto 40
 !
 !     Case 8 - Line between sides 2-3 and 3-1
 !
-38                         x1=xsect(m2,m3)
-                           y1=ysect(m2,m3)
-                           x2=xsect(m3,m1)
-                           y2=ysect(m3,m1)
-                           goto 40
+38                                      x1 = xsect(m2, m3)
+                                        y1 = ysect(m2, m3)
+                                        x2 = xsect(m3, m1)
+                                        y2 = ysect(m3, m1)
+                                        goto 40
 !
 !     Case 9 - Line between sides 3-1 and 1-2
 !
-39                         x1=xsect(m3,m1)
-                           y1=ysect(m3,m1)
-                           x2=xsect(m1,m2)
-                           y2=ysect(m1,m2)
-                           goto 40
-40                         call vecout(x1,y1,x2,y2,z(k))
-                        endif
-60                   continue
-                  endif
-80             continue
-            endif
-90       continue
-100   continue
-      return
-   end
+39                                      x1 = xsect(m3, m1)
+                                        y1 = ysect(m3, m1)
+                                        x2 = xsect(m1, m2)
+                                        y2 = ysect(m1, m2)
+                                        goto 40
+40                                      call vecout(x1, y1, x2, y2, z(k))
+                                    end if
+60                                  continue
+                                    end if
+80                                  continue
+                                    end if
+90                                  continue
+100                                 continue
+                                    return
+                                end
 
-   subroutine vecout(x1,y1,x2,y2,z)
-      implicit none
-      real(kind=4) x1,y1,x2,y2,z
+                                subroutine vecout(x1, y1, x2, y2, z)
+                                    implicit none
+                                    real(kind=4) x1, y1, x2, y2, z
 !
 !***** Replace from here
 !
@@ -218,17 +218,18 @@ contains
 !     the version of FORTRAN running on the Macintosh microcomputer
 !     on which this particular example was written.
 !
-      ! INTEGER LINETO
-      ! PARAMETER (LINETO=Z'89109000')
-      ! INTEGER MOVETO
-      ! PARAMETER (MOVETO=Z'89309000')
-      ! call toolbx(MOVETO,nint(x1),nint(y1))
-      ! call toolbx(LINETO,nint(x2),nint(y2))
+                                    ! INTEGER LINETO
+                                    ! PARAMETER (LINETO=Z'89109000')
+                                    ! INTEGER MOVETO
+                                    ! PARAMETER (MOVETO=Z'89309000')
+                                    ! call toolbx(MOVETO,nint(x1),nint(y1))
+                                    ! call toolbx(LINETO,nint(x2),nint(y2))
 
-      ! print *, 'x1,y1,x2,y2,z = ', x1,y1,x2,y2,z
+                                    ! print *, 'x1,y1,x2,y2,z = ', x1,y1,x2,y2,z
+                                    ! call list_insert(ll, pos)
 !
 !***** To here
 !
-      return
-   end
-end module contour
+                                    return
+                                end
+                                end module contour
