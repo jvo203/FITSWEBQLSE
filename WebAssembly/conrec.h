391c01bdeb04be6fd23f2537e3dc4b98165b671e
jvo203 <chris.zapart@nao.ac.jp>
2022-12-22 13:33:50 +0900

    new file:   WebAssembly/conrec.c

---
+++ WebAssembly/conrec.h
@@ -0,0 +1,10 @@
+#pragma once
+int conrec(double **d,
+           int ilb,
+           int iub,
+           int jlb,
+           int jub,
+           double *x,
+           double *y,
+           int nc,
+           double *z);
