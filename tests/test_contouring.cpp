b2f16605dfc8503b2b30d3c7e31c9510df1c4271
jvo203 <chris.zapart@nao.ac.jp>
2022-12-23 15:25:45 +0900

    modified:   tests/test_contouring.cpp

--- tests/test_contouring.cpp
+++ tests/test_contouring.cpp
@@ -12,6 +12,7 @@ using namespace std;
 #define max(x, y) (x > y ? x : y)
 
 // set a single pixel on an RGBA canvas
+// was int x, int y but changed to int y, int x (CONREC uses a reversed order? FORTRAN?)
 void set_pixel(unsigned char *buffer, int width, int height, int y, int x, unsigned char r, unsigned char g, unsigned char b, unsigned char a)
 {
     if (x < 0 || x >= width || y < 0 || y >= height)
