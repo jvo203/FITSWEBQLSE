02463fd34403faa1e35c6202c922e25e31b021b3
jvo203 <chris.zapart@nao.ac.jp>
2023-01-27 11:23:00 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -2721,7 +2721,8 @@ contains
       if ((current .eq. total) .and. (total .gt. 0)) then
          call set_ok_status(item, .true.)
 
-         if ((total .gt. 1) .and. (.not. item%video)) then
+         if (item%naxes(3) .gt. 1 .and. (.not. item%video)) then
+            ! if ((total .gt. 1) .and. (.not. item%video)) then
             if (allocated(item%frame_min)) item%dmin = minval(item%frame_min)
             if (allocated(item%frame_max)) item%dmax = maxval(item%frame_max)
             if (allocated(item%frame_median)) item%dmedian = &
