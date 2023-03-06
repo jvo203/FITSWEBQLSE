47804b0fe19a04fcabbda8ba03e882fee578824b
jvo203 <chris.zapart@nao.ac.jp>
2023-03-06 11:18:06 +0900

    modified:   src/http.c

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 0
-#define VERSION_SUB 24
+#define VERSION_SUB 23
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
@@ -12,4 +12,4 @@
         VERSION_SUB)
 
 #define WASM_VERSION "23.01.11.0"
-#define VERSION_STRING "F/SV2023-03-04.0-BETA"
+#define VERSION_STRING "F/SV2023-03-06.0-BETA"
