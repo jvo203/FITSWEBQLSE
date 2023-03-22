8cd9a7a886c727c0384862f4f14f15d7e23a28a7
jvo203 <chris.zapart@nao.ac.jp>
2023-03-22 17:19:57 +0900

    modified:   src/zpipe.c

--- src/zpipe.c
+++ src/zpipe.c
@@ -17,14 +17,6 @@
 #include <assert.h>
 #include "zlib.h"
 
-#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
-#include <fcntl.h>
-#include <io.h>
-#define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
-#else
-#define SET_BINARY_MODE(file)
-#endif
-
 #define CHUNK 16384
 
 /* Decompress from file source to file dest until stream ends or EOF.
