c635b4cf46a687ef4a3b2ae303d8b989dbc4e487
jvo203 <chris.zapart@nao.ac.jp>
2023-03-22 12:23:11 +0900

    modified:   src/http.c

--- src/http.c
+++ src/http.c
@@ -1463,6 +1463,9 @@ static void *handle_url_download(void *arg)
             {
                 bool error = false;
 
+                if (get_error_status(item))
+                    error = true;
+
                 if (!get_header_status(item) || !get_ok_status(item))
                 {
                     set_error_status_C(item, true);
