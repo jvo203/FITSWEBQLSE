ba4d3ffe21168956024f13903f147402ea8a7e21
jvo203 <chris.zapart@nao.ac.jp>
2023-03-24 15:08:48 +0900

    modified:   src/unzip.c

--- src/unzip.c
+++ src/unzip.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "junzip.h"
 
 int unzip(int fdin, int fdout)
 {
-    return -1;
+    FILE *fp;
+    int retval = -1;
+
+    JZFile *zip;
+
+    fp = fdopen(fdin, "rb");
+    if (!fp)
+    {
+        /* handle error */
+        printf("[C] fdopen failed.\n");
+        return -1;
+    }
+
+    zip = jzfile_from_stdio_file(fp);
+    if (zip == NULL)
+    {
+        printf("[C] jzfile_from_stdio_file failed.\n");
+        goto endClose;
+    }
+
+endClose:
+    if (zip != NULL)
+        zip->close(zip);
+
+    fclose(fp);
+    close(fdout);
+
+    return retval;
 }
