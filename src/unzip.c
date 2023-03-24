3ca9a0d2fc71efa2bd03b0c839575f196effa3df
jvo203 <chris.zapart@nao.ac.jp>
2023-03-24 11:58:48 +0900

    modified:   src/unzip.c

--- src/unzip.c
+++ src/unzip.c
@@ -1,19 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include "junzip.h"
+
+int processFile(JZFile *zip, int fdout)
+{
+    JZFileHeader header;
+    char filename[1024];
+    unsigned char *data;
+
+    if (jzReadLocalFileHeader(zip, &header, filename, sizeof(filename)))
+    {
+        printf("Couldn't read local file header!");
+        return -1;
+    }
+
+    if ((data = (unsigned char *)malloc(header.uncompressedSize)) == NULL)
+    {
+        printf("Couldn't allocate memory!");
+        return -1;
+    }
+
+    printf("%s, %d / %d bytes at offset %08X\n", filename,
+           header.compressedSize, header.uncompressedSize, header.offset);
+
+    if (jzReadData(zip, &header, data) != Z_OK)
+    {
+        printf("Couldn't read file data!");
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    return 0;
+}
 
 int unzip(int fdin, int fdout)
 {
     FILE *f;
     JZFile *zip;
+    JZEndRecord endRecord;
+
+    int retval = -1;
 
     f = fdopen(fdin, "rb");
     if (!f)
         return -1;
 
+    zip = jzfile_from_stdio_file(f);
+
+    if (jzReadEndRecord(zip, &endRecord))
+    {
+        printf("[C] Couldn't read ZIP file end record\n.");
+        goto endClose;
+    }
+
+    retval = processFile(zip, fdout);
+
+endClose:
+    zip->close(zip);
     fclose(f);
     close(fdout);
 
-    return 0;
+    return retval;
 }
