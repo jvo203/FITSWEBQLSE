2346bb12850edf4c8f0cdcfd75c42cb40db9fef6
jvo203 <chris.zapart@nao.ac.jp>
2023-03-30 13:12:57 +0900

    modified:   src/facil_http.c

--- src/facil_http.c
+++ src/facil_http.c
@@ -18,6 +18,8 @@ void on_request(http_s *request);
 
 void start_facil()
 {
+    signal(SIGPIPE, SIG_IGN); // ignore SIGPIPE
+
 #ifdef SHARE
     int rc = sqlite3_open_v2(SHARE "/splatalogue_v3.db", &splat_db, SQLITE_OPEN_READONLY | SQLITE_OPEN_FULLMUTEX, NULL);
 #else
@@ -36,15 +38,19 @@ void start_facil()
     // convert options.http_port to a string
     sprintf(port_s, "%" PRIu16 "", options.http_port);
 
+#ifdef DEBUG
+    printf("[C] Starting facil.io HTTP daemon listening on port %s... Press CTRL-C to stop it.\n", port_s);
+#endif
+
     // listen on port <options.http_port> and any available network binding (NULL == 0.0.0.0)
     if (http_listen(port_s, NULL, .on_request = on_request, .log = 1) == -1)
     {
-        perror("ERROR: facil.io couldn't initialize HTTP service (already running?)");
+        perror("[C] ERROR: facil.io couldn't initialize HTTP service (already running?)");
         exit(1);
     }
 
     // start the server
-    fio_start(.threads = 1);
+    fio_start(.workers = -2, .threads = -2);
 
     if (splat_db != NULL)
     {
