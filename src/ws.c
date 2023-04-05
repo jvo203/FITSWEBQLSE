7e58536ea0a5311cfc98893564cfe99e03ee54e0
jvo203 <chris.zapart@nao.ac.jp>
2023-04-05 13:21:40 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -189,7 +189,7 @@ static void mg_http_ws_callback(struct mg_connection *c, int ev, void *ev_data, 
     {
         struct mg_http_message *hm = (struct mg_http_message *)ev_data;
 
-        MG_INFO(("New request to: [%.*s], body size: %lu", (int)hm->uri.len, hm->uri.ptr, (unsigned long)hm->body.len));
+        // MG_INFO(("New request to: [%.*s], body size: %lu", (int)hm->uri.len, hm->uri.ptr, (unsigned long)hm->body.len));
 
         // open a WebSocket request
         if (mg_strstr(hm->uri, mg_str("/websocket")) != NULL)
@@ -275,6 +275,7 @@ static void mg_http_ws_callback(struct mg_connection *c, int ev, void *ev_data, 
             if (datasetId != NULL)
             {
                 datasetId++; // skip the slash character
+                printf("<heartbeat> request for '%s'\n", datasetId);
 
                 void *item = get_dataset(datasetId);
 
