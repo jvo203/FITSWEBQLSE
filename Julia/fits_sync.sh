bb20b35f2705834110148cf802295ab062e7efa5
jvo203 <chris.zapart@nao.ac.jp>
2023-04-10 14:25:25 +0900

    modified:   fits_sync.sh

--- Julia/fits_sync.sh
+++ Julia/fits_sync.sh
@@ -8,3 +8,12 @@ DATE=$(date -d "$DATE - 7 days" +%Y-%m-%d)
 
 # print it
 echo "Starting sync for $DATE"
+
+# make a sync command (run julia fits_sync $DATE)
+CMD="julia /home/chris/Julia/fits_sync.jl $DATE"
+
+# print the command
+echo $CMD
+
+# run the command
+$CMD
