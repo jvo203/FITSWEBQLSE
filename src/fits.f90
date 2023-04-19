80f3c96bf8c524fcb4949158d98f9719baaeded0
jvo203 <chris.zapart@nao.ac.jp>
2023-04-19 12:54:54 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -3130,7 +3130,7 @@ contains
          if (counter .gt. 0) then
             repeat = repeat + 1
             print *, item%datasetid, "::'submit_progress' failed, counter = ", counter, ", #repeats:", repeat
-            ! call sleep(1) ! 1 sec.
+            call sleep(1) ! 1 sec.
          end if
 
          ! break the loop after 60s
