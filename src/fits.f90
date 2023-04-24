10247b8140c5c7bf1572b60faa905bb483841480
jvo203 <chris.zapart@nao.ac.jp>
2023-04-24 16:43:57 +0900

    modified:   src/fits.f90

--- src/fits.f90
+++ src/fits.f90
@@ -1943,7 +1943,8 @@ contains
       bSuccess = .false.
 
       print *, "load_dataset::cache = ", trim(cache)
-      file = cache//'/'//'state'
+      print *, "load_dataset::cache+state = ", trim(cache)//'/'//'state'
+      file = trim(cache)//'/'//'state'
       print *, "load_dataset::file = ", trim(file)
 
       INQUIRE (FILE=trim(file), EXIST=file_exists)
