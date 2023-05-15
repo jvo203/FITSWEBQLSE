05984b3390c9f796367977f6bdf44c561677e2af
jvo203 <chris.zapart@nao.ac.jp>
2023-05-15 15:43:34 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -511,7 +511,10 @@ static void mg_http_ws_callback(struct mg_connection *c, int ev, void *ev_data, 
 
         // reject connections without an entry in a hash table
         if (!dataset_exists(datasetId))
+        {
+            printf("[C] rejecting the connection for '%s'.\n", datasetId);
             c->is_closing = 1; // Tell mongoose to close this connection
+        }
         else
         {
             struct websocket_session *session = (struct websocket_session *)malloc(sizeof(struct websocket_session));
