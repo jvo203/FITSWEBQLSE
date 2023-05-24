9ba75dfaf985c1a4e91b860c63f650cf280647f9
jvo203 <chris.zapart@nao.ac.jp>
2023-05-24 17:45:30 +0900

    modified:   src/ws.c

--- src/ws.c
+++ src/ws.c
@@ -2232,6 +2232,10 @@ extern void close_pipe(int fd)
 {
     int status;
 
+#ifdef DEBUG
+    printf("[C] closing pipe %d.\n", fd);
+#endif
+
     // close a pipe (to be called from Fortran)
     status = close(fd);
 
