6dfaa5a0d625b5bf46aa9ad25224daab43baae2f
jvo203 <chris.zapart@nao.ac.jp>
2023-08-25 12:00:52 +0900

    modified:   htdocs/fitswebql/fitswebqlse.js

--- htdocs/fitswebql/fitswebqlse.js
+++ htdocs/fitswebql/fitswebqlse.js
@@ -1,5 +1,5 @@
 function get_js_version() {
-    return "JS2023-08-25.1";
+    return "JS2023-08-25.2";
 }
 
 function uuidv4() {
@@ -5295,7 +5295,7 @@ function HaversineDistance(ra1, dec1, ra2, dec2) {
     console.log("dRA=", dRA, "dDEC=", dDEC);
 
     var a = Math.sin(dDEC / 2) * Math.sin(dDEC / 2) + Math.cos(dec1) * Math.cos(dec2) * Math.sin(dRA / 2) * Math.sin(dRA / 2);
-    var b = Math.asin(Math.sqrt(a)); // works better for small angles (where a is near 0)
+    var b = 2 * Math.asin(Math.sqrt(a)); // works better for small angles (where a is near 0)
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     console.log("a=", a, "b=", b, "c=", c);
