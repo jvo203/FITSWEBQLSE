2e531a9fe67380bbe8ac65323f90576192ae00e4
jvo203 <chris.zapart@nao.ac.jp>
2023-10-02 08:58:13 +0900

    modified:   Julia/rbf.jl

--- Julia/rbf.jl
+++ Julia/rbf.jl
@@ -1,12 +1,11 @@
 # read all Float64 values from a text file wts.dat
 # and store them in a vector
 
-# open the file
-f = open("wts.dat")
-
 # read all the values from the file
 # and store them in a vector
 wts = Float64[]
+
+f = open("wts.dat")
 while !eof(f)
     push!(wts, parse(Float64, readline(f)))
 end
@@ -16,3 +15,20 @@ close(f)
 
 # print the vector
 println(wts)
+
+# read the centres three Float64 values per line
+# and store them in an array of arrays
+# centres = Array{Float64,2}(undef, 0, 3)
+centres = []
+
+f = open("centres.dat")
+while !eof(f)
+    line = readline(f)
+    push!(centres, parse.(Float64, split(line)))
+end
+
+# close the file
+close(f)
+
+# print the array
+println(centres)
