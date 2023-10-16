e974d7da813a59cb7f52a090e71813496f37f516
jvo203 <chris.zapart@nao.ac.jp>
2023-10-16 10:12:50 +0900

    modified:   tests/testWCS.jl

--- tests/testWCS.jl
+++ tests/testWCS.jl
@@ -15,8 +15,8 @@ wcs = wcs_array[1]
 # print the WCS object
 println(wcs)
 
-physical = [856.49056, 438.4528, 136, 1.0] # 0-indexed (but ds9 seems to use 1-indexed)
-fk5 = [261.2105354, -34.2435452, 9.317640936376723e10, 1.0] # 0-indexed (but ds9 seems to use 1-indexed)
+physical = [856.49056, 438.4528, 136, 1.0] # 0-indexed (but ds9 seems to use 1-indexed for the z-axis)
+fk5 = [261.2105354, -34.2435452, 9.317640936376723e10, 1.0] # 0-indexed (but ds9 seems to use 1-indexed for the z-axis)
 
 println("Physical: ", physical)
 println("FK5: ", fk5)
