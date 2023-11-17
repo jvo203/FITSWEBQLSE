c7f5e380002b5891daf3ebd06c461325af107a0c
jvo203 <chris.zapart@nao.ac.jp>
2023-11-17 11:47:27 +0900

    modified:   src/version.h

--- src/version.h
+++ src/version.h
@@ -2,7 +2,7 @@
 
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 0
-#define VERSION_SUB 50
+#define VERSION_SUB 51
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
@@ -12,4 +12,4 @@
         VERSION_SUB)
 
 #define WASM_VERSION "23.11.15.0"
-#define VERSION_STRING "F/SV2023-11-15.0-BETA"
+#define VERSION_STRING "F/SV2023-11-17.0-BETA"
