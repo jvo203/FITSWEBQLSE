2fa2782b17ae35fb3b66233256c2785e67f3efa6
jvo203 <chris.zapart@nao.ac.jp>
2023-12-01 12:22:41 +0900

    modified:   Julia/stress_test.jl

--- Julia/stress_test.jl
+++ Julia/stress_test.jl
@@ -6,7 +6,20 @@ function get_dataset_url(host, port, id)
     return "http://" * host * ":" * port * "/fitswebql/FITSWebQL.html?db=alma&table=cube&datasetId=" * id
 end
 
-datasets = ["ALMA01047077", "ALMA01018218", "ALMA01003454", "ALMA01575449", "ALMA01015786", "ALMA01084695"]
+function get_dataset(host, port, id)
+    url = get_dataset_url(host, port, id)
+    return HTTP.get(url)
+end
 
 host = "localhost"
 port = "8080"
+datasets = ["ALMA01047077", "ALMA01018218", "ALMA01003454", "ALMA01575449", "ALMA01015786", "ALMA01084695"]
+
+for dataset in datasets
+    resp = get_dataset(host, port, dataset)
+
+    # check the HTTP response code
+    if resp.status == 200
+        println(resp)
+    end
+end
