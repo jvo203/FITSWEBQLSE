3cdffa5d401ad957f6ace5be7f03cda5eee147d8
jvo203 <chris.zapart@nao.ac.jp>
2024-01-07 15:41:43 +0900

    modified:   Julia/stress_test.jl

--- Julia/stress_test.jl
+++ Julia/stress_test.jl
@@ -384,7 +384,7 @@ end
 responses::Int64 = 0
 total_time::Float64 = 0.0
 stat = RemoteChannel(() -> Channel{Float64}(32))
-hist = Hist1D(Float64, bins=0.0:10.0:1000.0, overflow=true)
+hist = Hist1D(Float64, bins=0.0:10.0:2000.0, overflow=true)
 
 stat_task = @async while true
     global responses, total_time
