127d1a4f245a506fb7bbc9e72f5330b2df30f7e9
jvo203 <chris.zapart@nao.ac.jp>
2024-03-15 13:53:45 +0900

    modified:   Makefile

--- src/ws.h
+++ src/ws.h
@@ -51,7 +51,7 @@ typedef struct
 
     /* a mongoose Single-Producer Single-Consumer queue, writes to be protected by a spinlock or a mutex */
     struct mg_queue queue;
-#if !defined(__APPLE__) || !defined(__MACH__)
+#if (!defined(__APPLE__) || !defined(__MACH__)) && defined(SPIN)
     // use a spinlock on Linux
     pthread_spinlock_t queue_lock;
 #else
