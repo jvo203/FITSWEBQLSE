fc2bbf19b242c91a1195f910cea2a936b3e1a287
jvo203 <chris.zapart@nao.ac.jp>
2024-05-13 17:40:26 +0900

    modified:   WebAssembly/Rust/rwcs/src/lib.rs

--- WebAssembly/Rust/rwcs/src/lib.rs
+++ WebAssembly/Rust/rwcs/src/lib.rs
@@ -7,8 +7,7 @@ use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
 cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
+    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
     if #[cfg(feature = "wee_alloc")] {
         extern crate wee_alloc;
         #[global_allocator]
