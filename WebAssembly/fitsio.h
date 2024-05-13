e77a2825b9727c3033c352559cf61dff3125dbe8
jvo203 <chris.zapart@nao.ac.jp>
2024-05-13 12:04:08 +0900

    new file:   WebAssembly/fitsio.h

---
+++ WebAssembly/fitsio.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define FLEN_VALUE 71 /* max length of a keyword value string */
--- WebAssembly/wcs.h
+++ WebAssembly/wcs.h
@@ -1,16 +1,18 @@
 #pragma once
 
+#include "fitsio.h"
+
 // CFITSIO-based WCS
 struct fitswcs
 {
-    double xrval;  /* O - X reference value           */
-    double yrval;  /* O - Y reference value           */
-    double xrpix;  /* O - X reference pixel           */
-    double yrpix;  /* O - Y reference pixel           */
-    double xinc;   /* O - X increment per pixel       */
-    double yinc;   /* O - Y increment per pixel       */
-    double rot;    /* O - rotation angle (degrees)    */
-    char type[80]; /* O - type of projection ('-tan') */
+    double xrval;          /* O - X reference value           */
+    double yrval;          /* O - Y reference value           */
+    double xrpix;          /* O - X reference pixel           */
+    double yrpix;          /* O - Y reference pixel           */
+    double xinc;           /* O - X increment per pixel       */
+    double yinc;           /* O - Y increment per pixel       */
+    double rot;            /* O - rotation angle (degrees)    */
+    char type[FLEN_VALUE]; /* O - type of projection ('-tan') */
 };
 
 int myffgics(char *header, int nkeyrec, /* I - FITS header pointer           */
