868504b813e4a2646f0d2e33947f7f9c6fa77332
jvo203 <chris.zapart@nao.ac.jp>
2024-05-13 14:07:27 +0900

    modified:   WebAssembly/wcs.c

--- WebAssembly/wcs.h
+++ WebAssembly/wcs.h
@@ -25,3 +25,11 @@ int myffgics(char *header, int nkeyrec, /* I - FITS header pointer           */
              double *yinc,              /* O - Y increment per pixel       */
              double *rot,               /* O - rotation angle (degrees)    */
              char *type);               /* O - type of projection ('-tan') */
+
+int ffwldp(double xpix, double ypix, double xref, double yref,
+           double xrefpix, double yrefpix, double xinc, double yinc, double rot,
+           char *type, double *xpos, double *ypos, int *status);
+
+int myffxypx(double xpos, double ypos, double xref, double yref,
+             double xrefpix, double yrefpix, double xinc, double yinc, double rot,
+             char *type, double *xpix, double *ypix, int *status);
