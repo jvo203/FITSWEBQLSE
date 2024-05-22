ddac9d81290aca95ba49daa5709a575c40133e88
jvo203 <chris.zapart@nao.ac.jp>
2024-05-22 16:50:26 +0900

    modified:   tests/wcs/src/main.rs

--- tests/wcs/src/main.rs
+++ tests/wcs/src/main.rs
@@ -1,3 +1,18 @@
+use core::str;
+use std::fs::File;
+use std::io::BufReader;
+
+use fitsrs::fits::Fits;
+use wcs::{ImgXY, LonLat, WCS};
+
+pub fn test_wcs(str: &str) {
+    let fits_file = File::open(str).unwrap();
+    let mut reader = BufReader::new(fits_file);
+    let Fits { hdu } = Fits::from_reader(&mut reader).unwrap();
+    let header = hdu.get_header();
+    let wcs = WCS::new(&header).unwrap();
+}
+
 fn main() {
     println!("Comparing the WCS functions with WCSLIB / WCSTools.");
 }
