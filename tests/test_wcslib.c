e9661e29aff84169b7cc059e31436e05dd9421e2
jvo203 <chris.zapart@nao.ac.jp>
2024-05-22 14:04:18 +0900

    modified:   tests/test_wcslib.c

--- tests/test_wcslib.c
+++ tests/test_wcslib.c
@@ -29,21 +29,12 @@ void printerror(int status)
     return;
 }
 
-int main()
+void test_wcs(const char *filename, const double x, const double y, const double ra, const double dec)
 {
     fitsfile *fptr = NULL; /* pointer to the FITS file, defined in fitsio.h */
     struct wcsprm *wcs = NULL;
 
-    int status, nfound, anynull;
-    long naxes[2], fpixel, npixels, ii;
-
-    float *pixels = NULL;
-    char *mask = NULL;
-
-    // char filename[] = "/mnt/c/Documents and Settings/クリストファー/Downloads/ALMA00000085.fits";
-    char filename[] = "/Users/chris/Downloads/SVS13_13CO.clean.image.pbcor.fits";
-
-    status = 0;
+    int status = 0;
 
     // load a FITS file
     if (fits_open_file(&fptr, filename, READONLY, &status))
@@ -67,7 +58,11 @@ int main()
     if (wcs == NULL)
     {
         printf("wcs is NULL\n");
-        return 1;
+
+        if (fits_close_file(fptr, &status))
+            printerror(status);
+
+        return;
     }
 
     stat = wcspih(header, nkeys, relax, ctrl, &nreject, &nwcs, &wcs);
@@ -79,9 +74,6 @@ int main()
     double pixcrd[2];
     double world[2];
     double coords[2];
-
-    const double x = 905.0, y = 880.0;              // exported from DS9 as 'physical' coordinates
-    const double ra = 52.2656215, dec = 31.2677022; // exported from DS9 as 'fk5' WCS coordinates
 
     printf("[ds9] x: %f, y: %f\n", x, y);
     printf("[ds9] ra: %f, dec: %f\n", ra, dec);
@@ -140,31 +132,18 @@ int main()
 
     free(header);
 
-    /* read the NAXIS1 and NAXIS2 keyword to get image size */
-    if (fits_read_keys_lng(fptr, "NAXIS", 1, 2, naxes, &nfound, &status))
-        printerror(status);
-
-    npixels = naxes[0] * naxes[1]; /* number of pixels in the image */
-    fpixel = 1;
-    float nullval = 0; /* don't check for null values in the image */
-
-    pixels = (float *)malloc(npixels * sizeof(float));
-    mask = (char *)malloc(npixels * sizeof(char));
-
-    // it's a test program so don't bother with checking pixels and mask pointers for NULL, just assume they are properly allocated
-    if (fits_read_imgnull(fptr, TFLOAT, fpixel, npixels, pixels, mask, &anynull, &status))
-        printerror(status);
-
-    // print anynull
-    printf("anynull: %d\n", anynull);
-
-    free(pixels);
-    free(mask);
-
     if (fits_close_file(fptr, &status))
         printerror(status);
 
     free(wcs);
+}
+
+int main()
+{
+    // test_wcs("/Users/chris/Downloads/SVS13_13CO.clean.image.pbcor.fits", 905.0, 880.0, 52.2656215, 31.2677022);
+    test_wcs("/home/chris/ダウンロード/ALMA01084695.fits", 905.0, 880.0, 52.2656215, 31.2677022);
+
+    printf("got here\n");
 
     return 0;
 }
