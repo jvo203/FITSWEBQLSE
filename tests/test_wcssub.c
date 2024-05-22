bb588f79dbc3bea9b072e960b680385480877bec
jvo203 <chris.zapart@nao.ac.jp>
2024-05-22 17:26:54 +0900

    modified:   tests/test_wcssub.c

--- tests/test_wcssub.c
+++ tests/test_wcssub.c
@@ -133,6 +133,8 @@ int main()
 
     // passing the ra, dec obtained from SAO ds9
     test_wcs("/Users/chris/Downloads/ALMA01018218.fits", 856.49056, 438.4528, 261.2105354, -34.2435452); // OK file
+
+    // passing the ra, dec obtained from SAO ds9
     test_wcs("/home/chris/ダウンロード/SVS13_13CO.clean.image.pbcor.fits", 905.0, 880.0, 52.2656215, 31.2677022);
 
     return 0;
