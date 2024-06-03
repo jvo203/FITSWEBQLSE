a1a95afbf94bfeffca8489491d345dfbd781a4e7
jvo203 <chris.zapart@nao.ac.jp>
2024-06-03 16:33:46 +0900

    added fk4 to tests

--- tests/test_starlink.c
+++ tests/test_starlink.c
@@ -2,6 +2,30 @@
 #include <stdio.h>
 #include <star/ast.h>
 
+void test_fk4()
+{
+    double ra1, dec1, ra2, dec2;
+
+    astBegin;
+
+    AstSkyFrame *fk4 = astSkyFrame("System=fk4");
+    AstSkyFrame *icrs = astSkyFrame("System=ICRS");
+
+    AstFrameSet *fk42icrs = astConvert(fk4, icrs, " ");
+
+    // ds9 ra,dec in FK4
+    ra1 = 51.4924676 * AST__DD2R;
+    dec1 = 31.0959144 * AST__DD2R;
+
+    printf("Original ra,dec (ds9) exported as FK4: %f %f\n", ra1 * AST__DR2D, dec1 * AST__DR2D);
+    astTran2(fk42icrs, 1, &ra1, &dec1, 1, &ra2, &dec2);
+    printf("AST FK4 --> ICRS: %f %f\n", ra2 * AST__DR2D, dec2 * AST__DR2D);
+
+    printf("====================================================\n");
+
+    astEnd;
+}
+
 void test_fk5()
 {
     double ra1, dec1, ra2, dec2;
@@ -25,12 +49,12 @@ void test_fk5()
     printf("====================================================\n");
 
     // ds9 ra,dec in ICRS
-    ra1 = 52.2656094 * AST__DD2R;
+    /*ra1 = 52.2656094 * AST__DD2R;
     dec1 = 31.2677078 * AST__DD2R;
 
     printf("Original ra,dec (ds9) exported as ICRS: %f %f\n", ra1 * AST__DR2D, dec1 * AST__DR2D);
     astTran2(icrs2fk5, 1, &ra1, &dec1, 1, &ra2, &dec2);
-    printf("AST ICRS --> FK5: %f %f\n", ra2 * AST__DR2D, dec2 * AST__DR2D);
+    printf("AST ICRS --> FK5: %f %f\n", ra2 * AST__DR2D, dec2 * AST__DR2D);*/
 
     // Clean up
     // not really needed as astEnd should do it
@@ -45,6 +69,8 @@ void test_fk5()
 
 int main()
 {
+    test_fk4();
+
     test_fk5();
 
     return 0;
