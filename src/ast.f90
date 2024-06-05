c6e472ea7188a35ad3dc017c4166df66f713ed62
jvo203 <chris.zapart@nao.ac.jp>
2024-06-05 11:00:24 +0900

    added ast_begin and ast_end

--- src/ast.f90
+++ src/ast.f90
@@ -1,11 +1,20 @@
 module ast
-   use, intrinsic :: ISO_C_BINDING
-   use, intrinsic :: ISO_FORTRAN_ENV
-
    implicit none
 
    interface
+      ! void astBegin_( void )
+      subroutine AST_BEGIN() bind(C, name="astBegin_")
+         use, intrinsic :: ISO_C_BINDING
+         implicit none
+      end subroutine AST_BEGIN
 
+      ! void astEnd_( int *status )
+      subroutine AST_END( status ) bind(C, name="astEnd_")
+         use, intrinsic :: ISO_C_BINDING
+         implicit none
+
+         integer(C_INT), intent(out) :: status
+      end subroutine AST_END
    end interface
 
 end module ast
