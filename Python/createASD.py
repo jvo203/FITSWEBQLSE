f437b0cac92858db0861be70fa4e4ffa0d2ab3f8
jvo203 <chris.zapart@nao.ac.jp>
2024-07-03 14:46:22 +0900

    modified:   Python/createASD.py

--- Python/createASD.py
+++ Python/createASD.py
@@ -1,4 +1,6 @@
 import sqlite3
+import hashlib
+import urllib.request
 
 conn = sqlite3.connect("asd.db")
 c = conn.cursor()
@@ -9,3 +11,15 @@ try:
     c.execute(strSQL)
 except sqlite3.OperationalError as err:
     print(err, ":", strSQL)
+
+strSQL = "CREATE TABLE lines (Ion TEXT, Observed REAL, Ritz REAL, relative_intensity REAL, hash TEXT UNIQUE) ;"
+
+try:
+    c.execute(strSQL)
+except sqlite3.OperationalError as err:
+    print(err, ":", strSQL)
+
+server = "https://www.nist.gov/pml/atomic-spectra-database"
+
+# finalize_db()
+conn.close()
