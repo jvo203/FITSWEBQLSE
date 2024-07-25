e935eab669893110246d3949570f15e6dfab158c
jvo203 <chris.zapart@nao.ac.jp>
2024-07-25 17:25:33 +0900

    new file:   Julia/HDS.jl

---
+++ Julia/HDS.jl
@@ -0,0 +1,38 @@
+using FITSIO
+using Plots
+
+fitspath = homedir() * "/Downloads/fsclmo_HDSA00042941.fits"
+
+f = FITS(fitspath)
+N = ndims(f[1])
+println("ndims: ", N, ", size: ", size(f[1]))
+
+header = read_header(f[1])
+
+width = 0
+height = 0
+
+if haskey(header, "NAXIS1")
+    width = header["NAXIS1"]
+end
+
+if haskey(header, "NAXIS2")
+    height = header["NAXIS2"]
+end
+
+println("width: $(width), height: $(height)")
+
+data = read(f[1], :, :)
+println("data size: ", size(data))
+
+close(f)
+
+x = Int(width / 2)
+y = Int(height / 2)
+println("x: $(x), y: $(y)")
+
+# take the row at the center of the image
+row = data[:, y]
+println("row size: ", size(row))
+
+plot(row, label="Spectrum", xlabel="Pixel", ylabel="Intensity", legend=:topleft)
