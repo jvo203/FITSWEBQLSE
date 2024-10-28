1b48d4a45a49d94828845959f4b48dafec1f0f05
Christopher Zapart <chris.zapart@nao.ac.jp>
2024-10-28 17:37:32 +0900

    testing 'init_population'

---
+++ tests/test_de.f90
@@ -0,0 +1,22 @@
+program main
+    use differential_evolution
+    use omp_lib
+    implicit none
+
+    ! make dim a parameter
+    integer, parameter :: dim = 10
+
+    integer :: seed = 123
+    integer :: pop_size = 10    
+    real(float) :: min_val(dim), max_val(dim)
+    type(Population) :: pop
+
+    integer :: i
+
+    ! set the min and max values for each dimension to [-10, 10]
+    min_val(:) = -10.0
+    max_val(:) = 10.0
+
+    ! initialize the population
+    call init_population(pop, pop_size, dim, seed, min_val, max_val)
+end program main
