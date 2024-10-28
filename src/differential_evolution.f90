4da0d4151359cb6bb1eb13abbcd2c8fdc863b38f
Christopher Zapart <chris.zapart@nao.ac.jp>
2024-10-28 14:47:13 +0900

    created a Population structure

--- src/differential_evolution.f90
+++ src/differential_evolution.f90
@@ -11,13 +11,25 @@ module differential_evolution
    ! integer,parameter :: float = real64  ! double precision real kind [8 bytes].
 
    type Individual
-      real(float),dimension(:),allocatable :: params
+      real(float), dimension(:), allocatable :: genotype ! the genotype of the individual solution
       real(float) :: cost ! the cost of the individual solution
 
       ! control parameters
       real(float) :: cr = 0.5_float
       real(float) :: f = 0.5_float
    end type Individual
+
+   type, public :: Population
+      type(Individual), dimension(:), allocatable :: curr, best ! current and best individuals      
+
+      integer :: pop_size = 0 ! population size
+      integer :: best_idx = 0 ! index of the best individual
+
+      real(float) :: best_cost = huge(0.0_float) ! cost of the best individual
+      integer :: num_cost_evals = 0 ! number of cost evaluations
+      integer :: dim = 0 ! dimension of the problem      
+   end type Population
+
 
 contains
 
