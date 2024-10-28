4a9c28a59e689ee7f5b0ffba819cd48587414ea3
Christopher Zapart <chris.zapart@nao.ac.jp>
2024-10-28 14:21:24 +0900

    added an external code with the Mersenne Twister pseudorandom number generator

--- src/differential_evolution.f90
+++ src/differential_evolution.f90
@@ -1,5 +1,14 @@
 module differential_evolution
+   use,intrinsic :: iso_fortran_env
+   use mt19937_64
+   use omp_lib
+
    implicit none
+
+   private
+
+   integer,parameter :: float = real32  ! single precision real kind [4 bytes].
+   ! integer,parameter :: float = real64  ! double precision real kind [8 bytes].
 
 contains
 
