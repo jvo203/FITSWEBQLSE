b3944f2dbe4ae4e964ca4474c9825651566030a0
Christopher Zapart <chris.zapart@nao.ac.jp>
2024-10-28 14:34:14 +0900

    added an <Individual> structure

--- src/differential_evolution.f90
+++ src/differential_evolution.f90
@@ -10,6 +10,15 @@ module differential_evolution
    integer,parameter :: float = real32  ! single precision real kind [4 bytes].
    ! integer,parameter :: float = real64  ! double precision real kind [8 bytes].
 
+   type Individual
+      real(float),dimension(:),allocatable :: params
+      real(float) :: cost ! the cost of the individual solution
+
+      ! control parameters
+      real(float) :: cr = 0.5_float
+      real(float) :: f = 0.5_float
+   end type Individual
+
 contains
 
 end module differential_evolution
