73dbf288bd66d71b8cd6b81bf7b151536a0c94b8
Christopher Zapart <chris.zapart@nao.ac.jp>
2024-10-31 10:57:47 +0900

    updated the 'x265_encoder_encode()' function signature to make it compatible with x265 v4

--- src/microws.c
+++ src/microws.c
@@ -3372,8 +3372,9 @@ void write_ws_video(websocket_session *session, const int *seq_id, const float *
     // RGB-encode
     x265_nal *pNals = NULL;
     uint32_t iNal = 0;
+    x265_picture *pic_out = NULL;
 
-    int ret = x265_encoder_encode(session->encoder, &pNals, &iNal, session->picture, NULL);
+    int ret = x265_encoder_encode(session->encoder, &pNals, &iNal, session->picture, &pic_out);
     printf("[C] x265_encode::ret = %d, #frames = %d\n", ret, iNal);
 
     for (unsigned int i = 0; i < iNal; i++)
@@ -3497,8 +3498,9 @@ void write_ws_composite_video(websocket_session *session, const int *seq_id, con
     // RGB-encode
     x265_nal *pNals = NULL;
     uint32_t iNal = 0;
+    x265_picture *pic_out = NULL;
 
-    int ret = x265_encoder_encode(session->encoder, &pNals, &iNal, session->picture, NULL);
+    int ret = x265_encoder_encode(session->encoder, &pNals, &iNal, session->picture, &pic_out);
     printf("[C] x265_encode::ret = %d, #frames = %d\n", ret, iNal);
 
     for (unsigned int i = 0; i < iNal; i++)
