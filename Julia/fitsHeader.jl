7e7c74364b868e5649d9aa9ca6064ca89e518385
jvo203 <chris.zapart@nao.ac.jp>
2025-01-27 17:00:50 +0900

    debugging merging the cubes

--- Julia/fitsHeader.jl
+++ Julia/fitsHeader.jl
@@ -1,25 +1,56 @@
+using AstroImages
 using FITSIO
+using Plots
 
 basedir = homedir() * "/NAO/POLARISATION/ALMA"
 band = "band3"
+name = "concat.ms.cal.mfs.impbcor"
 
-# read FITS header from the I intensity FITS file
-filename = basedir * "/OrionKL/" * band * "/concat.ms.cal.mfs.impbcor.I.fits"
-header = FITSIO.read_header(filename)
-println(header)
+Stokes = ["I", "Q", "U", "V"]
+#Stokes = ["I", "Q", "U", "I"] # for testing
 
-# read a data cube from the Stokes IQU FITS file (the first image)
-filename = basedir * "/OrionKL/" * band * "/wolfram.StokesIQU.fits"
-f = FITS(filename)
-data = read(f[1])
-println(size(data))
+function real_data(filename)
+    f = FITS(filename)
+    data = read(f[1])
+    close(f)
 
-# modify the header to have the same cube dimensions as the data
-header["NAXIS4"] = size(data, 4)
-
-# write the modified header with new data to a new FITS file
-filename = basedir * "/OrionKL/" * band * "/concat.ms.cal.mfs.impbcor.StokesIQU.fits"
-FITS(filename, "w") do f
-    write(f, data; header=header)    
+    return data
 end
 
+# for each Stokes parameter, read the FITS data
+planes = []
+for stokes in Stokes
+    local data
+
+    fn = basedir * "/OrionKL/" * band * "/" * name * "." * stokes * ".fits"
+
+    try
+        data = real_data(fn)
+        println("Read file: ", fn, " with size: ", size(data))
+        push!(planes, data)
+    catch
+        println("Error reading file: ", fn)
+    end
+end
+println(size(planes))
+println(planes[1][1:10])
+
+# create a 4D cube from the planes
+cube = cat(planes..., dims=4)
+println("cube:", size(cube))
+println(cube[1:10])
+
+# read FITS header from the first Stokes parameter FITS file (I)
+filename = basedir * "/OrionKL/" * band * "/" * name * "." * Stokes[1] * ".fits"
+header = FITSIO.read_header(filename)
+
+# modify the header to have the same cube dimensions as the data
+header["NAXIS4"] = size(cube, 4)
+
+# write the modified header with new data to a new FITS file
+filename = basedir * "/OrionKL/" * band * "/" * name * ".StokesIQU.fits"
+FITS(filename, "w") do f
+    write(f, cube; header=header)    
+end
+
+#implot(planes[1][:,:,1,1])
