c80a51b15aecfb87b68ca4883b31434cf5f7feeb
jvo203 <chris.zapart@nao.ac.jp>
2025-02-04 14:55:28 +0900

    adjusted the polarisation vector field scaling function

--- htdocs/fitswebql/fitswebqlse.js
+++ htdocs/fitswebql/fitswebqlse.js
@@ -4184,7 +4184,7 @@ function process_polarisation(index, pol_width, pol_height, intensity, angle, ma
         let mag = (item.I - mean) / (std * Math.sqrt(2.0)); // normalise the intensity
         let x = item.x;
         let y = item.y;
-        let r = grid_spacing * 0.5 * (1.0 + erf(mag)); // between 0 and grid_spacing
+        let r = grid_spacing * 0.5 * (1.0 + erf(10 * mag)); // between 0 and grid_spacing
 
         let vector = { x: x - 0.5 * r * Math.cos(angle), y: y - 0.5 * r * Math.sin(angle), vx: r * Math.cos(angle), vy: r * Math.sin(angle) };
         vectors.push(vector);
