db32cbfe403271dc354467a66209c12e38c12b5d
jvo203 <chris.zapart@nao.ac.jp>
2025-07-15 16:40:16 +0900

    removed the debugging comment

--- src/fits.f90
+++ src/fits.f90
@@ -10269,8 +10269,7 @@ contains
       if (.not. c_associated(arg)) return
       call c_f_pointer(arg, resp)
 
-      ! disabled during debugging
-      ! call write_ws_video(resp%session, resp%seq_id, resp%timestamp, resp%elapsed, c_loc(resp%pixels), c_loc(resp%mask))
+      call write_ws_video(resp%session, resp%seq_id, resp%timestamp, resp%elapsed, c_loc(resp%pixels), c_loc(resp%mask))
 
       ! deallocate the pixels/mask
       deallocate (resp%pixels)
