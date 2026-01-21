d7d440b0a311d0e06fb602c48a366072c4cd8479
jvo203 <chris.zapart@nao.ac.jp>
2026-01-21 10:59:25 +0900

    switch loading of the surfase.js back to the CDN

--- htdocs/fitswebql/fitswebqlse.js
+++ htdocs/fitswebql/fitswebqlse.js
@@ -21575,8 +21575,8 @@ function enable_3d_view() {
 
         (function () {
             var po = document.createElement('script'); po.type = 'text/javascript'; po.async = false;
-            po.src = 'surface.js' + '?' + encodeURIComponent(get_js_version());
-            //po.src = 'https://cdn.jsdelivr.net/gh/jvo203/FITSWEBQLSE@' + votable.getAttribute('data-version-major') + '.' + votable.getAttribute('data-version-minor') + '.' + votable.getAttribute('data-version-sub') + '/htdocs/fitswebql/surface.min.js';
+            //po.src = 'surface.js' + '?' + encodeURIComponent(get_js_version());
+            po.src = 'https://cdn.jsdelivr.net/gh/jvo203/FITSWEBQLSE@' + votable.getAttribute('data-version-major') + '.' + votable.getAttribute('data-version-minor') + '.' + votable.getAttribute('data-version-sub') + '/htdocs/fitswebql/surface.min.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
         })();
 
