73878e07fc993d10d6943ccab62b41614ec081e2
Christopher Zapart <chris.zapart@nao.ac.jp>
2026-07-07 11:43:14 +0900

    adjusted the comment

--- src/fits.f90
+++ src/fits.f90
@@ -6013,8 +6013,8 @@ contains
       u = 7.5
       black = max(pmin, pmedian - u*madN)
       white = min(pmax, pmedian + u*madP)
-      !black = hist_quantile(data, pmin, pmax, 0.05)
-      !white = hist_quantile(data, pmin, pmax, 0.95)
+      !black = hist_quantile(data, pmin, pmax, 0.05) ! 0.01 or 0.05
+      !white = hist_quantile(data, pmin, pmax, 0.95) ! 0.99 or 0.95
       sensitivity = 1.0/(white - black)
       ratio_sensitivity = sensitivity
 
