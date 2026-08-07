51bd2816bd2dcb223e2eb5f491ffeb48c36fafab
Christopher Zapart <chris.zapart@nao.ac.jp>
2026-08-07 15:48:58 +0900

    make use of the <horizontal> & <vertical>

--- src/fits.f90
+++ src/fits.f90
@@ -7999,14 +7999,16 @@ contains
                   if (req%intensity .eq. velocity) then
                      call viewport_moment_map_1_rect(c_loc(item%compressed(frame, 1)%ptr), width, height,&
                      &item%frame_min(frame, 1), item%frame_max(frame, 1), c_loc(thread_I(:, :, tid)),&
-                     &c_loc(thread_Iv(:, :, tid)), c_loc(thread_mask(:, :, tid)), dimx, x1 - 1, x2 - 1, y1 - 1, y2 - 1, 0, 0, vel)
+                     &c_loc(thread_Iv(:, :, tid)), c_loc(thread_mask(:, :, tid)), dimx, x1 - 1, x2 - 1, y1 - 1, y2 - 1,&
+                     & x1 - req%x1, y1 - req%y1, vel)
                   end if
 
                   ! the 2nd moment
                   if (req%intensity .eq. dispersion) then
                      call viewport_moment_map_2_rect(c_loc(item%compressed(frame, 1)%ptr), width, height,&
                      &item%frame_min(frame, 1), item%frame_max(frame, 1), c_loc(thread_I(:, :, tid)), c_loc(thread_Iv(:, :, tid)),&
-                     & c_loc(thread_Iv2(:, :, tid)),c_loc(thread_mask(:, :, tid)), dimx, x1 - 1, x2 - 1, y1 - 1, y2 - 1, 0, 0, vel)
+                     & c_loc(thread_Iv2(:, :, tid)),c_loc(thread_mask(:, :, tid)), dimx, x1 - 1, x2 - 1, y1 - 1, y2 - 1,&
+                     & x1 - req%x1, y1 - req%y1, vel)
                   end if
 
                   ! the 8th moment (maximum)
@@ -8014,7 +8016,7 @@ contains
                      call viewport_moment_map_8_rect(c_loc(item%compressed(frame, 1)%ptr), width, height, &
                      &item%frame_min(frame, 1), item%frame_max(frame, 1), &
                      &c_loc(thread_I(:, :, tid)), c_loc(thread_mask(:, :, tid)),&
-                     & dimx, x1 - 1, x2 - 1, y1 - 1, y2 - 1, 0, 0)
+                     & dimx, x1 - 1, x2 - 1, y1 - 1, y2 - 1, x1 - req%x1, y1 - req%y1)
                   end if
                end if
             end if
